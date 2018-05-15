1c2e2ae584decfb3e0bb4809373d8ad02129e813
Paul Sealock <psealock@gmail.com>
2018-05-16 09:14:33 +1200

    use Gutenberg

--- client/layout/sidebar.js
+++ client/layout/sidebar.js
@@ -4,14 +4,12 @@
  */
 import { Component } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { TabPanel } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import Activity from 'dashboard/widgets/activity';
-import { Tabs, Tab, TabPanel } from 'components/tabs';
-
-import { TabPanel as GutenTabPanel } from '@wordpress/components';
 
 const onSelect = tabName => {
 	console.log( 'Selecting tab', tabName );
@@ -21,78 +19,49 @@ const Count = ( { value } ) => {
 	return <span>{ value }</span>;
 };
 
-const myTabTitle = (
-	<h2>
-		Hello World <Count value={ 22 } />
-	</h2>
-);
-console.log( typeof myTabTitle );
-
-function MyTabPanel() {
-	return (
-		<GutenTabPanel
-			className="my-tab-panel"
-			activeClass="active-tab"
-			onSelect={ onSelect }
-			tabs={ [
-				{
-					name: 'tab1',
-					title: myTabTitle,
-					className: 'tab-one',
-				},
-				{
-					name: 'tab2',
-					title: 'Tab 2',
-					className: 'tab-two',
-				},
-			] }
-		>
-			{ tabName => {
-				return <p>${ tabName }</p>;
-			} }
-		</GutenTabPanel>
-	);
-}
-
 class Sidebar extends Component {
-	// state = {
-	// 	activeTabIndex: 0,
-	// };
+	getTitle( str, count ) {
+		return (
+			<h2>
+				{ str } <Count value={ count } />
+			</h2>
+		);
+	}
 
 	getTabs() {
 		return [
-			{ title: __( 'Insights', 'woo-dash' ), key: 'insights', count: 3 },
-			{ title: __( 'Orders', 'woo-dash' ), key: 'orders', count: 0 },
-			{ title: __( 'Reviews', 'woo-dash' ), key: 'reviews', count: 21 },
+			{
+				name: 'insights',
+				title: this.getTitle( __( 'Insights', 'woo-dash' ), 3 ),
+				className: 'my-class',
+			},
+			{
+				name: 'orders',
+				title: this.getTitle( __( 'Orders', 'woo-dash' ), 0 ),
+				className: 'my-class',
+			},
+			{
+				name: 'reviews',
+				title: this.getTitle( __( 'Reviews', 'woo-dash' ), 21 ),
+				className: 'my-class',
+			},
 		];
 	}
 
-	// onTabClick = a => {
-	// 	console.log( a );
-	// };
-
 	render() {
-		// const { activeTabIndex } = this.state;
-		const activeTabIndex = 1;
 		const tabs = this.getTabs();
-		const activeTab = tabs[ activeTabIndex ];
 		return (
 			<div className="woo-dash__secondary">
-				{ MyTabPanel() }
-				<Tabs>
-					{ tabs.map( ( tab, index ) => (
-						<Tab
-							active={ activeTabIndex === index }
-							count={ tab.count }
-							key={ tab.key }
-							controls={ tab.key }
-							onClick={ onSelect }
-						>
-							{ tab.title }
-						</Tab>
-					) ) }
-				</Tabs>
-				<TabPanel id={ activeTab.key }>{ `Section for ${ activeTab.title }` }</TabPanel>
+				<TabPanel
+					className="my-tab-panel"
+					activeClass="active-tab"
+					onSelect={ onSelect }
+					tabs={ tabs }
+				>
+					{ tabName => {
+						return <p>${ tabName }</p>;
+					} }
+				</TabPanel>
 				<Activity />
 			</div>
 		);
