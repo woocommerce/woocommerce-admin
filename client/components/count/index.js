2e67f3ac4820404dbcc612dc0c58c8a394851532
Kelly Dwan <ryelle@users.noreply.github.com>
2018-05-31 13:02:56 -0400

    Add a label “Total” to the count balloon

--- client/components/count/index.js
+++ client/components/count/index.js
@@ -2,18 +2,28 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-const Count = ( { count } ) => {
-	return <span className="woocommerce-count">{ count }</span>;
+const Count = ( { count, label } ) => {
+	return (
+		<span className="woocommerce-count">
+			<span className="screen-reader-text">{ label }</span> { count }
+		</span>
+	);
 };
 
 Count.propTypes = {
 	count: PropTypes.number.isRequired,
+	label: PropTypes.string,
+};
+
+Count.defaultProps = {
+	label: __( 'Total', 'woo-dash' ),
 };
 
 export default Count;
