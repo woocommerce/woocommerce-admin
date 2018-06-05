2c08e2b324ba05b652507b57defed5268f752511
Kelly Dwan <ryelle@users.noreply.github.com>
2018-06-05 15:19:54 -0400

    Add currency and order calculation helpers from Calypso

--- client/components/activity-list/orders.js
+++ client/components/activity-list/orders.js
@@ -2,6 +2,7 @@
 /**
  * External dependencies
  */
+import { __, sprintf } from '@wordpress/i18n';
 import { Component, compose } from '@wordpress/element';
 import { Dashicon, withAPIData } from '@wordpress/components';
 import PropTypes from 'prop-types';
@@ -10,19 +11,38 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import ActivityCard from 'components/activity-card';
+import { getCurrencyFormatDecimal, getCurrencyFormatString } from 'lib/currency';
+import { getOrderRefundTotal } from 'lib/order-values';
 
 class OrdersList extends Component {
 	renderCard( order, i ) {
+		const address = { ...order.shipping, ...order.billing }; // We want the billing address, but shipping can be used as a fallback.
+		const name = `${ address.first_name } ${ address.last_name }`;
+		const productsCount = order.line_items.reduce( ( total, line ) => total + line.quantity, 0 );
+
+		const total = order.total;
+		const refundValue = getOrderRefundTotal( order );
+		const remainingTotal = getCurrencyFormatDecimal( order.total ) + refundValue;
+
 		return (
 			<ActivityCard
 				key={ i }
-				label="Order"
+				label={ __( 'Order', 'woo-dash' ) }
 				icon={ <Dashicon icon="format-aside" /> }
 				date={ order.date_created }
 			>
-				{ 'Zé Marques placed order #99' }
-				{ '4 products - $198.34' }
-				[ Completed ]
+				<div>{ sprintf( __( '%s placed order #%d', 'woo-dash' ), name, order.id ) }</div>
+				<div>
+					<span>{ sprintf( __( '%d products' ), productsCount ) }</span>
+					{ ' ' }
+					{ refundValue ? (
+						<span>
+							<s>{ getCurrencyFormatString( total ) }</s> { getCurrencyFormatString( remainingTotal ) }
+						</span>
+					) : (
+						<span>{ getCurrencyFormatString( total ) }</span>
+					) }
+				</div>
 			</ActivityCard>
 		);
 	}
