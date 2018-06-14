6b8b5388c5e78c1c6ca6327a1a59de892c05006b
Kelly Dwan <ryelle@users.noreply.github.com>
2018-06-14 14:38:38 -0400

    Add fake data & display revenue on the analytics test report

---
+++ client/analytics/report/mock-data.js
@@ -0,0 +1,84 @@
+/** @format */
+
+export default {
+	totals: {
+		gross_revenue: 9476.0,
+		coupons: 504.0,
+		refunds: 145.0,
+		shipping: 990.0,
+		taxes: '663.32',
+		net_revenue: '7173.68',
+		order_count: 42.0,
+	},
+	intervals: [
+		{
+			week: [
+				{
+					'2018-W22': {
+						gross_revenue: 8394.0,
+						coupons: 103.0,
+						refunds: 284.0,
+						shipping: 294.0,
+						taxes: '328.32',
+						net_revenue: '6264.68',
+						order_count: 30.0,
+					},
+					'2018-W23': {
+						gross_revenue: 9374.0,
+						coupons: 126.0,
+						refunds: 198.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+					'2018-W24': {
+						gross_revenue: 9476.0,
+						coupons: 504.0,
+						refunds: 145.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+					'2018-W25': {
+						gross_revenue: 9476.0,
+						coupons: 504.0,
+						refunds: 145.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+					'2018-W26': {
+						gross_revenue: 9476.0,
+						coupons: 504.0,
+						refunds: 145.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+					'2018-W27': {
+						gross_revenue: 9476.0,
+						coupons: 504.0,
+						refunds: 145.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+					'2018-W28': {
+						gross_revenue: 9476.0,
+						coupons: 504.0,
+						refunds: 145.0,
+						shipping: 990.0,
+						taxes: '663.32',
+						net_revenue: '7173.68',
+						order_count: 42.0,
+					},
+				},
+			],
+		},
+	],
+};
