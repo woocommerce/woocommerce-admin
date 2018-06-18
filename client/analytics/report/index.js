dd01f10fb5c8a0e34d0c9ab1ac2e5ebfccbb9c77
Justin Shreve <justin.shreve@gmail.com>
2018-06-18 12:43:43 -0400

    Refactor query string/route updating so it can be done using a nav-utils helper function

--- client/analytics/report/index.js
+++ client/analytics/report/index.js
@@ -2,39 +2,17 @@
 /**
  * External dependencies
  */
-import { Component, Fragment, compose } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 import PropTypes from 'prop-types';
-import { stringify as stringifyQueryObject } from 'qs';
-import { withRouter } from 'react-router-dom';
 
 /**
  * Internal dependencies
  */
 import ExampleReport from './example';
 import RevenueReport from './revenue';
-import Pagination from 'components/pagination';
 
 class Report extends Component {
-	constructor() {
-		super();
-		this.onPageChange = this.onPageChange.bind( this );
-		this.onPerPageChange = this.onPerPageChange.bind( this );
-	}
-	onPageChange( page ) {
-		this.updateLocation( { page } );
-	}
-
-	onPerPageChange( perPage ) {
-		this.updateLocation( { per_page: perPage } );
-	}
-
-	updateLocation( data ) {
-		const { path, query, history } = this.props;
-		const queryString = stringifyQueryObject( Object.assign( query, data ) );
-		history.push( `${ path }?${ queryString }` );
-	}
-
-	renderReport() {
+	render() {
 		const { params } = this.props;
 		switch ( params.report ) {
 			case 'revenue':
@@ -43,31 +21,10 @@ class Report extends Component {
 				return <ExampleReport />;
 		}
 	}
-
-	render() {
-		const { query } = this.props;
-		return (
-			<Fragment>
-				{ this.renderReport() }
-				<Pagination
-					page={ parseInt( query.page ) || 1 }
-					perPage={ parseInt( query.per_page ) || 25 }
-					total={ 5000 }
-					onPageChange={ this.onPageChange }
-					onPerPageChange={ this.onPerPageChange }
-				/>
-			</Fragment>
-		);
-	}
 }
 
 Report.propTypes = {
 	params: PropTypes.object.isRequired,
-	history: PropTypes.shape( {
-		push: PropTypes.func.isRequired,
-	} ),
-	path: PropTypes.string.isRequired,
-	query: PropTypes.object.isRequired,
 };
 
-export default compose( [ withRouter ] )( Report );
+export default Report;
