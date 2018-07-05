04c635e83fba31e51842015dcc19f0b2699cde74
Kevin Killingsworth <kevin@redfenix.com>
2018-07-05 15:13:57 -0500

    Add fresh-data provider.
    This adds the code necessary to include the fresh-data provider in the
    application.

--- client/index.js
+++ client/index.js
@@ -3,16 +3,26 @@
  * External dependencies
  */
 import { APIProvider } from '@wordpress/components';
+import { FreshDataProvider } from 'fresh-data';
 import { pick } from 'lodash';
 import { render } from '@wordpress/element';
+import { Provider as ReduxProvider } from 'react-redux';
 import { Provider as SlotFillProvider } from 'react-slot-fill';
 import 'react-dates/initialize';
 
 /**
  * Internal dependencies
  */
+import createStore from 'state/create-store';
+import reducer from 'state/reducer';
 import './stylesheets/_wpadmin-reset.scss';
 import { PageLayout } from './layout';
+import WooCommerceRestApi from './wc-api';
+
+const store = createStore( reducer );
+const apis = {
+	woocommerce: new WooCommerceRestApi(),
+};
 
 render(
 	<APIProvider
@@ -20,7 +30,11 @@ render(
 		{ ...pick( wp.api, [ 'postTypeRestBaseMapping', 'taxonomyRestBaseMapping' ] ) }
 	>
 		<SlotFillProvider>
-			<PageLayout />
+			<ReduxProvider store={ store }>
+				<FreshDataProvider apis={ apis }>
+					<PageLayout />
+				</FreshDataProvider>
+			</ReduxProvider>
 		</SlotFillProvider>
 	</APIProvider>,
 	document.getElementById( 'root' )
---
+++ client/state/reducer.js
@@ -0,0 +1,12 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { combineReducers } from 'redux';
+import { reducer as freshData } from 'fresh-data';
+
+const reducers = {
+	freshData,
+};
+
+export default combineReducers( reducers );
