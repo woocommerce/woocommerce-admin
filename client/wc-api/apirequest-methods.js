bbab496d1d7934a931f035867f1b3726ea9983f9
Kevin Killingsworth <kevin@redfenix.com>
2018-07-10 17:39:58 -0500

    Add tests for wc-api methods and orders.
    This adds tests for the wc-api methods, and for the order operations,
    mutations, and selectors.

--- client/wc-api/apirequest-methods.js
+++ client/wc-api/apirequest-methods.js
@@ -5,29 +5,31 @@
 import apiRequest from '@wordpress/api-request';
 import qs from 'qs';
 
-const URL_PREFIX = '/wc/v3';
+export const URL_PREFIX = '/wc/v3';
 
-const methods = {
-	get: () => ( endpointPath, params ) => {
-		// TODO: Support multisite via clientKey
-		const path = endpointPath.join( '/' );
-		const queryString = params ? `?${ qs.stringify( params ) }` : '';
-		const url = `${ URL_PREFIX }/${ path }${ queryString }`;
-		return apiRequest( { path: url } ).then( response => {
-			// TODO: Error handling.
-			return response;
-		} );
-	},
-	post: () => ( endpointPath, params ) => {
-		// TODO: Support multisite via clientKey
-		const path = endpointPath.join( '/' );
-		const { data } = params;
-		const url = `${ URL_PREFIX }/${ path }`;
-		return apiRequest( { path: url, method: 'POST', data } ).then( response => {
-			// TODO: Error handling.
-			return response;
-		} );
-	},
-};
+export function createMethods( request ) {
+	return {
+		get: () => ( endpointPath, params ) => {
+			// TODO: Support multisite via clientKey
+			const path = endpointPath.join( '/' );
+			const queryString = params ? `?${ qs.stringify( params ) }` : '';
+			const url = `${ URL_PREFIX }/${ path }${ queryString }`;
+			return request( { path: url } ).then( response => {
+				// TODO: Error handling.
+				return response;
+			} );
+		},
+		post: () => ( endpointPath, params ) => {
+			// TODO: Support multisite via clientKey
+			const path = endpointPath.join( '/' );
+			const { data } = params;
+			const url = `${ URL_PREFIX }/${ path }`;
+			return request( { path: url, method: 'POST', data } ).then( response => {
+				// TODO: Error handling.
+				return response;
+			} );
+		},
+	};
+}
 
-export default methods;
+export default createMethods( apiRequest );
---
+++ client/wc-api/test/orders.js
@@ -0,0 +1,175 @@
+/** @format */
+/**
+ * Internal dependencies
+ */
+import orders from '../orders';
+
+describe( 'orders', () => {
+	describe( 'operations', () => {
+		const methods = {};
+		const getResponse = [];
+		const postResponse = {};
+
+		beforeEach( () => {
+			methods.get = jest.fn();
+			methods.post = jest.fn();
+
+			methods.get.mockReturnValue( Promise.resolve().then( () => getResponse ) );
+			methods.post.mockReturnValue( Promise.resolve().then( () => postResponse ) );
+		} );
+
+		describe( 'read', () => {
+			it( 'should ignore resourceNames not meant for it.', () => {
+				const { read } = orders.operations;
+				const resourceNames = [ 'ignoreme', 'orders:', 'products-page:' ];
+				Promise.all( read( methods )( resourceNames ) ).then( () => {
+					expect( methods.get ).not.toHaveBeenCalled();
+					expect( methods.post ).not.toHaveBeenCalled();
+				} );
+			} );
+
+			it( 'should request multiple pages of orders individually.', () => {
+				const { read } = orders.operations;
+				const resourceNames = [
+					'orders-page:{"page":1,"per_page":10}',
+					'orders-page:{"page":3,"per_page":100}',
+					'ignoreme',
+				];
+				Promise.all( read( methods )( resourceNames ) ).then( () => {
+					expect( methods.post ).not.toHaveBeenCalled();
+					expect( methods.get ).toHaveBeenCalledTimes( 2 );
+					expect( methods.get ).toHaveBeenCalledWith( [ 'orders' ], { page: 1, per_page: 10 } );
+					expect( methods.get ).toHaveBeenCalledWith( [ 'orders' ], { page: 3, per_page: 100 } );
+				} );
+			} );
+		} );
+
+		describe( 'update', () => {
+			it( 'should ignore resourceNames not meant for it.', () => {
+				const { update } = orders.operations;
+				const resourceNames = [ 'ignoreme', 'orders:', 'products-page:' ];
+				Promise.all( update( methods )( resourceNames ) ).then( () => {
+					expect( methods.get ).not.toHaveBeenCalled();
+					expect( methods.post ).not.toHaveBeenCalled();
+				} );
+			} );
+
+			it( 'should request update of multiple orders individually.', () => {
+				const { update } = orders.operations;
+				const resourceNames = [ 'order:6', 'order:42', 'ignoreme' ];
+				const resourceData = {
+					'order:6': { name: 'six' },
+					'order:42': { name: 'forty-two' },
+				};
+				Promise.all( update( methods )( resourceNames, resourceData ) ).then( () => {
+					expect( methods.get ).not.toHaveBeenCalled();
+					expect( methods.post ).toHaveBeenCalledTimes( 2 );
+					expect( methods.post ).toHaveBeenCalledWith( [ 'orders', '6' ], {
+						data: { name: 'six' },
+					} );
+					expect( methods.post ).toHaveBeenCalledWith( [ 'orders', '42' ], {
+						data: { name: 'forty-two' },
+					} );
+				} );
+			} );
+		} );
+	} );
+
+	describe( 'mutations', () => {
+		const operations = {};
+		const updateResult = [
+			Promise.resolve().then( () => ( { id: 5, number: '5', status: 'processing' } ) ),
+		];
+
+		beforeEach( () => {
+			operations.update = jest.fn();
+			operations.update.mockReturnValue( updateResult );
+		} );
+
+		describe( 'updateOrder', () => {
+			it( 'should perform an update operation for an order.', () => {
+				const { updateOrder } = orders.mutations;
+
+				Promise.all( updateOrder( operations )( { id: 5, status: 'processing' } ) ).then( () => {
+					expect( operations.update ).toHaveBeenCalledTimes( 1 );
+					expect( operations.update ).toHaveBeenCalledWith( [ 'order:5' ], {
+						'order:5': { id: 5, status: 'processing' },
+					} );
+				} );
+			} );
+		} );
+
+		describe( 'fulfillOrder', () => {
+			it( 'should perform an update operation for an order.', () => {
+				const { fulfillOrder } = orders.mutations;
+
+				Promise.all( fulfillOrder( operations )( 52 ) ).then( () => {
+					expect( operations.update ).toHaveBeenCalledTimes( 1 );
+					expect( operations.update ).toHaveBeenCalledWith( [ 'order:52' ], {
+						'order:52': { id: 52, status: 'completed' },
+					} );
+				} );
+			} );
+		} );
+	} );
+
+	describe( 'selectors', () => {
+		const { getOrdersPage } = orders.selectors;
+
+		let getData;
+		let requireData;
+
+		beforeEach( () => {
+			getData = jest.fn();
+			requireData = jest.fn();
+		} );
+
+		describe( 'getOrdersPage', () => {
+			it( 'should get and require data for a given orders page.', () => {
+				getData.mockReturnValueOnce( [ 52, 42, 32 ] );
+				getData.mockReturnValueOnce( { id: 52, status: 'processing' } );
+				getData.mockReturnValueOnce( { id: 42, status: 'completed' } );
+				getData.mockReturnValueOnce( { id: 32, status: 'on-hold' } );
+
+				const requirement = { freshness: 5000 };
+
+				const result = getOrdersPage( getData, requireData )( requirement, 1, 3 );
+
+				expect( result ).toEqual( [
+					{ id: 52, status: 'processing' },
+					{ id: 42, status: 'completed' },
+					{ id: 32, status: 'on-hold' },
+				] );
+
+				expect( getData ).toHaveBeenCalledTimes( 4 );
+				expect( getData ).toHaveBeenCalledWith( 'orders-page:{"page":1,"per_page":3}' );
+				expect( getData ).toHaveBeenCalledWith( 'order:52' );
+				expect( getData ).toHaveBeenCalledWith( 'order:42' );
+				expect( getData ).toHaveBeenCalledWith( 'order:32' );
+
+				expect( requireData ).toHaveBeenCalledTimes( 1 );
+				expect( requireData ).toHaveBeenCalledWith(
+					requirement,
+					'orders-page:{"page":1,"per_page":3}'
+				);
+			} );
+
+			it( 'should return an empty array for a page that does not yet exist.', () => {
+				const requirement = { freshness: 5000 };
+
+				const result = getOrdersPage( getData, requireData )( requirement, 1, 3 );
+
+				expect( result ).toEqual( [] );
+
+				expect( getData ).toHaveBeenCalledTimes( 1 );
+				expect( getData ).toHaveBeenCalledWith( 'orders-page:{"page":1,"per_page":3}' );
+
+				expect( requireData ).toHaveBeenCalledTimes( 1 );
+				expect( requireData ).toHaveBeenCalledWith(
+					requirement,
+					'orders-page:{"page":1,"per_page":3}'
+				);
+			} );
+		} );
+	} );
+} );
