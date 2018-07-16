401d97088dac388485dbdce2ed18e7341dc914a8
Justin Shreve <justin.shreve@gmail.com>
2018-07-16 11:28:07 -0400

    Activity Panel Outbound Link

---
+++ client/layout/activity-panel/activity-outbound-link/index.js
@@ -0,0 +1,36 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+import Link from 'components/link';
+
+const ActivityOutboundLink = ( { href, type, className, children } ) => {
+	const classes = classnames( 'woocommerce-layout__activity-panel-outbound-link', className );
+	return (
+		<Link href={ href } type={ type } className={ classes }>
+			{ children }
+			<Gridicon icon="arrow-right" />
+		</Link>
+	);
+};
+
+ActivityOutboundLink.propTypes = {
+	href: PropTypes.string.isRequired,
+	type: PropTypes.oneOf( [ 'wp-admin', 'wc-admin', 'external' ] ).isRequired,
+	className: PropTypes.string,
+};
+
+ActivityOutboundLink.defaultProps = {
+	type: 'wp-admin',
+};
+
+export default ActivityOutboundLink;
