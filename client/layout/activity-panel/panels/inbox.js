1a0abd2c90264b3a70c8f7985dee9a03382c694f
Kelly Dwan <ryelle@users.noreply.github.com>
2018-07-17 10:30:35 -0400

    Mock up an “API response object” for the inbox notices

--- client/layout/activity-panel/panels/inbox.js
+++ client/layout/activity-panel/panels/inbox.js
@@ -3,6 +3,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import Gridicon from 'gridicons';
 
@@ -11,36 +12,96 @@ import Gridicon from 'gridicons';
  */
 import ActivityCard from '../activity-card';
 import ActivityHeader from '../activity-header';
+import { getAdminLink } from 'lib/nav-utils';
 import { Section } from 'layout/section';
 
+const demoNotices = [
+	{
+		id: 1,
+		type: 'informational',
+		title: __( 'Accept Apple Pay using Stripe', 'wc-admin' ),
+		content: __(
+			'Your Stripe payment gateway now allows your customers to pay using Apple Pay.',
+			'wc-admin'
+		),
+		icon: 'customize',
+		status: 'unread',
+		source: 'woocommerce-core',
+		date_created: '2018-07-12T16:23:08Z',
+	},
+	{
+		id: 2,
+		type: 'warning',
+		title: __( 'Extension subscription expired', 'wc-admin' ),
+		content: __(
+			'Your subscription for WooCommerce Subscriptions expired on Jun 7th 2018.',
+			'wc-admin'
+		),
+		icon: 'notice-outline',
+		status: 'read',
+		source: 'woocommerce-core',
+		date_created: '2018-07-11T02:49:00Z',
+	},
+	{
+		id: 3,
+		type: 'informational',
+		title: __( 'Looking for the Store Notice setting?', 'wc-admin' ),
+		content: __( 'It can now be found in the customizer.', 'wc-admin' ),
+		icon: 'info-outline',
+		status: 'read',
+		source: 'woocommerce-core',
+		date_created: '2018-07-10T02:49:00Z',
+		actions: [
+			{
+				id: 1,
+				name: 'store-customizer-setting',
+				label: 'Open Customizer',
+				// @todo What format will links be? Should they default to external/full URLs?
+				// Full URLs that are wc-admin links will force a page refresh though…
+				url: getAdminLink( 'customize.php?autofocus%5Bpanel%5D=woocommerce' ),
+			},
+		],
+	},
+];
+
 class InboxPanel extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = {
+			notices: demoNotices,
+		};
+	}
+
 	render() {
+		const { notices = [] } = this.state;
+		const getButtonsFromActions = actions => {
+			if ( ! actions ) {
+				return [];
+			}
+			return actions.map( action => (
+				<Button isDefault href={ action.url }>
+					{ action.label }
+				</Button>
+			) );
+		};
+
 		return (
 			<Fragment>
 				<ActivityHeader title={ __( 'Inbox', 'wc-admin' ) } />
 				<Section>
-					<ActivityCard
-						className="woocommerce-inbox-activity-card"
-						title={ __( 'Accept Apple Pay using Stripe', 'wc-admin' ) }
-						date={ '2018-07-12T16:23:08Z' }
-						icon={ <Gridicon icon="customize" size={ 48 } /> }
-						unread
-					>
-						{ __(
-							'Your Stripe payment gateway now allows your customers to pay using Apple Pay.',
-							'wc-admin'
-						) }
-					</ActivityCard>
-					<ActivityCard
-						className="woocommerce-inbox-activity-card"
-						title={ __( 'Extension subscription expired', 'wc-admin' ) }
-						date={ '2018-07-11T02:49:00Z' }
-					>
-						{ __(
-							'Your subscription for WooCommerce Subscriptions expired on Jun 7th 2018.',
-							'wc-admin'
-						) }
-					</ActivityCard>
+					{ notices.map( note => (
+						<ActivityCard
+							key={ note.id }
+							className="woocommerce-inbox-activity-card"
+							title={ note.title }
+							date={ note.date_created }
+							icon={ <Gridicon icon={ note.icon } size={ 48 } /> }
+							unread={ 'unread' === note.status }
+							actions={ getButtonsFromActions( note.actions ) }
+						>
+							{ note.content }
+						</ActivityCard>
+					) ) }
 				</Section>
 			</Fragment>
 		);
