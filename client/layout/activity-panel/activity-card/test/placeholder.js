4c6340ddda89c41acdcd988460733759b690c15e
Kelly Dwan <ryelle@users.noreply.github.com>
2018-07-18 11:41:21 -0400

    Update tests for new lines prop

--- client/layout/activity-panel/activity-card/test/placeholder.js
+++ client/layout/activity-panel/activity-card/test/placeholder.js
@@ -11,47 +11,32 @@ import { ActivityCardPlaceholder } from '../';
 
 describe( 'ActivityCardPlaceholder', () => {
 	test( 'should render a default placeholder', () => {
-		const card = shallow(
-			<ActivityCardPlaceholder>
-				<span className="is-placeholder" />
-			</ActivityCardPlaceholder>
-		);
+		const card = shallow( <ActivityCardPlaceholder /> );
 		expect( card ).toMatchSnapshot();
 	} );
 
 	test( 'should render a card placeholder with subtitle placeholder', () => {
-		const card = shallow(
-			<ActivityCardPlaceholder hasSubtitle>
-				<span className="is-placeholder" />
-			</ActivityCardPlaceholder>
-		);
+		const card = shallow( <ActivityCardPlaceholder hasSubtitle /> );
 		expect( card ).toMatchSnapshot();
 	} );
 
 	test( 'should render a card placeholder with date placeholder', () => {
-		const card = shallow(
-			<ActivityCardPlaceholder hasDate>
-				<span className="is-placeholder" />
-			</ActivityCardPlaceholder>
-		);
+		const card = shallow( <ActivityCardPlaceholder hasDate /> );
 		expect( card ).toMatchSnapshot();
 	} );
 
 	test( 'should render a card placeholder with action placeholder', () => {
-		const card = shallow(
-			<ActivityCardPlaceholder hasAction>
-				<span className="is-placeholder" />
-			</ActivityCardPlaceholder>
-		);
+		const card = shallow( <ActivityCardPlaceholder hasAction /> );
 		expect( card ).toMatchSnapshot();
 	} );
 
 	test( 'should render a card placeholder with all optional placeholder', () => {
-		const card = shallow(
-			<ActivityCardPlaceholder hasAction hasDate hasSubtitle>
-				<span className="is-placeholder" />
-			</ActivityCardPlaceholder>
-		);
+		const card = shallow( <ActivityCardPlaceholder hasAction hasDate hasSubtitle /> );
+		expect( card ).toMatchSnapshot();
+	} );
+
+	test( 'should render a card placeholder with multiple lines of content', () => {
+		const card = shallow( <ActivityCardPlaceholder lines={ 3 } /> );
 		expect( card ).toMatchSnapshot();
 	} );
 } );
