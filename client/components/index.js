8661d7a39d8f63f518a619ec0d09d06adf5e9cc4
Kelly Dwan <ryelle@users.noreply.github.com>
2018-08-20 15:51:33 -0400

    Merge simple/complex & alphabetize by first exported name

--- client/components/index.js
+++ client/components/index.js
@@ -6,28 +6,25 @@
 // Turn on react-dates classes/styles, see https://github.com/airbnb/react-dates#initialize
 import 'react-dates/initialize';
 
-// Simple components
+export { AdvancedFilters, DatePicker, FilterPicker, ReportFilters } from './filters';
+export { default as AnimationSlider } from './animation-slider';
 export { default as Card } from './card';
+export { default as Chart } from './chart';
 export { default as Count } from './count';
+export { DateRange } from './calendar';
 export { default as DropdownButton } from './dropdown-button';
+export { EllipsisMenu, MenuItem, MenuTitle } from './ellipsis-menu';
 export { default as Flag } from './flag';
 export { default as Gravatar } from './gravatar';
 export { default as Link } from './link';
 export { default as OrderStatus } from './order-status';
 export { default as Pagination } from './pagination';
 export { default as ProductImage } from './product-image';
+export { default as ProductRating } from './rating/product';
+export { default as Rating } from './rating';
+export { default as ReviewRating } from './rating/review';
 export { default as SegmentedSelection } from './segmented-selection';
 export { default as SplitButton } from './split-button';
-
-// Complex components
-export { default as AnimationSlider } from './animation-slider';
-export { DateRange } from './calendar';
-export { default as Chart } from './chart';
-export { EllipsisMenu, MenuItem, MenuTitle } from './ellipsis-menu';
-export { AdvancedFilters, DatePicker, FilterPicker, ReportFilters } from './filters';
-export { default as useFilters } from './higher-order/use-filters';
-export { default as Rating } from './rating';
-export { default as ProductRating } from './rating/product';
-export { default as ReviewRating } from './rating/review';
 export { SummaryList, SummaryNumber } from './summary';
 export { TableCard, Table, TableSummary } from './table';
+export { default as useFilters } from './higher-order/use-filters';
