ac0cb573ca34ddffb6e53ebf706d34d442c3ede7
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-10-12 18:36:28 +0200

    Move withState to export line so it doesn't break docs generation

--- client/components/tag/index.js
+++ client/components/tag/index.js
@@ -20,73 +20,69 @@ import './style.scss';
  *
  * @return { object } -
  */
-const Tag = withState( {
-	isVisible: false,
-} )(
-	( {
-		id,
-		instanceId,
-		isVisible,
-		label,
-		popoverContents,
-		remove,
-		removeLabel,
-		screenReaderLabel,
-		setState,
-		className,
-	} ) => {
-		screenReaderLabel = screenReaderLabel || label;
-		if ( ! label ) {
-			// A null label probably means something went wrong
-			// @todo Maybe this should be a loading indicator?
-			return null;
-		}
-		const classes = classnames( 'woocommerce-tag', className, {
-			'has-remove': !! remove,
-		} );
-		const labelId = `woocommerce-tag__label-${ instanceId }`;
-		const labelTextNode = (
-			<Fragment>
-				<span className="screen-reader-text">{ screenReaderLabel }</span>
-				<span aria-hidden="true">{ label }</span>
-			</Fragment>
-		);
-
-		return (
-			<span className={ classes }>
-				{ popoverContents ? (
-					<Button
-						className="woocommerce-tag__text"
-						id={ labelId }
-						onClick={ () => setState( () => ( { isVisible: true } ) ) }
-						isToggled={ isVisible }
-					>
-						{ labelTextNode }
-					</Button>
-				) : (
-					<span className="woocommerce-tag__text" id={ labelId }>
-						{ labelTextNode }
-					</span>
-				) }
-				{ popoverContents &&
-					isVisible && (
-						<Popover onClose={ () => setState( () => ( { isVisible: false } ) ) }>
-							{ popoverContents }
-						</Popover>
-					) }
-				{ remove && (
-					<IconButton
-						className="woocommerce-tag__remove"
-						icon={ <Dashicon icon="dismiss" size={ 20 } /> }
-						onClick={ remove( id ) }
-						label={ removeLabel }
-						aria-describedby={ labelId }
-					/>
-				) }
-			</span>
-		);
+const Tag = ( {
+	id,
+	instanceId,
+	isVisible,
+	label,
+	popoverContents,
+	remove,
+	removeLabel,
+	screenReaderLabel,
+	setState,
+	className,
+} ) => {
+	screenReaderLabel = screenReaderLabel || label;
+	if ( ! label ) {
+		// A null label probably means something went wrong
+		// @todo Maybe this should be a loading indicator?
+		return null;
 	}
-);
+	const classes = classnames( 'woocommerce-tag', className, {
+		'has-remove': !! remove,
+	} );
+	const labelId = `woocommerce-tag__label-${ instanceId }`;
+	const labelTextNode = (
+		<Fragment>
+			<span className="screen-reader-text">{ screenReaderLabel }</span>
+			<span aria-hidden="true">{ label }</span>
+		</Fragment>
+	);
+
+	return (
+		<span className={ classes }>
+			{ popoverContents ? (
+				<Button
+					className="woocommerce-tag__text"
+					id={ labelId }
+					onClick={ () => setState( () => ( { isVisible: true } ) ) }
+					isToggled={ isVisible }
+				>
+					{ labelTextNode }
+				</Button>
+			) : (
+				<span className="woocommerce-tag__text" id={ labelId }>
+					{ labelTextNode }
+				</span>
+			) }
+			{ popoverContents &&
+				isVisible && (
+					<Popover onClose={ () => setState( () => ( { isVisible: false } ) ) }>
+						{ popoverContents }
+					</Popover>
+				) }
+			{ remove && (
+				<IconButton
+					className="woocommerce-tag__remove"
+					icon={ <Dashicon icon="dismiss" size={ 20 } /> }
+					onClick={ remove( id ) }
+					label={ removeLabel }
+					aria-describedby={ labelId }
+				/>
+			) }
+		</span>
+	);
+};
 
 Tag.propTypes = {
 	/**
@@ -119,4 +115,6 @@ Tag.defaultProps = {
 	removeLabel: __( 'Remove tag', 'wc-admin' ),
 };
 
-export default withInstanceId( Tag );
+export default withState( {
+	isVisible: false,
+} )( withInstanceId( Tag ) );
