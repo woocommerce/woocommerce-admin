fca0951f1b3ef2dc958b3c1d794769be7f5a028c
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-10-14 18:46:17 +0200

    Use NAMESPACE constant instead of hard-coded value

--- client/store/orders/resolvers.js
+++ client/store/orders/resolvers.js
@@ -9,11 +9,12 @@ import apiFetch from '@wordpress/api-fetch';
  * Internal dependencies
  */
 import { stringifyQuery } from 'lib/nav-utils';
+import { NAMESPACE } from 'store/constants';
 
 export default {
 	async getOrders( state, query ) {
 		try {
-			const orders = await apiFetch( { path: '/wc/v3/orders' + stringifyQuery( query ) } );
+			const orders = await apiFetch( { path: NAMESPACE + 'orders' + stringifyQuery( query ) } );
 			dispatch( 'wc-admin' ).setOrders( orders, query );
 		} catch ( error ) {
 			dispatch( 'wc-admin' ).setOrdersError( query );
