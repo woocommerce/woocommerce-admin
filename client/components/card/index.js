e1b380b04fa35f041cc1cae91b357385693d2215
Jonathan Belcher <belcherj@gmail.com>
2018-10-29 16:12:58 -0400

    Return Card header to previous state

--- client/components/card/index.js
+++ client/components/card/index.js
@@ -10,51 +10,37 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import './style.scss';
-import EllipsisMenu from 'components/ellipsis-menu';
-import { Section } from 'components/section';
-import { validateComponent } from 'lib/proptype-validator';
-import SectionHeader from 'components/section-header';
+import { EllipsisMenu } from '../ellipsis-menu';
 
-/**
- * A basic card component with a header. The header can contain a title, an action, and an `EllipsisMenu` menu.
- */
 class Card extends Component {
 	render() {
 		const { action, children, menu, title } = this.props;
-		const className = classnames( 'woocommerce-card', this.props.className, {
+		const className = classnames( 'woo-dash__card', this.props.className, {
 			'has-menu': !! menu,
 			'has-action': !! action,
 		} );
 		return (
 			<div className={ className }>
 				{ title && (
-					<SectionHeader title={ title } menu={ menu }>
-						{ action }
-					</SectionHeader>
+					<div className="woo-dash__card-header">
+						<h2 className="woo-dash__card-title">{ title }</h2>
+						{ action && <div className="woo-dash__card-action">{ action }</div> }
+						{ menu && <div className="woo-dash__card-menu">{ menu }</div> }
+					</div>
 				) }
-				<Section className="woocommerce-card__body">{ children }</Section>
+				<div className="woo-dash__card-body">{ children }</div>
 			</div>
 		);
 	}
 }
 
 Card.propTypes = {
-	/**
-	 * One "primary" action for this card, appears in the card header.
-	 */
 	action: PropTypes.node,
-	/**
-	 * Additional CSS classes.
-	 */
 	className: PropTypes.string,
-	/**
-	 * An `EllipsisMenu`, with filters used to control the content visible in this card
-	 */
-	menu: validateComponent( EllipsisMenu ),
-	/**
-	 * The title to use for this card.
-	 */
-	title: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
+	menu: PropTypes.shape( {
+		type: PropTypes.oneOf( [ EllipsisMenu ] ),
+	} ),
+	title: PropTypes.string,
 };
 
 export default Card;
