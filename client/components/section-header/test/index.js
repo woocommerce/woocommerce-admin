a3717a2343858e5c495316b9ef43e04b4b02670a
Jonathan Belcher <belcherj@gmail.com>
2018-10-30 13:37:02 -0400

    Add snapshot to test

--- client/components/section-header/test/index.js
+++ client/components/section-header/test/index.js
@@ -3,6 +3,8 @@
  *
  * @format
  */
+import React from 'react';
+import renderer from 'react-test-renderer';
 
 /**
  * Internal dependencies
@@ -13,5 +15,10 @@ describe( 'SectionHeader', () => {
 	test( 'should have correct title', () => {
 		const sectionHeader = <SectionHeader title="A SectionHeader Example" />;
 		expect( sectionHeader.props.title ).toBe( 'A SectionHeader Example' );
+	} );
+
+	test( 'it renders correctly', () => {
+		const tree = renderer.create( <SectionHeader title="A SectionHeader Example" /> ).toJSON();
+		expect( tree ).toMatchSnapshot();
 	} );
 } );
