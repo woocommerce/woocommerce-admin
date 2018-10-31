cb7b655b4b4d1028c42a0bbfa47a2b228bed47f7
Peter Fabian <peter.fabian.github@gmail.com>
2018-10-31 15:50:24 +0100

    Moved JS tests to its own shell script which is run optionally.

--- bin/travis.sh
+++ bin/travis.sh
@@ -3,10 +3,17 @@
 
 if [ $1 == 'before' ]; then
 
-	composer global require "phpunit/phpunit=6.*"
+	# composer global require "phpunit/phpunit=6.*"
 
 	if [[ ${RUN_PHPCS} == 1 ]]; then
 		composer install
+	fi
+
+	if [[ ${RUN_JS} == 1 ]]; then
+		sudo rm -rf ~/.nvm
+  		curl -sL "https://deb.nodesource.com/setup_${NODE_RELEASE}" | sudo -E bash -
+  		sudo apt-get install -y nodejs
+  		npm install
 	fi
 
 fi
