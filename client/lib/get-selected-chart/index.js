4e4927c693ecdaf754d9b017ad96675c6e51a264
Jonathan Belcher <belcherj@gmail.com>
2018-11-07 12:15:35 -0500

    Clarify docs

--- client/lib/get-selected-chart/index.js
+++ client/lib/get-selected-chart/index.js
@@ -6,7 +6,8 @@
 import { find } from 'lodash';
 
 /**
- * Takes a chart name returns the configuration for that chart from and array of charts
+ * Takes a chart name returns the configuration for that chart from and array
+ * of charts. If the chart is not found it will return the first chart.
  *
  * @param {string} chartName - the name of the chart to get configuration for
  * @param {array} charts - list of charts for a particular report
