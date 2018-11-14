6494a07719496bc1dc0e01c73f8dd301377ab8b2
Jonathan Belcher <belcherj@gmail.com>
2018-11-14 10:37:18 -0500

    Update to latest method for creating summary/chart

--- client/analytics/report/taxes/config.js
+++ client/analytics/report/taxes/config.js
@@ -7,6 +7,24 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 
+export const charts = [
+	{
+		key: 'gross_discount',
+		label: __( 'Gross Discount', 'wc-admin' ),
+		type: 'currency',
+	},
+	{
+		key: 'taxes_count',
+		label: __( 'Taxes Count', 'wc-admin' ),
+		type: 'number',
+	},
+	{
+		key: 'orders_count',
+		label: __( 'Orders Count', 'wc-admin' ),
+		type: 'number',
+	},
+];
+
 export const filters = [
 	{
 		label: __( 'Show', 'wc-admin' ),
--- client/analytics/report/taxes/index.js
+++ client/analytics/report/taxes/index.js
@@ -4,19 +4,38 @@
  */
 import { Component, Fragment } from '@wordpress/element';
 import PropTypes from 'prop-types';
+
+/**
+ * WooCommerce dependencies
+ */
+import { ReportFilters } from '@woocommerce/components';
+
 /**
  * Internal dependencies
  */
-import { filters } from './config';
-import { ReportFilters } from '@woocommerce/components';
-import TaxesReportChart from './chart';
+import { charts, filters } from './config';
+import getSelectedChart from 'lib/get-selected-chart';
+import ReportChart from 'analytics/components/report-chart';
+import ReportSummary from 'analytics/components/report-summary';
 export default class TaxesReport extends Component {
 	render() {
 		const { query, path } = this.props;
 		return (
 			<Fragment>
 				<ReportFilters query={ query } path={ path } filters={ filters } />
-				<TaxesReportChart query={ query } path={ path } />
+				<ReportSummary
+					charts={ charts }
+					endpoint="taxes"
+					query={ query }
+					selectedChart={ getSelectedChart( query.chart, charts ) }
+				/>
+				<ReportChart
+					charts={ charts }
+					endpoint="taxes"
+					path={ path }
+					query={ query }
+					selectedChart={ getSelectedChart( query.chart, charts ) }
+				/>
 			</Fragment>
 		);
 	}
