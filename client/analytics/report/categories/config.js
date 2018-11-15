f4951e879189cbba877ca5711f918c2e39f8c814
Jonathan Belcher <belcherj@gmail.com>
2018-11-15 13:10:21 -0500

    Final touch ups

--- client/analytics/report/categories/config.js
+++ client/analytics/report/categories/config.js
@@ -4,12 +4,6 @@
  */
 import { __ } from '@wordpress/i18n';
 
-/**
- * Internal dependencies
- */
-import { getRequestByIdString } from 'lib/async-requests';
-import { NAMESPACE } from 'store/constants';
-
 export const charts = [
 	{
 		key: 'orders_count',
@@ -34,21 +28,14 @@ export const charts = [
 ];
 
 export const filters = [
-	{ label: __( 'All Categories', 'wc-admin' ), value: 'all' },
 	{
-		label: __( 'Comparison', 'wc-admin' ),
-		value: 'compare',
-		settings: {
-			type: 'categories',
-			param: 'category',
-			getLabels: getRequestByIdString( NAMESPACE + 'categories', category => ( {
-				id: category.id,
-				label: category.code,
-			} ) ),
-			labels: {
-				title: __( 'Compare Category Codes', 'wc-admin' ),
-				update: __( 'Compare', 'wc-admin' ),
-			},
-		},
+		label: __( 'Show', 'wc-admin' ),
+		staticParams: [ 'chart' ],
+		param: 'filter',
+		showFilters: () => true,
+		filters: [
+			{ label: __( 'All Categories', 'wc-admin' ), value: 'all' },
+			{ label: __( 'Advanced Filters', 'wc-admin' ), value: 'advanced' },
+		],
 	},
 ];
