61b972aca8e74a3c1aa114e6de44a13e301a3027
Jonathan Belcher <belcherj@gmail.com>
2018-11-16 08:38:58 -0500

    Seperate WooCommerce deps

--- client/analytics/report/coupons/index.js
+++ client/analytics/report/coupons/index.js
@@ -6,12 +6,16 @@ import { Component, Fragment } from '@wordpress/element';
 import PropTypes from 'prop-types';
 
 /**
+ * WooCommerce dependencies
+ */
+import { ReportFilters } from '@woocommerce/components';
+
+/**
  * Internal dependencies
  */
 import { charts, filters } from './config';
 import CouponsReportTable from './table';
 import getSelectedChart from 'lib/get-selected-chart';
-import { ReportFilters } from '@woocommerce/components';
 import ReportChart from 'analytics/components/report-chart';
 import ReportSummary from 'analytics/components/report-summary';
 
