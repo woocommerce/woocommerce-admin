8d2c292c0761a85d01cf6a1530113bd1ff46baba
Jonathan Belcher <belcherj@gmail.com>
2018-11-19 11:11:40 -0500

    Make snapshots better

--- packages/components/src/tag/test/index.js
+++ packages/components/src/tag/test/index.js
@@ -12,29 +12,24 @@ import Tag from '../';
 const noop = () => {};
 
 describe( 'Tag', () => {
-	test( 'it renders correctly', () => {
+	test( '<Tag label="foo" /> should render a tag with the label foo', () => {
 		const tree = renderer.create( <Tag label="foo" /> ).toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
-	test( 'it renders correctly with id', () => {
-		const tree = renderer.create( <Tag label="foo" id={ 1 } /> ).toJSON();
-		expect( tree ).toMatchSnapshot();
-	} );
-
-	test( 'it renders correctly with remove', () => {
+	test( '<Tag label="foo" remove={ noop } /> should render a tag with a close button', () => {
 		const tree = renderer.create( <Tag label="foo" remove={ noop } /> ).toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
-	test( 'it renders correctly with popover', () => {
+	test( '<Tag label="foo" popoverContents={ <p>This is a popover</p> } /> should render a tag with a popover', () => {
 		const tree = renderer
 			.create( <Tag label="foo" popoverContents={ <p>This is a popover</p> } /> )
 			.toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
-	test( 'it renders correctly with popover', () => {
+	test( '<Tag label="foo" screenReaderLabel="FooBar" /> should render a tag with a screen reader label', () => {
 		const tree = renderer.create( <Tag label="foo" screenReaderLabel="FooBar" /> ).toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
