awais300/wc-admin

Create Stock report

client/analytics/report/stock/index.js
@@ -1,0 +1,39 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { Component, Fragment } from '@wordpress/element';
+import PropTypes from 'prop-types';
+
+/**
+ * WooCommerce dependencies
+ */
+import { ReportFilters } from '@woocommerce/components';
+
+/**
+ * Internal dependencies
+ */
+import { dateRangeFilter, filters } from './config';
+import StockReportTable from './table';
+
+export default class CouponsReport extends Component {
+	render() {
+		const { query, path } = this.props;
+
+		return (
+			<Fragment>
+				<ReportFilters
+					query={ query }
+					path={ path }
+					dateRangeFilter={ dateRangeFilter }
+					filters={ filters }
+				/>
+				<StockReportTable query={ query } />
+			</Fragment>
+		);
+	}
+}
+
+CouponsReport.propTypes = {
+	query: PropTypes.object.isRequired,
+};

woocommerce/woocommerce-admin

client/analytics/report/stock/index.js
@@ -1,0 +1,39 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { Component, Fragment } from '@wordpress/element';
+import PropTypes from 'prop-types';
+
+/**
+ * WooCommerce dependencies
+ */
+import { ReportFilters } from '@woocommerce/components';
+
+/**
+ * Internal dependencies
+ */
+import { showDatePicker, filters } from './config';
+import StockReportTable from './table';
+
+export default class CouponsReport extends Component {
+	render() {
+		const { query, path } = this.props;
+
+		return (
+			<Fragment>
+				<ReportFilters
+					query={ query }
+					path={ path }
+					showDatePicker={ showDatePicker }
+					filters={ filters }
+				/>
+				<StockReportTable query={ query } />
+			</Fragment>
+		);
+	}
+}
+
+CouponsReport.propTypes = {
+	query: PropTypes.object.isRequired,
+};