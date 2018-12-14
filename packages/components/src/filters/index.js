c0cdd95e8129a436017623afc793cae4456c60da
Albert Juhé Lluveras <contact@albertjuhe.com>
2018-12-14 18:17:42 +0100

    Remove dateRangeFilter.show from ReportFilters

--- packages/components/src/filters/index.js
+++ packages/components/src/filters/index.js
@@ -92,15 +92,6 @@ ReportFilters.propTypes = {
 	 */
 	advancedFilters: PropTypes.object,
 	/**
-	 * Config options for the date range picker.
-	 */
-	dateRangeFilter: PropTypes.shape( {
-		/**
-		 * Whether the date range filter must be shown.
-		 */
-		show: PropTypes.bool,
-	} ),
-	/**
 	 * Config option passed through to `FilterPicker` - if not used, `FilterPicker` is not displayed.
 	 */
 	filters: PropTypes.array,
@@ -120,9 +111,6 @@ ReportFilters.propTypes = {
 
 ReportFilters.defaultProps = {
 	advancedFilters: {},
-	dateRangeFilter: {
-		show: true,
-	},
 	filters: [],
 	query: {},
 	showDatePicker: true,
