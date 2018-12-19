cea24739cb4bcf755bc4d4b91c295ffd2c2227c1
Joshua Flowers <joshuatf@gmail.com>
2018-12-19 16:16:46 +0800

    Add category breadcrumbs component

--- client/analytics/report/categories/table.js
+++ client/analytics/report/categories/table.js
@@ -4,21 +4,23 @@
  */
 import { __, _n } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
 import { map } from 'lodash';
 
 /**
  * WooCommerce dependencies
  */
-import { Link } from '@woocommerce/components';
 import { formatCurrency, getCurrencyFormatDecimal } from '@woocommerce/currency';
 
 /**
  * Internal dependencies
  */
-import ReportTable from 'analytics/components/report-table';
+import CategoryBreacrumbs from './breadcrumbs';
 import { numberFormat } from 'lib/number';
+import ReportTable from 'analytics/components/report-table';
+import withSelect from 'wc-api/with-select';
 
-export default class CategoriesReportTable extends Component {
+class CategoriesReportTable extends Component {
 	constructor( props ) {
 		super( props );
 
@@ -64,29 +66,16 @@ export default class CategoriesReportTable extends Component {
 		];
 	}
 
-	getRowsContent( categories ) {
-		return map( categories, category => {
-			const {
-				category_id,
-				items_sold,
-				net_revenue,
-				products_count,
-				orders_count,
-				extended_info,
-			} = category;
-			const { name } = extended_info;
+	getRowsContent( categoryStats ) {
+		return map( categoryStats, categoryStat => {
+			const { category_id, items_sold, net_revenue, products_count, orders_count } = categoryStat;
+			const categories = this.props.categories;
+			const category = categories[ category_id ];
 
 			return [
 				{
-					display: (
-						<Link
-							href={ 'term.php?taxonomy=product_cat&post_type=product&tag_ID=' + category_id }
-							type="wp-admin"
-						>
-							{ name }
-						</Link>
-					),
-					value: name,
+					display: <CategoryBreacrumbs category={ category } categories={ categories } />,
+					value: category && category.name,
 				},
 				{
 					display: numberFormat( items_sold ),
@@ -156,3 +145,18 @@ export default class CategoriesReportTable extends Component {
 		);
 	}
 }
+
+export default compose(
+	withSelect( select => {
+		const { getCategories, getCategoriesError, isGetCategoriesRequesting } = select( 'wc-api' );
+		const categoriesQuery = {
+			per_page: 100,
+		};
+
+		const categories = getCategories( categoriesQuery );
+		const isError = Boolean( getCategoriesError( categoriesQuery ) );
+		const isRequesting = isGetCategoriesRequesting( categoriesQuery );
+
+		return { categories, isError, isRequesting };
+	} )
+)( CategoriesReportTable );
