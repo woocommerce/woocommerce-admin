96b12e9b1f8a7142f63e7707d22396a54df95376
Joshua Flowers <joshuatf@gmail.com>
2019-01-02 12:01:37 +0800

    Remove state derived props

--- client/dashboard/dashboard-charts/index.js
+++ client/dashboard/dashboard-charts/index.js
@@ -5,7 +5,6 @@
 import { __ } from '@wordpress/i18n';
 import classNames from 'classnames';
 import Gridicon from 'gridicons';
-import { isEqual } from 'lodash';
 import { ToggleControl, IconButton, NavigableMenu } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import PropTypes from 'prop-types';
@@ -23,24 +22,14 @@ import { getChartFromKey, showCharts } from './config';
 import './style.scss';
 
 class DashboardCharts extends Component {
-	constructor( props ) {
+	constructor() {
 		super( ...arguments );
 		this.state = {
+			chartType: 'line', // @TODO: Remove this and use from props containing persisted user preferences.
 			showCharts,
-			query: props.query,
 		};
 
 		this.toggle = this.toggle.bind( this );
-	}
-
-	static getDerivedStateFromProps( props, state ) {
-		const { query } = props;
-		if ( ! isEqual( { ...query, ...state.type }, state.query ) ) {
-			return {
-				query: { ...query, ...state.type },
-			};
-		}
-		return null;
 	}
 
 	toggle( key ) {
@@ -55,7 +44,9 @@ class DashboardCharts extends Component {
 
 	handleTypeToggle( type ) {
 		return () => {
-			this.setState( state => ( { query: { ...state.query, type } } ) );
+			this.setState( {
+				chartType: type,
+			} );
 		};
 	}
 
@@ -79,7 +70,7 @@ class DashboardCharts extends Component {
 
 	render() {
 		const { path } = this.props;
-		const { query } = this.state;
+		const query = { ...this.props.query, type: this.state.chartType };
 		return (
 			<Fragment>
 				<div className="woocommerce-dashboard__dashboard-charts">
