9a8210a18197de3b222fa668087d5700814d297c
Joshua Flowers <joshuatf@gmail.com>
2019-01-04 18:15:51 +0800

    Add dashboard chart links to reports

--- client/dashboard/dashboard-charts/block.js
+++ client/dashboard/dashboard-charts/block.js
@@ -2,21 +2,33 @@
 /**
  * External dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 import PropTypes from 'prop-types';
 
 /**
  * WooCommerce dependencies
  */
 import { Card } from '@woocommerce/components';
+import { getAdminLink, history } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
  */
 import ReportChart from 'analytics/components/report-chart';
 import './block.scss';
+import { __ } from '@wordpress/i18n';
 
 class ChartBlock extends Component {
+	handleChartClick = () => {
+		const { charts } = this.props;
+
+		if ( ! charts || ! charts.length ) {
+			return null;
+		}
+
+		history.push( 'analytics/' + charts[ 0 ].endpoint + '?chart=' + charts[ 0 ].key );
+	};
+
 	render() {
 		const { charts, endpoint, path, query } = this.props;
 
@@ -25,8 +37,28 @@ class ChartBlock extends Component {
 		}
 
 		return (
-			<Fragment>
-				<Card className="woocommerce-dashboard__chart-block" title={ charts[ 0 ].label }>
+			<div
+				role="presentation"
+				className="woocommerce-dashboard__chart-block-wrapper"
+				onClick={ this.handleChartClick }
+			>
+				<Card
+					className="woocommerce-dashboard__chart-block"
+					title={ charts[ 0 ].label }
+					onClick={ this.handleChartClick }
+				>
+					<a
+						className="woocommerce-dashboard__chart-block screen-reader-text"
+						href={ getAdminLink(
+							'admin.php?page=wc-admin#/analytics/' +
+								charts[ 0 ].endpoint +
+								'?chart=' +
+								charts[ 0 ].key
+						) }
+					>
+						{ /* translators: appended to end of chart type for report link */
+						charts[ 0 ].label + __( ' Report', 'wc-admin' ) }
+					</a>
 					<ReportChart
 						charts={ charts }
 						endpoint={ endpoint }
@@ -36,7 +68,7 @@ class ChartBlock extends Component {
 						selectedChart={ charts[ 0 ] }
 					/>
 				</Card>
-			</Fragment>
+			</div>
 		);
 	}
 }
