27dd2bb7fbdc24633842540d7e1fe10185596840
Joshua Flowers <joshuatf@gmail.com>
2019-01-14 16:12:50 +0800

    Hydrate initial state through localized script

--- client/wc-api/user/selectors.js
+++ client/wc-api/user/selectors.js
@@ -8,7 +8,8 @@ import { DEFAULT_REQUIREMENT } from '../constants';
 const getCurrentUserData = ( getResource, requireResource ) => (
 	requirement = DEFAULT_REQUIREMENT
 ) => {
-	return requireResource( requirement, 'current-user-data' ).data || {};
+	const initalState = wcAdminServer.currentUserData; // eslint-disable-line
+	return requireResource( requirement, 'current-user-data' ).data || initalState;
 };
 
 export default {
