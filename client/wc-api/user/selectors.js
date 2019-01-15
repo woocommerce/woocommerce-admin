aad9413c1959e68c202d4a52ba66d8b02ae91d4f
Joshua Flowers <joshuatf@gmail.com>
2019-01-15 10:00:00 +0800

    Change eslint ignore to undef only

--- client/wc-api/user/selectors.js
+++ client/wc-api/user/selectors.js
@@ -8,7 +8,7 @@ import { DEFAULT_REQUIREMENT } from '../constants';
 const getCurrentUserData = ( getResource, requireResource ) => (
 	requirement = DEFAULT_REQUIREMENT
 ) => {
-	const initalState = wcAdminServer.currentUserData; // eslint-disable-line
+	const initalState = wcAdminServer.currentUserData; // eslint-disable-line no-undef
 	return requireResource( requirement, 'current-user-data' ).data || initalState;
 };
 
