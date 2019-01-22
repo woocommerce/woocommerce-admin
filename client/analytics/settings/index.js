059567c82f1d6e1a7b9351616f7f70a68d5b4bef
Joshua Flowers <joshuatf@gmail.com>
2019-01-22 17:41:23 +0800

    Extract setting as separate component

--- client/analytics/settings/index.js
+++ client/analytics/settings/index.js
@@ -6,7 +6,7 @@ import { __, sprintf } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
-import { remove, uniqueId } from 'lodash';
+import { remove } from 'lodash';
 import { withDispatch } from '@wordpress/data';
 
 /**
@@ -19,7 +19,7 @@ import { SectionHeader, useFilters } from '@woocommerce/components';
  */
 import './style.scss';
 import Header from 'header';
-import sanitizeHTML from 'lib/sanitize-html';
+import Setting from './setting';
 
 const SETTINGS_FILTER = 'woocommerce_admin_settings_page';
 
@@ -29,10 +29,13 @@ class Settings extends Component {
 
 		this.state = {
 			hasError: false,
-			excludedStatuses: wcSettings.wcAdminSettings.woocommerce_excluded_report_order_statuses || [],
+			settings: {
+				woocommerce_excluded_report_order_statuses:
+					wcSettings.wcAdminSettings.woocommerce_excluded_report_order_statuses || [],
+			},
 		};
 
-		this.toggleExcludedStatus = this.toggleExcludedStatus.bind( this );
+		this.handleInputChange = this.handleInputChange.bind( this );
 	}
 
 	componentDidCatch( error ) {
@@ -44,49 +47,28 @@ class Settings extends Component {
 		/* eslint-enable no-console */
 	}
 
-	toggleExcludedStatus = e => {
-		const status = e.target.value;
-		const excludedStatuses = this.state.excludedStatuses;
-		if ( e.target.checked ) {
-			excludedStatuses.push( status );
-		} else {
-			remove( excludedStatuses, value => value === status );
-		}
-		this.setState( { excludedStatuses } );
-	};
-
 	resetDefaults = () => {};
 
 	saveChanges = () => {
-		this.props.updateSettings( {
-			woocommerce_excluded_report_order_statuses: this.state.excludedStatuses.join( ',' ),
-		} );
+		this.props.updateSettings( this.state.settings );
 		// @TODO: Need a confirmation on successful update.
 	};
 
-	getOrderStatusOptions() {
-		const orderStatuses = wcSettings.orderStatuses;
-		const { excludedStatuses } = this.state;
+	handleInputChange( e ) {
+		const { checked, name, type, value } = e.target;
+		const { settings } = this.state;
 
-		return Object.keys( orderStatuses ).map( key => {
-			const id = uniqueId( 'woocommerce-settings__excluded-status-' );
-			return (
-				<label htmlFor={ id } key={ key }>
-					<input
-						id={ id }
-						type="checkbox"
-						onChange={ this.toggleExcludedStatus }
-						aria-label={ sprintf(
-							__( 'Exclude the %s status from reports', 'wc-admin' ),
-							status.name
-						) }
-						checked={ excludedStatuses.includes( key ) }
-						value={ key }
-					/>
-					{ orderStatuses[ key ] }
-				</label>
-			);
-		} );
+		if ( 'checkbox' === type && Array.isArray( settings[ name ] ) ) {
+			if ( checked ) {
+				settings[ name ].push( value );
+			} else {
+				remove( settings[ name ], v => v === value );
+			}
+		} else {
+			settings[ name ] = value;
+		}
+
+		this.setState( { settings } );
 	}
 
 	render() {
@@ -95,7 +77,7 @@ class Settings extends Component {
 			return null;
 		}
 
-		const help = sprintf(
+		const helpText = sprintf(
 			__(
 				'Orders with these statuses are excluded from the totals in your reports.' +
 					'The <strong>Refunded</strong> status can not be excluded.  <a href="%s">Learn more</a>',
@@ -104,6 +86,17 @@ class Settings extends Component {
 			'#',
 			'wc-admin' // @TODO: this needs to be replaced with a real link
 		);
+
+		const orderStatuses = Object.keys( wcSettings.orderStatuses ).map( key => {
+			return {
+				value: key,
+				label: wcSettings.orderStatuses[ key ],
+				description: sprintf(
+					__( 'Exclude the %s status from reports', 'wc-admin' ),
+					wcSettings.orderStatuses[ key ]
+				),
+			};
+		} );
 
 		return (
 			<Fragment>
@@ -114,16 +107,15 @@ class Settings extends Component {
 					] }
 				/>
 				<SectionHeader title={ __( 'Analytics Settings', 'wc-admin' ) } />
-				<div className="woocommerce-settings__setting-name">
-					{ __( 'Excluded Statuses', 'wc-admin' ) }
-				</div>
-				<div className="woocommerce-settings__setting-options">
-					{ this.getOrderStatusOptions() }
-					<span
-						className="woocommerce-settings__setting-help"
-						dangerouslySetInnerHTML={ sanitizeHTML( help ) }
-					/>
-				</div>
+				<Setting
+					label={ __( 'Excluded Statuses', 'wc-admin' ) }
+					options={ orderStatuses }
+					helpText={ helpText }
+					inputType="checkbox"
+					name="woocommerce_excluded_report_order_statuses"
+					handleChange={ this.handleInputChange }
+					value={ wcSettings.wcAdminSettings.woocommerce_excluded_report_order_statuses }
+				/>
 				<div className="woocommerce-settings__actions">
 					<Button isDefault onClick={ this.resetDefaults }>
 						{ __( 'Reset Defaults', 'wc-admin' ) }
