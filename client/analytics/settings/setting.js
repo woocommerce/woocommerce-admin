81c2ce7c47c3c0a89f9fcd64e4d9505c69f7f025
Joshua Flowers <joshuatf@gmail.com>
2019-01-22 19:03:30 +0800

    Add checkboxGroup option as input type

--- client/analytics/settings/setting.js
+++ client/analytics/settings/setting.js
@@ -13,34 +13,51 @@ import sanitizeHTML from 'lib/sanitize-html';
 export default class Settings extends Component {
 	renderInput = () => {
 		const { handleChange, name, inputType, options, value } = this.props;
-		let id = uniqueId( name );
+		const id = uniqueId( name );
 
 		switch ( inputType ) {
+			case 'checkboxGroup':
+				const test = options.map( optionGroup => (
+					<div className="woocommerce-settings__setting-option-group" key={ optionGroup.key }>
+						{ optionGroup.label && (
+							<span className="woocommerce-settings__setting-option-group-label">
+								{ optionGroup.label }
+							</span>
+						) }
+						{ this.renderCheckboxOptions( optionGroup.options ) }
+					</div>
+				) );
+				return test;
 			case 'checkbox':
-				return options.map( option => {
-					id = id + '-' + option.value;
-					return (
-						<label htmlFor={ id } key={ option.value }>
-							<input
-								id={ id }
-								type="checkbox"
-								name={ name }
-								onChange={ handleChange }
-								aria-label={ option.description }
-								checked={ value && value.includes( option.value ) }
-								value={ option.value }
-							/>
-							{ option.label }
-						</label>
-					);
-				} );
-
+				return this.renderCheckboxOptions( options );
 			default:
 				return (
 					<input id={ id } type="text" name={ name } onChange={ handleChange } value={ value } />
 				);
 		}
 	};
+
+	renderCheckboxOptions( options ) {
+		const { handleChange, name, value } = this.props;
+
+		return options.map( option => {
+			const id = uniqueId( name + '-' + option.value );
+			return (
+				<label htmlFor={ id } key={ option.value }>
+					<input
+						id={ id }
+						type="checkbox"
+						name={ name }
+						onChange={ handleChange }
+						aria-label={ option.description }
+						checked={ value && value.includes( option.value ) }
+						value={ option.value }
+					/>
+					{ option.label }
+				</label>
+			);
+		} );
+	}
 
 	render() {
 		const { helpText, label } = this.props;
