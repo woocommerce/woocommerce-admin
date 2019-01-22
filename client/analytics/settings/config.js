8eee99e9400ab192e781d1e84a01856713837f04
Joshua Flowers <joshuatf@gmail.com>
2019-01-22 19:03:50 +0800

    Separate status types into default and custom groups

--- client/analytics/settings/config.js
+++ client/analytics/settings/config.js
@@ -4,21 +4,46 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 
+const defaultOrderStatuses = [
+	'completed',
+	'processing',
+	'refunded',
+	'cancelled',
+	'failed',
+	'pending',
+	'on-hold',
+];
+const orderStatuses = Object.keys( wcSettings.orderStatuses )
+	.filter( status => status !== 'refunded' )
+	.map( key => {
+		return {
+			value: key,
+			label: wcSettings.orderStatuses[ key ],
+			description: sprintf(
+				__( 'Exclude the %s status from reports', 'wc-admin' ),
+				wcSettings.orderStatuses[ key ]
+			),
+		};
+	} );
+
 export const analyticsSettings = [
 	{
 		name: 'woocommerce_excluded_report_order_statuses',
 		label: __( 'Excluded Statuses', 'wc-admin' ),
-		inputType: 'checkbox',
-		options: Object.keys( wcSettings.orderStatuses ).map( key => {
-			return {
-				value: key,
-				label: wcSettings.orderStatuses[ key ],
-				description: sprintf(
-					__( 'Exclude the %s status from reports', 'wc-admin' ),
-					wcSettings.orderStatuses[ key ]
+		inputType: 'checkboxGroup',
+		options: [
+			{
+				key: 'defaultStatuses',
+				options: orderStatuses.filter(
+					status => defaultOrderStatuses.includes( status.value ) && 'refunded' !== status.value
 				),
-			};
-		} ),
+			},
+			{
+				key: 'customStatuses',
+				label: __( 'Custom Statuses', 'wc-admin' ),
+				options: orderStatuses.filter( status => ! defaultOrderStatuses.includes( status.value ) ),
+			},
+		],
 		helpText: sprintf(
 			__(
 				'Orders with these statuses are excluded from the totals in your reports.' +
