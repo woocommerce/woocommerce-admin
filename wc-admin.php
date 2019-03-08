1ca9bf191d8df950b57de4aef070cf8787bd748b
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-03-08 09:00:55 -0700

    Check to make sure we’re overriding only AS’s built in WP Post data store.

--- wc-admin.php
+++ wc-admin.php
@@ -185,9 +185,15 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 /**
  * Filter in our ActionScheduler Store class.
  *
+ * @param string $store_class ActionScheduler Store class name.
  * @return string ActionScheduler Store class name.
  */
-function wc_admin_set_actionscheduler_store_class() {
+function wc_admin_set_actionscheduler_store_class( $store_class ) {
+	// Don't override any other overrides.
+	if ( 'ActionScheduler_wpPostStore' !== $store_class ) {
+		return $store_class;
+	}
+
 	// Include our store class here instead of wc_admin_plugins_loaded()
 	// because ActionScheduler is hooked into `plugins_loaded` at a
 	// much higher priority.
