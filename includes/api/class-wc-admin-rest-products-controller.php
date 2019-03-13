24bf02e5e617641c927444de84868a9882e4f572
Joshua Flowers <joshuatf@gmail.com>
2019-03-13 14:53:41 +0800

    Remove product_name search in favor of sku and name searching

--- includes/api/class-wc-admin-rest-products-controller.php
+++ includes/api/class-wc-admin-rest-products-controller.php
@@ -55,12 +55,7 @@ class WC_Admin_REST_Products_Controller extends WC_REST_Products_Controller {
 	 * @return array
 	 */
 	public function get_collection_params() {
-		$params                 = parent::get_collection_params();
-		$params['product_name'] = array(
-			'description'       => __( 'Search for a similar product name.', 'woocommerce-admin' ),
-			'type'              => 'string',
-			'validate_callback' => 'rest_validate_request_arg',
-		);
+		$params                   = parent::get_collection_params();
 		$params['product_search'] = array(
 			'description'       => __( 'Search by similar product name or sku.', 'woocommerce-admin' ),
 			'type'              => 'string',
@@ -78,10 +73,6 @@ class WC_Admin_REST_Products_Controller extends WC_REST_Products_Controller {
 	protected function prepare_objects_query( $request ) {
 		$args = parent::prepare_objects_query( $request );
 
-		if ( ! empty( $request['product_name'] ) ) {
-			$args['post_title__like'] = $request['product_name'];
-		}
-
 		if ( ! empty( $request['product_search'] ) ) {
 			$args['product_search'] = $request['product_search'];
 		}
@@ -96,34 +87,12 @@ class WC_Admin_REST_Products_Controller extends WC_REST_Products_Controller {
 	 * @return WP_Error|WP_REST_Response
 	 */
 	public function get_items( $request ) {
-		add_filter( 'posts_where', array( __CLASS__, 'add_wp_query_post_title_filter' ), 10, 2 );
 		add_filter( 'posts_where', array( __CLASS__, 'add_wp_query_product_search_filter' ), 10, 2 );
 		add_filter( 'posts_join', array( __CLASS__, 'add_wp_query_product_search_join' ), 10, 2 );
 		$response = parent::get_items( $request );
-		remove_filter( 'posts_where', array( __CLASS__, 'add_wp_query_post_title_filter' ), 10 );
 		remove_filter( 'posts_where', array( __CLASS__, 'add_wp_query_product_search_filter' ), 10 );
 		remove_filter( 'posts_join', array( __CLASS__, 'add_wp_query_product_search_join' ), 10 );
 		return $response;
-	}
-
-	/**
-	 * Add post title searching to WP Query
-	 *
-	 * @param string $where Where clause used to search posts.
-	 * @param object $wp_query WP_Query object.
-	 * @return string
-	 */
-	public static function add_wp_query_post_title_filter( $where, $wp_query ) {
-		global $wpdb;
-
-		$post_title_search = $wp_query->get( 'post_title__like' );
-		if ( $post_title_search ) {
-			$post_title_search = $wpdb->esc_like( $post_title_search );
-			$post_title_search = ' \'%' . $post_title_search . '%\'';
-			$where            .= ' AND ' . $wpdb->posts . '.post_title LIKE ' . $post_title_search;
-		}
-
-		return $where;
 	}
 
 	/**
