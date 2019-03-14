dc5cb55c2c018aaeefb57325b73573d5dfa3c020
Joshua Flowers <joshuatf@gmail.com>
2019-03-14 15:14:41 +0800

    Use search_code param in coupon search results

--- client/wc-api/items/utils.js
+++ client/wc-api/items/utils.js
@@ -20,9 +20,15 @@ export function searchItemsByString( select, endpoint, search ) {
 	let isError = false;
 	search.forEach( searchWord => {
 		const query = {
-			search: searchWord,
 			per_page: 10,
 		};
+		switch ( endpoint ) {
+			case 'coupons':
+				query.search_code = searchWord;
+				break;
+			default:
+				query.search = searchWord;
+		}
 		const newItems = getItems( endpoint, query );
 		newItems.forEach( ( item, id ) => {
 			items[ id ] = item;
