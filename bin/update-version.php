f6da97e2bf628de76186d04494dcf553f22abd10
Joshua Flowers <joshuatf@gmail.com>
2019-03-22 15:02:11 +0800

    Add version update script before build

---
+++ bin/update-version.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Updates PHP versions to match those in package.json before start or build.
+ *
+ * @package WooCommerce Admin
+ */
+
+$package_json = file_get_contents( 'package.json' );
+$package      = json_decode( $package_json );
+$plugin_file  = file( 'woocommerce-admin.php' );
+$lines        = array();
+
+foreach ( $plugin_file as $line ) {
+	if ( stripos( $line, ' * Version: ' ) !== false ) {
+		$line = " * Version: {$package->version}\n";
+	}
+	$lines[] = $line;
+}
+file_put_contents( 'woocommerce-admin.php', $lines );
