c5c58df229753a087be3610ff0f0a0a0690740c1
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-04-18 12:13:10 +0200

    Get rid of 'step' state property

--- client/analytics/settings/historical-data/index.js
+++ client/analytics/settings/historical-data/index.js
@@ -3,13 +3,13 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import moment from 'moment';
 
 /**
  * Internal dependencies
  */
+import HistoricalDataActions from './actions';
 import HistoricalDataPeriodSelector from './period-selector';
 import HistoricalDataProgress from './progress';
 import HistoricalDataStatus from './status';
@@ -29,20 +29,11 @@ class HistoricalData extends Component {
 				label: 'all',
 			},
 			skipChecked: true,
-			step: 'ready',
 		};
 
-		this.changeStep = this.changeStep.bind( this );
 		this.onDateChange = this.onDateChange.bind( this );
 		this.onPeriodChange = this.onPeriodChange.bind( this );
 		this.onSkipChange = this.onSkipChange.bind( this );
-	}
-
-	changeStep( newStep ) {
-		return () =>
-			this.setState( {
-				step: newStep,
-			} );
 	}
 
 	onPeriodChange( val ) {
@@ -69,21 +60,50 @@ class HistoricalData extends Component {
 		} );
 	}
 
-	isInProgress() {
-		const { step } = this.state;
-		return step === 'customers' || step === 'orders' || step === 'finalizing';
+	getStatus() {
+		const {
+			customersProgress,
+			customersTotal,
+			inProgress,
+			ordersProgress,
+			ordersTotal,
+		} = this.props;
+
+		if ( inProgress ) {
+			if ( customersProgress < customersTotal ) {
+				return 'customers';
+			}
+			if ( ordersProgress < ordersTotal ) {
+				return 'orders';
+			}
+			return 'finalizing';
+		}
+		if (
+			( customersTotal > 0 || ordersTotal > 0 ) &&
+			customersProgress === customersTotal &&
+			ordersProgress === ordersTotal
+		) {
+			return 'finished';
+		}
+		return 'ready';
 	}
 
 	render() {
 		const {
 			customersProgress,
 			customersTotal,
+			hasImportedData,
 			importDate,
+			inProgress,
 			ordersProgress,
 			ordersTotal,
 		} = this.props;
-		const { period, skipChecked, step } = this.state;
-		const isInProgress = this.isInProgress();
+		const { period, skipChecked } = this.state;
+		const hasImportedAllData =
+			! inProgress &&
+			hasImportedData &&
+			customersProgress === customersTotal &&
+			ordersProgress === ordersTotal;
 
 		return (
 			<Fragment>
@@ -99,17 +119,17 @@ class HistoricalData extends Component {
 								'woocommerce-admin'
 							) }
 						</span>
-						{ step !== 'finished' && (
+						{ ! hasImportedAllData && (
 							<Fragment>
 								<HistoricalDataPeriodSelector
 									dateFormat={ this.dateFormat }
-									disabled={ isInProgress }
+									disabled={ inProgress }
 									onPeriodChange={ this.onPeriodChange }
 									onDateChange={ this.onDateChange }
 									value={ period }
 								/>
 								<HistoricalDataSkipCheckbox
-									disabled={ isInProgress }
+									disabled={ inProgress }
 									checked={ skipChecked }
 									onChange={ this.onSkipChange }
 								/>
@@ -125,53 +145,17 @@ class HistoricalData extends Component {
 								/>
 							</Fragment>
 						) }
-						<HistoricalDataStatus importDate={ importDate } status={ step } />
+						<HistoricalDataStatus importDate={ importDate } status={ this.getStatus() } />
 					</div>
 				</div>
-				<div className="woocommerce-settings__actions woocommerce-settings-historical-data__actions">
-					{ step === 'ready' && (
-						<Button isPrimary onClick={ this.changeStep( 'customers' ) }>
-							{ __( 'Start', 'woocommerce-admin' ) }
-						</Button>
-					) }
-					{ isInProgress && (
-						<Fragment>
-							<Button
-								className="woocommerce-settings-historical-data__action-button"
-								isPrimary
-								onClick={ this.changeStep( 'stopped' ) }
-							>
-								{ __( 'Stop Import', 'woocommerce-admin' ) }
-							</Button>
-							<div className="woocommerce-setting__help woocommerce-settings-historical-data__action-help">
-								{ __(
-									'Imported data will not be lost if the import is stopped.',
-									'woocommerce-admin'
-								) }
-								<br />
-								{ __(
-									'Navigating away from this page will not affect the import.',
-									'woocommerce-admin'
-								) }
-							</div>
-						</Fragment>
-					) }
-					{ step === 'stopped' && (
-						<Fragment>
-							<Button isPrimary onClick={ this.changeStep( 'customers' ) }>
-								{ __( 'Start', 'woocommerce-admin' ) }
-							</Button>
-							<Button isDefault onClick={ this.changeStep( 'finished' ) }>
-								{ __( 'Delete Previously Imported Data', 'woocommerce-admin' ) }
-							</Button>
-						</Fragment>
-					) }
-					{ step === 'finished' && (
-						<Button isDefault onClick={ this.changeStep( 'ready' ) }>
-							{ __( 'Import Additional Historical Data', 'woocommerce-admin' ) }
-						</Button>
-					) }
-				</div>
+				<HistoricalDataActions
+					customersProgress={ customersProgress }
+					customersTotal={ customersTotal }
+					hasImportedData={ hasImportedData }
+					inProgress={ inProgress }
+					ordersProgress={ ordersProgress }
+					ordersTotal={ ordersTotal }
+				/>
 			</Fragment>
 		);
 	}
@@ -181,8 +165,9 @@ export default withSelect( () => {
 	return {
 		customersProgress: 0,
 		customersTotal: 0,
+		hasImportedData: false,
 		importDate: 'July 1',
-		isRequesting: false,
+		inProgress: false,
 		ordersProgress: 0,
 		ordersTotal: 0,
 	};
