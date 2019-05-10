b6523a206abcecd8aa93daaff784f109733143ba
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-05-10 16:30:00 +0200

    Remove getImportTotalsError and isGetImportTotalsRequesting selectors which were not used

--- client/wc-api/imports/selectors.js
+++ client/wc-api/imports/selectors.js
@@ -1,11 +1,6 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { isNil } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import { getResourceName } from '../utils';
@@ -19,24 +14,6 @@ const getImportTotals = ( getResource, requireResource ) => (
 	return requireResource( requirement, resourceName ) || { customers: null, orders: null };
 };
 
-const getImportTotalsError = getResource => ( type, query = {} ) => {
-	const resourceName = getResourceName( 'import-totals', query );
-	return getResource( resourceName ).error;
-};
-
-const isGetImportTotalsRequesting = getResource => ( type, query = {} ) => {
-	const resourceName = getResourceName( 'import-totals', query );
-	const { lastRequested, lastReceived } = getResource( resourceName );
-
-	if ( isNil( lastRequested ) || isNil( lastReceived ) ) {
-		return true;
-	}
-
-	return lastRequested > lastReceived;
-};
-
 export default {
 	getImportTotals,
-	getImportTotalsError,
-	isGetImportTotalsRequesting,
 };
