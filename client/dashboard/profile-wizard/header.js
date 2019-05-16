0928a6502b2c2d2994297da978f3935590c101a1
Joshua Flowers <joshuatf@gmail.com>
2019-05-16 17:50:55 +0800

    Add header logo as SVG

--- client/dashboard/profile-wizard/header.js
+++ client/dashboard/profile-wizard/header.js
@@ -4,11 +4,15 @@
  */
 import { Component } from '@wordpress/element';
 
+/**
+ * Internal dependencies
+ */
+import HeaderLogo from './header-logo';
 export default class ProfileWizardHeader extends Component {
 	render() {
 		return (
 			<div className="woocommerce-profile-wizard__header">
-				<img src={ wcSettings.wcAdminAssetUrl + '/woo-jetpack.png' } alt="WooCommerce + Jetpack" />
+				<HeaderLogo />
 			</div>
 		);
 	}
