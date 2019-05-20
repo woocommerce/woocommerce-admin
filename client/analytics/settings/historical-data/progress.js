a43186d6f832d686e65b65c6f336ad34a538bae1
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-05-20 17:05:34 +0200

    Fix RTL displays

--- client/analytics/settings/historical-data/progress.js
+++ client/analytics/settings/historical-data/progress.js
@@ -6,22 +6,26 @@ import { __, sprintf } from '@wordpress/i18n';
 import { isNil } from 'lodash';
 
 function HistoricalDataProgress( { label, progress, total } ) {
-	let text = sprintf( __( 'Imported %(label)s', 'woocommerce-admin' ), {
+	const labelText = sprintf( __( 'Imported %(label)s', 'woocommerce-admin' ), {
 		label,
 	} );
 
-	if ( ! isNil( progress ) && ! isNil( total ) ) {
-		text +=
-			' ' +
-			sprintf( __( '%(progress)s of %(total)s', 'woocommerce-admin' ), {
-				progress,
-				total,
-			} );
-	}
+	const labelCounters =
+		! isNil( progress ) && ! isNil( total )
+			? sprintf( __( '%(progress)s of %(total)s', 'woocommerce-admin' ), {
+					progress,
+					total,
+				} )
+			: null;
 
 	return (
 		<div className="woocommerce-settings-historical-data__progress">
-			<span className="woocommerce-settings-historical-data__progress-label">{ text }</span>
+			<span className="woocommerce-settings-historical-data__progress-label">{ labelText }</span>
+			{ labelCounters && (
+				<span className="woocommerce-settings-historical-data__progress-label">
+					{ labelCounters }
+				</span>
+			) }
 			<progress
 				className="woocommerce-settings-historical-data__progress-bar"
 				max={ total }
