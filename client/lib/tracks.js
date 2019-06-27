21a71319aa4bb93c57383c2264bda720925610df
Timmy Crawford <timmydcrawford@gmail.com>
2019-06-27 14:01:47 -0700

    Update comment

--- client/lib/tracks.js
+++ client/lib/tracks.js
@@ -12,7 +12,7 @@ const tracksDebug = debug( 'wc-admin:tracks' );
 /**
  * Record an event to Tracks
  *
- * @param {String} eventName The name of the event to record, always prefixed with wca_
+ * @param {String} eventName The name of the event to record, always prefixed with wcadmin_
  * @param {Object} eventProperties event properties to include in the event
  */
 
