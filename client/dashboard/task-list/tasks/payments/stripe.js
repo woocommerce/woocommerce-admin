5f75faf3623cba5f20a8d40f68ffd68c61fe881c
Justin Shreve <justin.shreve@gmail.com>
2019-09-03 10:06:59 -0400

    Add payment method configuration

---
+++ client/dashboard/task-list/tasks/payments/stripe.js
@@ -0,0 +1,17 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+
+/**
+ * WooCommerce dependencies
+ */
+
+class Stripe extends Component {
+	render() {
+		return null;
+	}
+}
+
+export default Stripe;
