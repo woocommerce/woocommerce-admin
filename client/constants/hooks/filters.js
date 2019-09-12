ce830ac12a21a627e6475464c31869f421ee32d8
Paul Sealock <psealock@gmail.com>
2019-09-12 17:13:55 -0400

    again

--- client/constants/hooks/filters.js
+++ client/constants/hooks/filters.js
@@ -3,11 +3,15 @@
  *
  * Enables manipulation of data used to create a report table.
  *
- * @format
+ * @param {object} reportTableData - data used to create the table.
+ * @param {string} reportTableData.endpoint - table api endpoint.
+ * @param {array} reportTableData.headers - table headers data.
+ * @param {array} reportTableData.rows - table rows data.
+ * @param {object} reportTableData.totals - total aggregates for request.
+ * @param {array} reportTableData.summary - summary numbers data.
  * @param {object} reportTableData.items - response from api requerst.
  * @return {object} reportTableData
  */
-
 export const TABLE_FILTER = 'woocommerce_admin_report_table';
 
 /**
