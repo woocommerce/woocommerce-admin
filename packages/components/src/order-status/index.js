d65893940e3bf3d02c704d703d544219519a9e68
Darren Ethier <darren@roughsmootheng.in>
2019-10-07 16:00:18 -0400

    decouple OrderStatus from wcSettings

--- packages/components/src/order-status/index.js
+++ packages/components/src/order-status/index.js
@@ -5,21 +5,19 @@
  */
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import { getSetting } from '@woocommerce/wc-admin-settings';
 
 /**
  * Use `OrderStatus` to display a badge with human-friendly text describing the current order status.
  *
  * @return { object } -
  */
-const OrderStatus = ( { order, className } ) => {
+const OrderStatus = ( { order, className, orderStatusMap } ) => {
 	const { status } = order;
-	const orderStatuses = getSetting( 'orderStatuses', {} );
 	const classes = classnames( 'woocommerce-order-status', className );
 	const indicatorClasses = classnames( 'woocommerce-order-status__indicator', {
 		[ 'is-' + status ]: true,
 	} );
-	const label = orderStatuses[ status ] || status;
+	const label = orderStatusMap[ status ] || status;
 	return (
 		<div className={ classes }>
 			<span className={ indicatorClasses } />
@@ -37,6 +35,10 @@ OrderStatus.propTypes = {
 	 * Additional CSS classes.
 	 */
 	className: PropTypes.string,
+	/**
+	 * A map of status to label for order statuses.
+	 */
+	orderStatusMap: PropTypes.object,
 };
 
 export default OrderStatus;
