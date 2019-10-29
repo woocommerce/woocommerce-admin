7061888bef2e3ada758c48e1a59ca723e55de9e9
Joshua Flowers <joshuatf@gmail.com>
2019-10-29 11:10:08 +0800

    Add wccom redirect if purchasable products exist

--- client/dashboard/index.js
+++ client/dashboard/index.js
@@ -2,8 +2,10 @@
 /**
  * External dependencies
  */
+import { addQueryArgs } from '@wordpress/url';
 import { Component } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -14,6 +16,52 @@ import ProfileWizard from './profile-wizard';
 import withSelect from 'wc-api/with-select';
 
 class Dashboard extends Component {
+	componentDidUpdate( prevProps ) {
+		const profileItems = get( this.props, 'profileItems', {} );
+		const prevProfileItems = get( prevProps, 'profileItems', {} );
+
+		if ( profileItems.completed && ! prevProfileItems.completed ) {
+			this.redirectToCart();
+		}
+	}
+
+	getProductIds() {
+		const profileItems = get( this.props, 'profileItems', {} );
+
+		const productIds = profileItems.product_types.map(
+			type =>
+				wcSettings.onboarding.productTypes[ type ] &&
+				wcSettings.onboarding.productTypes[ type ].product
+		);
+
+		const theme = wcSettings.onboarding.themes.find(
+			themeData => themeData.slug === profileItems.theme
+		);
+
+		if ( theme && theme.id && ! theme.is_installed ) {
+			productIds.push( theme.id );
+		}
+
+		return productIds;
+	}
+
+	redirectToCart() {
+		if ( ! window.wcAdminFeatures.onboarding ) {
+			return;
+		}
+
+		const productIds = this.getProductIds();
+		if ( ! productIds.length ) {
+			return;
+		}
+
+		const url = addQueryArgs( 'https://woocommerce.com/cart', {
+			'wccom-back': window.location.href,
+			'wccom-replace-with': productIds.join( ',' ),
+		} );
+		window.location = url;
+	}
+
 	render() {
 		const { path, profileItems, query } = this.props;
 
