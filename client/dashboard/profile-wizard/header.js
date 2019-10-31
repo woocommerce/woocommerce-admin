a56b262075f95ad846d2cf19d4ab1e05b1dadc39
Joshua Flowers <joshuatf@gmail.com>
2019-10-31 17:06:51 +0800

    Allow returning to previous steps on step click

--- client/dashboard/profile-wizard/header.js
+++ client/dashboard/profile-wizard/header.js
@@ -7,6 +7,11 @@ import { filter } from 'lodash';
 import classnames from 'classnames';
 
 /**
+ * WooCommerce dependencies
+ */
+import { updateQueryString } from '@woocommerce/navigation';
+
+/**
  * Internal dependencies
  */
 import { Stepper } from '@woocommerce/components';
@@ -14,9 +19,21 @@ import HeaderLogo from './header-logo';
 
 export default class ProfileWizardHeader extends Component {
 	renderStepper() {
-		const steps = filter( this.props.steps, step => !! step.label );
-		return <Stepper steps={ steps } currentStep={ this.props.currentStep } />;
+		const { currentStep, steps } = this.props;
+		const visibleSteps = filter( steps, step => !! step.label );
+		const currentStepIndex = visibleSteps.findIndex( s => s.key === currentStep );
+
+		visibleSteps.map( step => {
+			const stepIndex = visibleSteps.findIndex( s => s.key === step.key );
+			if ( stepIndex < currentStepIndex ) {
+				step.onClick = key => updateQueryString( { step: key } );
+			}
+			return step;
+		} );
+
+		return <Stepper steps={ visibleSteps } currentStep={ currentStep } />;
 	}
+
 	render() {
 		const currentStep = this.props.steps.find( s => s.key === this.props.currentStep );
 		const showStepper = ! currentStep || ! currentStep.label ? false : true;
