a100ad98eb98a7a214e2305d5d70dcd403654683
Paul Sealock <psealock@gmail.com>
2019-11-07 17:25:01 +1300

    Add src/Package.php for Core inclusion

---
+++ src/Package.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Returns information about the package and handles init.
+ *
+ * @package Automattic/WooCommerce/ExamplePackage
+ */
+
+namespace Automattic\WooCommerce\WCAdmin;
+
+use \Automattic\WooCommerce\Admin\FeaturePlugin;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main package class.
+ */
+class Package {
+
+	/**
+	 * Version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '0.21.0';
+
+	/**
+	 * Init the package.
+	 */
+	public static function init() {
+		FeaturePlugin::instance()->init();
+	}
+
+	/**
+	 * Return the version of the package.
+	 *
+	 * @return string
+	 */
+	public static function get_version() {
+		return self::VERSION;
+	}
+
+	/**
+	 * Return the path to the package.
+	 *
+	 * @return string
+	 */
+	public static function get_path() {
+		return dirname( __DIR__ );
+	}
+}
