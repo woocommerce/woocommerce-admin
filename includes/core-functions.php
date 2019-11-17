99b2b489313b7406a7c674077004c41fff8012dc
Paul Sealock <psealock@gmail.com>
2019-11-18 11:54:56 +1300

    try moving requires

--- includes/core-functions.php
+++ includes/core-functions.php
@@ -9,25 +9,25 @@
 
 use \Automattic\WooCommerce\Admin\Loader;
 
-//if ( ! function_exists( 'wc_admin_number_format' ) ) {
+// if ( ! function_exists( 'wc_admin_number_format' ) ) {
 	/**
 	 * Format a number using the decimal and thousands separator settings in WooCommerce.
 	 *
 	 * @param mixed $number Number to be formatted.
 	 * @return string
 	 */
-	function wc_admin_number_format( $number ) {
-		$currency_settings = Loader::get_currency_settings();
-		return number_format(
-			$number,
-			0,
-			$currency_settings['decimalSeparator'],
-			$currency_settings['thousandSeparator']
-		);
-	}
-//}
+function wc_admin_number_format( $number ) {
+	$currency_settings = Loader::get_currency_settings();
+	return number_format(
+		$number,
+		0,
+		$currency_settings['decimalSeparator'],
+		$currency_settings['thousandSeparator']
+	);
+}
+// }
 
-//if ( ! function_exists( 'wc_admin_url' ) ) {
+// if ( ! function_exists( 'wc_admin_url' ) ) {
 	/**
 	 * Retrieves a URL to relative path inside WooCommerce admin with
 	 * the provided query parameters.
@@ -37,17 +37,17 @@ use \Automattic\WooCommerce\Admin\Loader;
 	 *
 	 * @return string       Fully qualified URL pointing to the desired path.
 	 */
-	function wc_admin_url( $path = null, $query = array() ) {
-		if ( ! empty( $query ) ) {
-			$query_string = http_build_query( $query );
-			$path         = $path ? '&path=' . $path . '&' . $query_string : '';
-		}
-
-		return admin_url( 'admin.php?page=wc-admin' . $path, dirname( __FILE__ ) );
+function wc_admin_url( $path = null, $query = array() ) {
+	if ( ! empty( $query ) ) {
+		$query_string = http_build_query( $query );
+		$path         = $path ? '&path=' . $path . '&' . $query_string : '';
 	}
-//}
 
-//if ( ! function_exists( 'wc_admin_record_tracks_event' ) ) {
+	return admin_url( 'admin.php?page=wc-admin' . $path, dirname( __FILE__ ) );
+}
+// }
+
+// if ( ! function_exists( 'wc_admin_record_tracks_event' ) ) {
 	/**
 	 * Record an event using Tracks.
 	 *
@@ -55,18 +55,18 @@ use \Automattic\WooCommerce\Admin\Loader;
 	 * @param string $event_name Event name for tracks.
 	 * @param array  $properties Properties to pass along with event.
 	 */
-	function wc_admin_record_tracks_event( $event_name, $properties = array() ) {
-		if ( ! class_exists( 'WC_Tracks' ) ) {
-			if ( ! defined( 'WC_ABSPATH' ) || ! file_exists( WC_ABSPATH . 'includes/tracks/class-wc-tracks.php' ) ) {
-				return;
-			}
-			include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks.php';
-			include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-event.php';
-			include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-client.php';
-			include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-footer-pixel.php';
-			include_once WC_ABSPATH . 'includes/tracks/class-wc-site-tracking.php';
+function wc_admin_record_tracks_event( $event_name, $properties = array() ) {
+	if ( ! class_exists( 'WC_Tracks' ) ) {
+		if ( ! defined( 'WC_ABSPATH' ) || ! file_exists( WC_ABSPATH . 'includes/tracks/class-wc-tracks.php' ) ) {
+			return;
 		}
-
-		WC_Tracks::record_event( $event_name, $properties );
+		include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks.php';
+		include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-event.php';
+		include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-client.php';
+		include_once WC_ABSPATH . 'includes/tracks/class-wc-tracks-footer-pixel.php';
+		include_once WC_ABSPATH . 'includes/tracks/class-wc-site-tracking.php';
 	}
-//}
+
+	WC_Tracks::record_event( $event_name, $properties );
+}
+// }
--- includes/wc-admin-update-functions.php
+++ includes/wc-admin-update-functions.php
@@ -9,39 +9,39 @@
 
 use \Automattic\WooCommerce\Admin\Install as Installer;
 
-//if ( ! function_exists( 'wc_admin_update_0201_order_status_index' ) ) {
+// if ( ! function_exists( 'wc_admin_update_0201_order_status_index' ) ) {
 	/**
 	 * Update order stats `status` index length.
 	 * See: https://github.com/woocommerce/woocommerce-admin/issues/2969.
 	 */
-	function wc_admin_update_0201_order_status_index() {
-		global $wpdb;
+function wc_admin_update_0201_order_status_index() {
+	global $wpdb;
 
-		// Max DB index length. See wp_get_db_schema().
-		$max_index_length = 191;
+	// Max DB index length. See wp_get_db_schema().
+	$max_index_length = 191;
 
-		$index = $wpdb->get_row( "SHOW INDEX FROM {$wpdb->prefix}wc_order_stats WHERE key_name = 'status'" );
+	$index = $wpdb->get_row( "SHOW INDEX FROM {$wpdb->prefix}wc_order_stats WHERE key_name = 'status'" );
 
-		if ( property_exists( $index, 'Sub_part' ) ) {
-			// The index was created with the right length. Time to bail.
-			if ( $max_index_length === $index->Sub_part ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName
-				return;
-			}
-
-			// We need to drop the index so it can be recreated.
-			$wpdb->query( "DROP INDEX `status` ON {$wpdb->prefix}wc_order_stats" );
+	if ( property_exists( $index, 'Sub_part' ) ) {
+		// The index was created with the right length. Time to bail.
+		if ( $max_index_length === $index->Sub_part ) { // phpcs:ignore WordPress.NamingConventions.ValidVariableName
+			return;
 		}
 
-		// Recreate the status index with a max length.
-		$wpdb->query( $wpdb->prepare( "ALTER TABLE {$wpdb->prefix}wc_order_stats ADD INDEX status (status(%d))", $max_index_length ) );
+		// We need to drop the index so it can be recreated.
+		$wpdb->query( "DROP INDEX `status` ON {$wpdb->prefix}wc_order_stats" );
 	}
-//}
 
-//if ( ! function_exists( 'wc_admin_update_0201_db_version' ) ) {
+	// Recreate the status index with a max length.
+	$wpdb->query( $wpdb->prepare( "ALTER TABLE {$wpdb->prefix}wc_order_stats ADD INDEX status (status(%d))", $max_index_length ) );
+}
+// }
+
+// if ( ! function_exists( 'wc_admin_update_0201_db_version' ) ) {
 	/**
 	 * Update DB Version.
 	 */
-	function wc_admin_update_0201_db_version() {
-		Installer::update_db_version( '0.20.1' );
-	}
-//}
+function wc_admin_update_0201_db_version() {
+	Installer::update_db_version( '0.20.1' );
+}
+// }
