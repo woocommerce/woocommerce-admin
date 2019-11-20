927c972ecc8befc4e8a480d33ee6b607c6498346
Joshua Flowers <joshuatf@gmail.com>
2019-11-20 20:21:50 +0800

    Fix imported_from stat typo

--- src/ReportsSync.php
+++ src/ReportsSync.php
@@ -96,11 +96,11 @@ class ReportsSync {
 		}
 
 		// Update imported from date if older than previous.
-		$previous_import_date = $import_stats['import_from'];
+		$previous_import_date = $import_stats['imported_from'];
 		$current_import_date  = $days ? date( 'Y-m-d 00:00:00', time() - ( DAY_IN_SECONDS * $days ) ) : -1;
 
 		if ( ! $previous_import_date || -1 === $current_import_date || new \DateTime( $previous_import_date ) > new \DateTime( $current_import_date ) ) {
-			$import_stats['import_from'] = $current_import_date;
+			$import_stats['imported_from'] = $current_import_date;
 		}
 
 		update_option( 'wc_admin_import_stats', $import_stats );
