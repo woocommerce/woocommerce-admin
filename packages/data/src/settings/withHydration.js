ec8cfa32c845e99624cb0cd701c128c44d6c74de
Paul Sealock <psealock@gmail.com>
2019-12-20 11:37:20 +1300

    restore

--- packages/data/src/settings/withHydration.js
+++ packages/data/src/settings/withHydration.js
@@ -14,16 +14,25 @@ import { STORE_NAME } from './constants';
 export const withSettingsHydration = group => OriginalComponent => {
 	return ( props ) => {
 		const settings = useRef( getAllSettings() );
+
 		useSelect( ( select, registry ) => {
 			if ( ! settings.current ) {
 				return;
 			}
 
-			const { updateSettingsForGroup } = registry.dispatch(
+			const { isResolving, hasFinishedResolution } = select( STORE_NAME );
+			const { startResolution, finishResolution, updateSettingsForGroup } = registry.dispatch(
 				STORE_NAME
 			);
 
-			updateSettingsForGroup( group, settings.current );
+			if (
+				! isResolving( 'getSettings', [ group ] ) &&
+				! hasFinishedResolution( 'getSettings', [ group ] )
+			) {
+				startResolution( 'getSettings', [ group ] );
+				updateSettingsForGroup( group, settings.current );
+				finishResolution( 'getSettings', [ group ] );
+			}
 		}, [] );
 
 		return <OriginalComponent { ...props } />;
