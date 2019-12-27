d844b5d59bf93e3d7e30603ee2b67a7d012daad5
Joshua Flowers <joshuatf@gmail.com>
2019-12-27 17:45:43 +0800

    Add tests for theme activation

--- tests/api/onboarding-themes.php
+++ tests/api/onboarding-themes.php
@@ -80,4 +80,46 @@ class WC_Tests_API_Onboarding_Themes extends WC_REST_Unit_Test_Case {
 
 		$this->assertEquals( 'woocommerce_rest_invalid_theme', $data['code'] );
 	}
+
+	/**
+	 * Test that activating a valid theme works.
+	 */
+	public function test_activate_theme() {
+		wp_set_current_user( $this->user );
+
+		$previous_theme = get_stylesheet();
+		$request        = new WP_REST_Request( 'POST', $this->endpoint . '/activate' );
+		$request->set_query_params(
+			array(
+				'theme' => 'storefront',
+			)
+		);
+		$response     = $this->server->dispatch( $request );
+		$data         = $response->get_data();
+		$active_theme = get_stylesheet();
+
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 'storefront', $data['slug'] );
+		$this->assertEquals( 'success', $data['status'] );
+		$this->assertNotEquals( 'storefront', $previous_theme );
+		$this->assertEquals( 'storefront', $active_theme );
+	}
+
+	/**
+	 * Test that activating an invalid theme fails.
+	 */
+	public function test_activate_invalid_theme() {
+		wp_set_current_user( $this->user );
+
+		$request = new WP_REST_Request( 'POST', $this->endpoint . '/activate' );
+		$request->set_query_params(
+			array(
+				'theme' => 'invalid-theme-name',
+			)
+		);
+		$response = $this->server->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'woocommerce_rest_invalid_theme', $data['code'] );
+	}
 }
