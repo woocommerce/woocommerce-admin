76a520db28ed2831e3769da0e391d293b165e3ea
Ron Rennick <ron@ronandandrea.com>
2020-01-13 11:27:08 -0400

    add PHP 7.3 to Travis unit tests

--- bin/travis.sh
+++ bin/travis.sh
@@ -4,7 +4,7 @@
 if [ "$1" == 'before' ]; then
 	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 	npm run build:feature-config
-	if [[ "$RUN_PHPCS" == "1" || "$RUN_RANDOM" == "1" ]]; then
+	if [[ "$RUN_PHPCS" == "1" || "$RUN_RANDOM" == "1" || "$TRAVIS_PHP_VERSION" == "7.2" ]]; then
 		composer install
 	else
 		composer install --no-dev
