acdd6e1023ad575715c0136985e3c8fb1c7a751b
Ron Rennick <ron@ronandandrea.com>
2020-01-21 12:52:21 -0400

    rename to COMPOSER_DEV

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -4,7 +4,7 @@ cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 phpunit --version
 if [[ {$RUN_RANDOM} == 1 ]]; then
 	./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random
-elif [[ {$COMPOSE_DEV} == 1 ]]; then
+elif [[ {$COMPOSER_DEV} == 1 ]]; then
 	./vendor/bin/phpunit -c phpunit.xml.dist
 else
 	phpunit -c phpunit.xml.dist
--- bin/travis.sh
+++ bin/travis.sh
@@ -5,7 +5,7 @@ if [ "$1" == 'before' ]; then
 	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 	npm run build:feature-config
 	# Load PHPUnit version in dev dependencies for PHP ^7.2 until WP unit tests support PHPUnit 8.X
-	if [[ "$COMPOSE_DEV" == "1" ]]; then
+	if [[ "$COMPOSER_DEV" == "1" ]]; then
 		composer install
 	else
 		composer install --no-dev
