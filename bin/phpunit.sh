67167c4e11d330e666f1c2a4fe50a9c390d499b9
Ron Rennick <ron@ronandandrea.com>
2020-01-21 13:54:13 -0400

    update dependencies for PHP <7.1

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
-./vendor/bin/phpunit --version
-if [[ {$RUN_RANDOM} == 1 ]]; then
+if [[ {$COMPOSER_DEV} == 1 ]]; then
+	./vendor/bin/phpunit --version
 	./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random
 else
-	./vendor/bin/phpunit -c phpunit.xml.dist
+	phpunit --version
+	phpunit -c phpunit.xml.dist
 fi
 TEST_RESULT=$?
 cd "$WORKING_DIR"
