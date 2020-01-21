5f0e889d8c7b4fa7a716b071fce3816ae851f95b
Ron Rennick <ron@ronandandrea.com>
2020-01-21 11:34:09 -0400

    add order items to stats reset in unit tests

--- tests/framework/helpers/class-wc-helper-reports.php
+++ tests/framework/helpers/class-wc-helper-reports.php
@@ -21,6 +21,34 @@ class WC_Helper_Reports {
 		$wpdb->query( 'DELETE FROM ' . \Automattic\WooCommerce\Admin\API\Reports\Products\DataStore::get_db_table_name() ); // @codingStandardsIgnoreLine.
 		$wpdb->query( 'DELETE FROM ' . \Automattic\WooCommerce\Admin\API\Reports\Coupons\DataStore::get_db_table_name() ); // @codingStandardsIgnoreLine.
 		$wpdb->query( 'DELETE FROM ' . \Automattic\WooCommerce\Admin\API\Reports\Customers\DataStore::get_db_table_name() ); // @codingStandardsIgnoreLine.
+		$wpdb->query(
+			"DELETE FROM
+				{$wpdb->prefix}woocommerce_order_itemmeta
+			WHERE
+				order_item_id NOT IN (
+					SELECT
+						oi.order_item_id
+					FROM
+						{$wpdb->posts} o,
+						{$wpdb->prefix}woocommerce_order_items oi
+					WHERE
+						o.ID = oi.order_id
+						AND o.post_type IN ( 'shop_order', 'shop_order_refund' )
+				)"
+		); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		$wpdb->query(
+			"DELETE FROM
+				{$wpdb->prefix}woocommerce_order_items
+			WHERE
+				order_id NOT IN (
+					SELECT
+						ID
+					FROM
+						{$wpdb->posts}
+					WHERE
+						post_type IN ( 'shop_order', 'shop_order_refund' )
+				)"
+		); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 		\Automattic\WooCommerce\Admin\CategoryLookup::instance()->regenerate();
 	}
 }
