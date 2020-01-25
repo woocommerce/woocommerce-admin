136d3ad7bc27b1d010a13048b62c2df597729357
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-01-24 18:54:10 -0700

    Add E2E test of product inventory update from activity panel.

--- tests/e2e-tests/specs/stock-panel.test.js
+++ tests/e2e-tests/specs/stock-panel.test.js
@@ -17,6 +17,9 @@ import {
 } from '../../../../../tests/e2e-tests/utils';
 import { verifyAndPublish } from '../../../../../tests/e2e-tests/utils/components';
 
+const config = require( 'config' );
+const baseUrl = config.get( 'url' );
+
 const goToInventorySettings = async () => {
 	// Go to inventory settings page
 	await StoreOwnerFlow.openSettings( 'products', 'inventory' );
@@ -76,12 +79,7 @@ describe( 'Store owner can manage stock levels from activity panel', () => {
 	} );
 
 	it( 'shows products that are low in stock', async () => {
-		// await expect( page ).toClick( '#activity-panel-tab-stock' );
-		// await new Promise(r => setTimeout(r, 5000));
-
 		// Create a product that is low in stock.
-
-		// Go to "add product" page.
 		await StoreOwnerFlow.openNewProduct();
 
 		// Make sure we're on the add order page.
@@ -97,13 +95,59 @@ describe( 'Store owner can manage stock levels from activity panel', () => {
 		await expect( page ).toFill( '#_stock', '1' );
 
 		// Publish product.
-		verifyAndPublish();
+		await Promise.all( [
+			verifyAndPublish(),
+			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+		] );
 
 		// Verify that the stock panel indicates unread.
-		await expect( page ).toMatchElement( '#activity-panel-tab-stock.has-unread' );
+		await page.waitForSelector( '#activity-panel-tab-stock.has-unread' );
 
 		// Open stock panel.
 		await expect( page ).toClick( '#activity-panel-tab-stock' );
 		await page.waitForSelector( '.woocommerce-stock-activity-card:not(.is-loading)' );
+
+		// Verify the low in stock product is displayed.
+		const stockCard = await page.$( '.woocommerce-stock-activity-card' );
+		await expect( stockCard ).toMatchElement( '.woocommerce-activity-card__title', { text: 'Low in stock product' } );
+	} );
+
+	it( 'can update inventory level', async () => {
+		// Open dashboard.
+		await page.goto( baseUrl + 'wp-admin/admin.php?page=wc-admin', {
+			waitUntil: 'networkidle0',
+		} );
+
+		// Open stock panel.
+		await expect( page ).toClick( '#activity-panel-tab-stock' );
+		await page.waitForSelector( '.woocommerce-stock-activity-card:not(.is-loading)' );
+
+		// Verify the low in stock product is displayed.
+		let stockCard = await page.$( '.woocommerce-stock-activity-card' );
+		await expect( stockCard ).toMatchElement( '.woocommerce-activity-card__title', { text: 'Low in stock product' } );
+
+		// Modify the inventory level.
+		await expect( stockCard ).toClick( 'button', { text: 'Update stock' } );
+
+		// We apparently need to reselect the card when the innerHTML changes.
+		stockCard = await page.$( '.woocommerce-stock-activity-card' );
+
+		await page.waitForSelector( '.woocommerce-stock-activity-card__edit-quantity input' );
+		await expect( stockCard ).toFill( 'input[type="number"]', '5' );
+		await expect( stockCard ).toClick( 'button', { text: 'Save' } );
+
+		// Refresh the page.
+		// NOTE: we should be able to just close and reopen the stock panel.
+		// There seems to be an issue where the panel doesn't recognize
+		// an empty array response as "no low stock items".
+		await page.reload( { waitUntil: 'networkidle0' } );
+
+		// Open stock panel.
+		await page.waitForSelector( '#activity-panel-tab-stock:not(.has-unread)' );
+		await expect( page ).toClick( '#activity-panel-tab-stock' );
+		await page.waitForSelector( '.woocommerce-empty-activity-card' );
+
+		// Verify there are no more low stock products.
+		await expect( page ).toMatchElement( '.woocommerce-layout__activity-panel-header-title', { text: 'No products with low stock' } );
 	} );
 } );
