990fd2b40f8689e430c725dabdf6d3bb7c925151
Joshua Flowers <joshuatf@gmail.com>
2020-02-09 13:07:49 -0800

    Add notes version hooks

--- src/Package.php
+++ src/Package.php
@@ -41,10 +41,11 @@ class Package {
 
 		// Avoid double initialization when the feature plugin is in use.
 		if ( defined( 'WC_ADMIN_VERSION_NUMBER' ) ) {
+			$update_version = new WC_Admin_Notes_Update_Version();
 			if ( version_compare( WC_ADMIN_VERSION_NUMBER, self::VERSION, '<' ) ) {
-				WC_Admin_Notes_Update_Version::add_note();
+				$update_version::add_note();
 			} else {
-				WC_Admin_Notes_Update_Version::delete_note();
+				$update_version::delete_note();
 			}
 
 			return;
