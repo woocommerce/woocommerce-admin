62c8c073b215435379703448a39228455d157d88
Joshua Flowers <joshuatf@gmail.com>
2020-02-14 10:27:45 +0100

    Add remaining profiler steps

--- tests/e2e-tests/specs/onboarding-profiler.test.js
+++ tests/e2e-tests/specs/onboarding-profiler.test.js
@@ -42,9 +42,47 @@ describe( 'Store owner can enable or disable onboarding', () => {
 		await expect( page ).toMatchElement( '.woocommerce-profile-wizard__usage-modal' );
 		await expect( page ).toClick( 'button', { text: 'Continue' } );
 
+		// Skip the Jetpack connection for testing purposes.
+		await page.goto( baseUrl + 'wp-admin/admin.php?page=wc-admin&step=store-details', {
+			waitUntil: 'networkidle0',
+		} );
+
 		// Store details.
+		await expect( page ).toFill( '#inspector-text-control-0', '123 Peachtree Lane' );
+		await expect( page ).toFill( '.woocommerce-select-control__control-input', 'United States (US) -- Georgia' );
+		await page.keyboard.press( 'Enter' );
+		await expect( page ).toFill( '#inspector-text-control-2', 'Atlanta' );
+		await expect( page ).toFill( '#inspector-text-control-3', '30312' );
+		await expect( page ).toClick( 'button', { text: 'Continue' } );
+
+		// Industry.
 		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
-		await expect( page ).toMatchElement( '.woocommerce-profile-wizard__plugins-actions' );
-		await expect( page ).toClick( 'button', { text: 'Activate & continue' } );
+		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:first-child input' );
+		await expect( page ).toClick( 'button', { text: 'Continue' } );
+
+		// Product types
+		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:first-child input' );
+		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:nth-child(2) input' );
+		await expect( page ).toClick( 'button', { text: 'Continue' } );
+
+		// Business details.
+		await Promise.all( [
+			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+			expect( page ).toMatchElement( '.woocommerce-card__body div:first-child .woocommerce-select-control__control' ),
+		] );
+		await expect( page ).toClick( '.woocommerce-card__body div:first-child .woocommerce-select-control__control' );
+		await expect( page ).toClick( '#woocommerce-select-control__option-1-0' );
+		await expect( page ).toClick( '.woocommerce-card__body div:nth-child(2) .woocommerce-select-control__control' );
+		await expect( page ).toClick( '#woocommerce-select-control__option-2-no' );
+		await expect( page ).toClick( 'button', { text: 'Continue' } );
+
+		// Theme.
+		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+		await expect( page ).toClick( '.woocommerce-profile-wizard__theme:nth-child(2) button', { text: 'Choose' } );
+		await expect( page ).toClick( 'button', { text: "I'll do it later" } );
+
+		// Make sure the profiler is closed.
+		await expect( page ).toMatchElement( 'body:not(.woocommerce-profile-wizard__body)' );
 	} );
 } );
