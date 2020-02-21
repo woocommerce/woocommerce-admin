f258056157d033f8ac5d5c3e7f775286904f90a2
Joshua Flowers <joshuatf@gmail.com>
2020-02-21 10:30:45 +0100

    Add test for showing free text search option

--- packages/components/src/search/test/index.js
+++ packages/components/src/search/test/index.js
@@ -10,6 +10,18 @@ import { shallow } from 'enzyme';
 import { Search } from '../index';
 
 describe( 'Search', () => {
+	it( 'shows the free text search option', () => {
+		const search = shallow(
+			<Search
+				type="products"
+				allowFreeTextSearch
+			/>
+		);
+		const options = search.instance().appendFreeTextSearch( [], 'Product Query' );
+
+		expect( options.length ).toBe( 1 );
+	} );
+
 	it( "doesn't show options with an empty search", () => {
 		const search = shallow(
 			<Search
