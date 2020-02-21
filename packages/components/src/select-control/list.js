8861e4b9673dcdfe3ed9950602d9f703e5ec8295
Joshua Flowers <joshuatf@gmail.com>
2020-02-21 10:26:50 +0100

    Revert changes in #3700

--- packages/components/src/select-control/list.js
+++ packages/components/src/select-control/list.js
@@ -151,10 +151,6 @@ class List extends Component {
 		const listboxClasses = classnames( 'woocommerce-select-control__listbox', {
 			'is-static': staticList,
 		} );
-		const optionsHaveValues = options ? options[ 0 ].value.id !== '' : false;
-		if ( ! optionsHaveValues ) {
-			return null;
-		}
 
 		return (
 			<div ref={ this.listbox } id={ listboxId } role="listbox" className={ listboxClasses }>
