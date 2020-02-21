f6ceaef49285a7c1871e3e93f2f2ed81ecef6371
Fernando Marichal <contacto@fernandomarichal.com>
2020-02-21 18:32:08 -0300

    Stripped HTML from product titles and decoded before displaying them
    Stripped html from product titles and entities are decoded before displaying them

--- packages/components/src/search/test/index.js
+++ packages/components/src/search/test/index.js
@@ -8,6 +8,7 @@ import { shallow } from 'enzyme';
  * Internal dependencies
  */
 import { Search } from '../index';
+import { computeSuggestionMatch } from '../autocompleters/utils';
 
 describe( 'Search', () => {
 	it( 'shows the free text search option', () => {
@@ -32,6 +33,12 @@ describe( 'Search', () => {
 		const options = search.instance().appendFreeTextSearch( [], '' );
 
 		expect( options.length ).toBe( 0 );
+	} );
+
+	it( 'returns an object with decoded text', () => {
+		const decodedText = computeSuggestionMatch( 'A test &amp; a &#97;&#101;&#115;&#116;', 'test' );
+		const expected = '{"suggestionBeforeMatch":"A ","suggestionMatch":"test","suggestionAfterMatch":" & a aest"}';
+		expect( JSON.stringify( decodedText ) ).toBe( expected );
 	} );
 } );
 
