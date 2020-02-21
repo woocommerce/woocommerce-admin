e0842e534d681a7c30287a129a38bd134ed8855d
Joshua T Flowers <joshuatf@gmail.com>
2020-02-21 01:35:10 +0100

    Add deactivation note for feature plugin (#3687)
    * Add version deactivation note
    
    * Add the note to deactivate if the version is older than the current WC version
    
    * Deactivate wc admin feature plugin on action click
    
    * Add notes version hooks
    
    * change the Package class namespace to exclude from standalone autoloader
    
    * add use statement for FeaturePlugin
    
    * add note explaining namespace
    
    * use wc-admin-deactivate-plugin note name
    
    * Rename file and class to WC_Admin_Notes_Deactivate_Plugin
    
    Co-authored-by: Ron Rennick <ron@ronandandrea.com>
    Co-authored-by: Paul Sealock <psealock@gmail.com>

--- src/Package.php
+++ src/Package.php
@@ -5,9 +5,16 @@
  * @package Automattic/WooCommerce/WCAdmin
  */
 
-namespace Automattic\WooCommerce\Admin;
+/**
+ * This namespace isn't compatible with the PSR-4
+ * which ensures that the copy in the standalone plugin will not be autoloaded.
+ */
+namespace Automattic\WooCommerce\Admin\Composer;
 
 defined( 'ABSPATH' ) || exit;
+
+use Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Deactivate_Plugin;
+use Automattic\WooCommerce\Admin\FeaturePlugin;
 
 /**
  * Main package class.
@@ -39,6 +46,13 @@ class Package {
 
 		// Avoid double initialization when the feature plugin is in use.
 		if ( defined( 'WC_ADMIN_VERSION_NUMBER' ) ) {
+			$update_version = new WC_Admin_Notes_Deactivate_Plugin();
+			if ( version_compare( WC_ADMIN_VERSION_NUMBER, self::VERSION, '<' ) ) {
+				$update_version::add_note();
+			} else {
+				$update_version::delete_note();
+			}
+
 			return;
 		}
 
