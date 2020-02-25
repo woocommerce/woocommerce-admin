5c3ae2ea6d7de472f07e8021cc677a47354a8575
Fernando Marichal <contacto@fernandomarichal.com>
2020-02-25 09:34:49 -0300

    pre-commit changes

--- src/API/Reports/Products/Controller.php
+++ src/API/Reports/Products/Controller.php
@@ -70,8 +70,8 @@ class Controller extends \WC_REST_Reports_Controller implements ExportableInterf
 		$data = array();
 
 		foreach ( $products_data->data as $product_data ) {
-			$item   = $this->prepare_item_for_response( $product_data, $request );
-			if ( isset( $item->data['extended_info']['name'] ) ){
+			$item = $this->prepare_item_for_response( $product_data, $request );
+			if ( isset( $item->data['extended_info']['name'] ) ) {
 				$item->data['extended_info']['name'] = wp_strip_all_tags( $item->data['extended_info']['name'] );
 			}
 			$data[] = $this->prepare_response_for_collection( $item );
