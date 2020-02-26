00952ab390e43d469b5c5be0be2fee89ad29db06
Joshua Flowers <joshuatf@gmail.com>
2020-02-26 16:06:36 +0100

    Add paypal to plugin name constants

--- client/wc-api/onboarding/constants.js
+++ client/wc-api/onboarding/constants.js
@@ -7,22 +7,26 @@ import { __ } from '@wordpress/i18n';
  * Plugin slugs and names as key/value pairs.
  */
 export const pluginNames = {
-	jetpack: __( 'Jetpack', 'woocommerce-admin' ),
-	'woocommerce-services': __( 'WooCommerce Services', 'woocommerce-admin' ),
-	'mailchimp-for-woocommerce': __(
-		'Mailchimp for WooCommerce',
-		'woocommerce-admin'
-	),
 	'facebook-for-woocommerce': __(
 		'Facebook for WooCommerce',
 		'woocommerce-admin'
 	),
-	'woocommerce-shipstation-integration': __(
-		'WooCommerce ShipStation Gateway',
+	jetpack: __( 'Jetpack', 'woocommerce-admin' ),
+	'mailchimp-for-woocommerce': __(
+		'Mailchimp for WooCommerce',
+		'woocommerce-admin'
+	),
+	'woocommerce-gateway-paypal-express-checkout': __(
+		'WooCommerce PayPal',
 		'woocommerce-admin'
 	),
 	'woocommerce-gateway-stripe': __(
 		'WooCommerce Stripe',
+		'woocommerce-admin'
+	),
+	'woocommerce-services': __( 'WooCommerce Services', 'woocommerce-admin' ),
+	'woocommerce-shipstation-integration': __(
+		'WooCommerce ShipStation Gateway',
 		'woocommerce-admin'
 	),
 };
