fa3d21534ae6fc64ca6e42b5002b735c88f333f7
Fernando <ultimoround@gmail.com>
2020-03-02 09:14:26 -0300

    Update Country Labeling to Match Core (#3790)
    * Updated country labeling
    
    Country labeling on Customer Report was updated
    
    * Updated country labeling in other files

--- client/analytics/report/customers/config.js
+++ client/analytics/report/customers/config.js
@@ -95,13 +95,16 @@ export const advancedFilters = applyFilters( CUSTOMERS_REPORT_ADVANCED_FILTERS_F
 		},
 		country: {
 			labels: {
-				add: __( 'Country', 'woocommerce-admin' ),
+				add: __( 'Country / Region', 'woocommerce-admin' ),
 				placeholder: __( 'Search', 'woocommerce-admin' ),
-				remove: __( 'Remove country filter', 'woocommerce-admin' ),
-				rule: __( 'Select a country filter match', 'woocommerce-admin' ),
+				remove: __( 'Remove country / region filter', 'woocommerce-admin' ),
+				rule: __( 'Select a country / region filter match', 'woocommerce-admin' ),
 				/* translators: A sentence describing a Product filter. See screen shot for context: https://cloudup.com/cCsm3GeXJbE */
-				title: __( '{{title}}Country{{/title}} {{rule /}} {{filter /}}', 'woocommerce-admin' ),
-				filter: __( 'Select country', 'woocommerce-admin' ),
+				title: __(
+					'{{title}}Country / Region{{/title}} {{rule /}} {{filter /}}',
+					'woocommerce-admin'
+				),
+				filter: __( 'Select country / region', 'woocommerce-admin' ),
 			},
 			rules: [
 				{
