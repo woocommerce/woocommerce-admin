6b1ec77adc9ef8ec97ebaf1e3a13ff20f1cb9b2a
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-03-04 12:22:44 -0700

    Properly prepare/escape special characters in Tax code search.

--- src/API/Taxes.php
+++ src/API/Taxes.php
@@ -99,9 +99,8 @@ class Taxes extends \WC_REST_Taxes_Controller {
 		// Filter by tax code.
 		$tax_code_search = $prepared_args['search'];
 		if ( $tax_code_search ) {
-			$tax_code_search = $wpdb->esc_like( $tax_code_search );
-			$tax_code_search = ' \'%' . $tax_code_search . '%\'';
-			$query          .= ' AND CONCAT_WS( "-", NULLIF(tax_rate_country, ""), NULLIF(tax_rate_state, ""), NULLIF(tax_rate_name, ""), NULLIF(tax_rate_priority, "") ) LIKE ' . $tax_code_search;
+			$code_like = '%' . $wpdb->esc_like( $tax_code_search ) . '%';
+			$query    .= $wpdb->prepare( ' AND CONCAT_WS( "-", NULLIF(tax_rate_country, ""), NULLIF(tax_rate_state, ""), NULLIF(tax_rate_name, ""), NULLIF(tax_rate_priority, "") ) LIKE %s', $code_like );
 		}
 
 		// Filter by included tax rate IDs.
