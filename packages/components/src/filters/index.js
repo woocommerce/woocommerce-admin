27c92b19121e1e23b9936b62399dab8fbfc714a8
Paul Sealock <psealock@gmail.com>
2020-03-05 09:07:18 +1300

    ReportFilters component: Add sane defaults

--- packages/components/src/filters/index.js
+++ packages/components/src/filters/index.js
@@ -11,6 +11,8 @@ import PropTypes from 'prop-types';
  * WooCommerce dependencies
  */
 import { updateQueryString } from '@woocommerce/navigation';
+import { getDateParamsFromQuery, getCurrentDates } from '@woocommerce/date';
+import Currency from '@woocommerce/currency';
 
 /**
  * Internal dependencies
@@ -82,6 +84,19 @@ class ReportFilters extends Component {
 		onDateSelect( data );
 	}
 
+	getDateQuery( query ) {
+		const { period, compare, before, after } = getDateParamsFromQuery( query );
+		const { primary: primaryDate, secondary: secondaryDate } = getCurrentDates( query );
+		return {
+			period,
+			compare,
+			before,
+			after,
+			primaryDate,
+			secondaryDate,
+		};
+	}
+
 	render() {
 		const {
 			dateQuery,
@@ -100,7 +115,7 @@ class ReportFilters extends Component {
 						{ showDatePicker && (
 							<DateRangeFilterPicker
 								key={ JSON.stringify( query ) }
-								dateQuery={ dateQuery }
+								dateQuery={ dateQuery || this.getDateQuery( query ) }
 								onRangeSelect={ this.onRangeSelect }
 								isoDateFormat={ isoDateFormat }
 							/>
@@ -166,7 +181,7 @@ ReportFilters.propTypes = {
 	/**
 	 * The currency formatting instance for the site.
 	 */
-	currency: PropTypes.object.isRequired,
+	currency: PropTypes.object,
 	/**
 	 * The date query string represented in object form.
 	 */
@@ -182,7 +197,7 @@ ReportFilters.propTypes = {
 		secondaryDate: PropTypes.shape( {
 			label: PropTypes.string.isRequired,
 			range: PropTypes.string.isRequired,
-		} ).isRequired,
+		} ),
 	} ),
 	/**
 	 * ISO date format string.
@@ -197,6 +212,15 @@ ReportFilters.defaultProps = {
 	query: {},
 	showDatePicker: true,
 	onDateSelect: () => {},
+	currency: new Currency( {
+		code: 'USD',
+		precision: 2,
+		symbol: '$',
+		symbolPosition: 'left',
+		decimalSeparator: '.',
+		thousandSeparator: ',',
+		priceFormat: '%1$s%2$s',
+	} ),
 };
 
 export default ReportFilters;
