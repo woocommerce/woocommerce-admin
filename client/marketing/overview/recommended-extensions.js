385196a0e6c796d7ed3f71aaa1b65dcfdd6f2708
Daniel Bitzer <danielbitzer@gmail.com>
2020-03-17 15:53:18 +1030

    Add initial marketing cards

---
+++ client/marketing/overview/recommended-extensions.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * WooCommerce dependencies
+ */
+import { Card } from '@woocommerce/components';
+
+class RecommendedExtensions extends Component {
+	render() {
+		return (
+			<Card
+				title={ __( 'Recommended extensions', 'woocommerce-admin' ) }
+				description={ __( 'Great marketing requires the right tools. Take your marketing to the next level with our recommended marketing extensions.', 'woocommerce-admin' ) }
+			/>
+		)
+	}
+}
+
+export default RecommendedExtensions;
