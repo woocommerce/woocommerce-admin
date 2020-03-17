1f6bfa7c2263e6a2c598d2b70790632444a09f0d
Daniel Bitzer <danielbitzer@gmail.com>
2020-03-17 15:37:08 +1030

    Register marketing page with layout controller

---
+++ client/marketing/overview/index.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+
+class MarketingOverview extends Component {
+	render() {
+		return '...';
+	}
+}
+
+export default MarketingOverview;
