860060b06510d6d6636f64bd0fdf325236ce23a2
Daniel Bitzer <danielbitzer@gmail.com>
2020-03-18 17:54:42 +1030

    Add initial extension data to InstalledExtensions card

--- client/marketing/overview/installed-extensions.js
+++ client/marketing/overview/installed-extensions.js
@@ -8,13 +8,36 @@ import { __ } from '@wordpress/i18n';
  * WooCommerce dependencies
  */
 import { Card } from '@woocommerce/components';
+import { getSetting } from '@woocommerce/wc-admin-settings';
+import InstalledExtensionRow from "./installed-extension-row";
 
 class InstalledExtensions extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			extensions: [],
+		}
+	}
+
+	componentDidMount() {
+		const { installedExtensions } = getSetting( 'marketing', {} );
+
+		this.setState( { extensions: installedExtensions } )
+	}
+
 	render() {
+		const { extensions } = this.state;
+
 		return (
 			<Card
 				title={ __( 'Installed marketing extensions', 'woocommerce-admin' ) }
-			/>
+				className="woocommerce-marketing-installed-extensions-card"
+			>
+				{ extensions.map( ( extension ) => {
+					return <InstalledExtensionRow { ...extension } />
+				} ) }
+			</Card>
 		)
 	}
 }
