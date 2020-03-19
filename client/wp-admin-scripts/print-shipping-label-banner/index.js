95c5f1127e7e645473042288325c6bee92b40393
David Stone <david@nnucomputerwhiz.com>
2020-03-19 15:41:48 -0600

    Load the shipping banner and tracking sidebar in the appropriate places

--- client/wp-admin-scripts/print-shipping-label-banner/index.js
+++ client/wp-admin-scripts/print-shipping-label-banner/index.js
@@ -6,6 +6,8 @@ import ShippingBanner from './shipping-banner';
 import './store';
 
 const metaBox = document.getElementById( 'wc-admin-shipping-banner-root' );
+const args =
+	( metaBox.dataset.args && JSON.parse( metaBox.dataset.args ) ) || {};
 
 // Render the header.
-render( <ShippingBanner />, metaBox );
+render( <ShippingBanner itemsCount={ args.shippable_items_count } />, metaBox );
