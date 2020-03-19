8bf73d42692d13dd6fc6af64d2e9ef64417c663f
Daniel Bitzer <danielbitzer@gmail.com>
2020-03-20 10:16:10 +1030

    Add basic styling to RecommendedExtensions card

--- client/marketing/overview/recommended-extensions/index.js
+++ client/marketing/overview/recommended-extensions/index.js
@@ -16,6 +16,7 @@ import { Card } from '@woocommerce/components';
  */
 import { WC_ADMIN_NAMESPACE } from '../../../wc-api/constants';
 import './style.scss'
+import PlaceholderImg from './hubspot.svg';
 
 class RecommendedExtensions extends Component {
 	constructor( props ) {
@@ -52,23 +53,28 @@ class RecommendedExtensions extends Component {
 
 	render() {
 		const { extensions, isLoading } = this.state;
+		const classNameBase = 'woocommerce-marketing-recommended-extensions-card';
 
 		return (
 			<Card
 				title={ __( 'Recommended extensions', 'woocommerce-admin' ) }
 				description={ __( 'Great marketing requires the right tools. Take your marketing to the next level with our recommended marketing extensions.', 'woocommerce-admin' ) }
-				className="woocommerce-marketing-recommended-extensions-card"
+				className={ classNameBase }
 			>
 				<Fragment>
 					{ isLoading && <Spinner /> }
 					{ isLoading || (
-						<div className="woocommerce-marketing-recommended-extensions-card__items">
+						<div className={ `${ classNameBase }__items` }>
 							{ isLoading || extensions.map( ( extension ) => (
-								<div key={ extension.product }
-									className="woocommerce-marketing-recommended-extensions-card__item">
-									<h4>{ extension.title }</h4>
-									<p>{ extension.description }</p>
-								</div>
+								<a key={ extension.product }
+									href="#"
+									className={ `${ classNameBase }__item` }>
+									<img src={ PlaceholderImg } className={ `${ classNameBase }__item-img` } />
+									<div className={ `${ classNameBase }__item-text` }>
+										<h4>{ extension.title }</h4>
+										<p>{ extension.description }</p>
+									</div>
+								</a>
 							) ) }
 						</div>
 					) }
