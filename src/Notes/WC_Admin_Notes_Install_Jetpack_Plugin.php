70f3f216c21f3a4c4ed35ec573d1c3e9e079db88
Rebecca Scott <me@becdetat.com>
2020-03-21 11:16:51 +1000

    Also allow WooCommerce Services plugin when creating note

--- src/Notes/WC_Admin_Notes_Install_Jetpack_Plugin.php
+++ src/Notes/WC_Admin_Notes_Install_Jetpack_Plugin.php
@@ -24,8 +24,8 @@ class WC_Admin_Notes_Install_Jetpack_Plugin {
 	 * @param string $slug The slug of the plugin being installed.
 	 */
 	public static function possibly_add_install_jetpack_note( $slug ) {
-		// Exit early if we're not installing the Jetpack plugin.
-		if ( 'jetpack' !== $slug ) {
+		// Exit early if we're not installing the Jetpack or the WooCommerce Services plugins.
+		if ( 'jetpack' !== $slug && 'woocommerce-services' !== $slug ) {
 			return;
 		}
 
