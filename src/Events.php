499bbe2ac677028732a34c982ec6fd11476f6427
Jason Conroy <jason@findingsimple.com>
2020-04-01 08:51:00 +1030

    Remove facebook extension inbox note

--- src/Events.php
+++ src/Events.php
@@ -10,7 +10,6 @@ namespace Automattic\WooCommerce\Admin;
 
 defined( 'ABSPATH' ) || exit;
 
-use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Facebook_Extension;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Giving_Feedback_Notes;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Mobile_App;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_New_Sales_Record;
@@ -66,7 +65,6 @@ class Events {
 		WC_Admin_Notes_New_Sales_Record::possibly_add_sales_record_note();
 		WC_Admin_Notes_Giving_Feedback_Notes::add_notes_for_admin_giving_feedback();
 		WC_Admin_Notes_Mobile_App::possibly_add_mobile_app_note();
-		WC_Admin_Notes_Facebook_Extension::possibly_add_facebook_note();
 		WC_Admin_Notes_Tracking_Opt_In::possibly_add_tracking_opt_in_note();
 		WC_Admin_Notes_Onboarding_Email_Marketing::possibly_add_onboarding_email_marketing_note();
 		WC_Admin_Notes_Personalize_Store::possibly_add_personalize_store_note();
