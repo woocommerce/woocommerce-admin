f83215a9a16fefc3e1d3453750029c24ad18f685
Jason Conroy <jason@findingsimple.com>
2020-04-03 06:18:35 +1030

    Remove unnecessary use of WC_Admin_Notes_Marketing

--- src/FeaturePlugin.php
+++ src/FeaturePlugin.php
@@ -16,7 +16,6 @@ use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Welcome_Message;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Woo_Subscriptions_Notes;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Tracking_Opt_In;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_WooCommerce_Payments;
-use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Marketing;
 
 /**
  * Feature plugin main class.
@@ -187,7 +186,6 @@ class FeaturePlugin {
 		new WC_Admin_Notes_Welcome_Message();
 		new WC_Admin_Notes_Tracking_Opt_In();
 		new WC_Admin_Notes_WooCommerce_Payments();
-		new WC_Admin_Notes_Marketing();
 	}
 
 	/**
