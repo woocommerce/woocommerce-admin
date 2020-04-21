1f36772bbd3d576b7392249e7da307947156213e
Rasmy Nguyen <rtnguyen87@gmail.com>
2020-04-21 15:54:49 +0900

    Fix logic to decide which connect method to render
    When the Stripe component renders for the first time, it
    always returns the manual connect method. This is because there is
    an additional call to requiresManualConfig() in getConnectStep which
    looks for values in activePlugins that won't be present on the initial
    component render.
    
    This removes this check and instead relies on the isPending state to
    help decide to render a connect method or wait for oAuth connectURL
    to finish fetching.

--- client/dashboard/task-list/tasks/payments/stripe.js
+++ client/dashboard/task-list/tasks/payments/stripe.js
@@ -29,7 +29,7 @@ class Stripe extends Component {
 			connectURL: null,
 			errorTitle: null,
 			errorMessage: null,
-			isPending: true,
+			isPending: false,
 		};
 
 		this.updateSettings = this.updateSettings.bind( this );
@@ -294,7 +294,12 @@ class Stripe extends Component {
 	}
 
 	getConnectStep() {
-		const { oAuthConnectFailed, connectURL, errorMessage } = this.state;
+		const {
+			connectURL,
+			errorMessage,
+			isPending,
+			oAuthConnectFailed,
+		} = this.state;
 		const connectStep = {
 			key: 'connect',
 			label: __( 'Connect your Stripe account', 'woocommerce-admin' ),
@@ -307,12 +312,11 @@ class Stripe extends Component {
 			};
 		}
 
-		if ( ! this.requiresManualConfig() ) {
-			// We may still be fetching the connect URL.
-			if ( ! oAuthConnectFailed && ! connectURL ) {
-				return connectStep;
-			}
+		if ( isPending ) {
+			return connectStep;
+		}
 
+		if ( ! oAuthConnectFailed && connectURL ) {
 			return {
 				...connectStep,
 				description: __(
@@ -360,10 +364,7 @@ export default compose(
 		const { getActivePlugins, isJetpackConnected } = select(
 			PLUGINS_STORE_NAME
 		);
-		const options = getOptions( [
-			'woocommerce_stripe_settings',
-			'woocommerce_default_country',
-		] );
+		const options = getOptions( [ 'woocommerce_stripe_settings' ] );
 		const stripeSettings = get(
 			options,
 			[ 'woocommerce_stripe_settings' ],
