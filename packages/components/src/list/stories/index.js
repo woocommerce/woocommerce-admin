1510487d9bb179eb70c2e4111d519570679c60f2
Matt Sherman <matt@jam123.com>
2020-04-27 10:14:53 -0400

    Before and after examples for List

--- packages/components/src/list/stories/index.js
+++ packages/components/src/list/stories/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import Gridicon from 'gridicons';
+
+/**
  * Internal dependencies
  */
 import List from '../';
@@ -23,6 +28,47 @@ export const Default = () => {
 		{
 			title: 'A list item with no action',
 			description: 'List item description text',
+		},
+		{
+			title: 'Click me!',
+			onClick: () => {
+				// eslint-disable-next-line no-alert
+				window.alert( 'List item clicked' );
+			},
+		},
+	];
+
+	return <List items={ listItems } />;
+};
+
+export const BeforeAndAfter = () => {
+	const listItems = [
+		{
+			before: <Gridicon icon="cart" />,
+			after: <Gridicon icon="chevron-right" />,
+			title: 'WooCommerce.com',
+			description: 'List item description text',
+			href: 'https://woocommerce.com',
+		},
+		{
+			before: <Gridicon icon="my-sites" />,
+			after: <Gridicon icon="chevron-right" />,
+			title: 'WordPress.org',
+			description: 'List item description text',
+			href: 'https://wordpress.org',
+		},
+		{
+			before: <Gridicon icon="link-break" />,
+			title: 'A list item with no action',
+			description: 'List item description text',
+		},
+		{
+			before: <Gridicon icon="notice" />,
+			title: 'Click me!',
+			onClick: () => {
+				// eslint-disable-next-line no-alert
+				window.alert( 'List item clicked' );
+			},
 		},
 	];
 
