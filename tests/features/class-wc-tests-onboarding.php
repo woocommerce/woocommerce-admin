ba0455a43426adf546578ea53d3dcc081f85d2f7
Timmy Crawford <timmydcrawford@gmail.com>
2020-04-28 13:59:44 -0700

    Updates per feedback.

--- tests/features/class-wc-tests-onboarding.php
+++ tests/features/class-wc-tests-onboarding.php
@@ -32,6 +32,10 @@ class WC_Tests_Onboarding extends WC_Unit_Test_Case {
 			'id'   => 4,
 			'slug' => 'poutine',
 		);
+		$theme4        = (object) array(
+			'noid'   => 5,
+			'noslug' => 'timbits',
+		);
 		$some_themes   = array( $theme1, $theme2, $theme3, $theme4 );
 		$sorted_themes = \Automattic\WooCommerce\Admin\Features\Onboarding::sort_woocommerce_themes( $some_themes );
 		$this->assertEquals( 'Storefront', $sorted_themes[0]->slug );
