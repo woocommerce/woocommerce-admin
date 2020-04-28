bc7edf9e5395e31b5715992ed4c4e7874e73afde
Paul Sealock <psealock@gmail.com>
2020-04-28 14:19:01 +1200

    handleScroll

--- client/homepage/layout.js
+++ client/homepage/layout.js
@@ -1,32 +1,75 @@
 /**
  * External dependencies
  */
-// import { Fragment } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-const Layout = () => {
-	return (
-		<div className="woocommerce-homepage">
-			<div className="woocommerce-homepage-column is-inbox">
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
+class Layout extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			inboxHeight: this.getInboxHeight(),
+		};
+
+		this.handleScroll = this.handleScroll.bind( this );
+	}
+
+	getInboxHeight() {
+		const scroll = window.scrollY;
+		const base = 90;
+		return `calc(100vh ${ base - scroll < 0 ? '+' : '-' } ${ Math.abs(
+			base - scroll
+		) }px)`;
+	}
+
+	componentDidMount() {
+		window.addEventListener( 'scroll', this.handleScroll );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'scroll', this.handleScroll );
+	}
+
+	handleScroll( e ) {
+		if ( e.target === window.document ) {
+			this.setState( {
+				inboxHeight: this.getInboxHeight(),
+			} );
+		}
+	}
+
+	render() {
+		const { inboxHeight } = this.state;
+		const inboxStyles = {
+			maxHeight: inboxHeight,
+		};
+		return (
+			<div className="woocommerce-homepage">
+				<div
+					className="woocommerce-homepage-column is-inbox"
+					style={ inboxStyles }
+				>
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+				</div>
+				<div className="woocommerce-homepage-column">
+					<div className="temp-content" />
+					<div className="temp-content" />
+					<div className="temp-content" />
+				</div>
 			</div>
-			<div className="woocommerce-homepage-column">
-				<div className="temp-content" />
-				<div className="temp-content" />
-				<div className="temp-content" />
-			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default Layout;
