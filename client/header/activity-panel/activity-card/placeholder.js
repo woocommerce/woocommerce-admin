d581a85a6d0fa4f7aaaf50b8e7756203cad72489
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-05-01 13:31:12 -0600

    Reduce external dependencies in ActivityCardPlaceholder.

--- client/header/activity-panel/activity-card/placeholder.js
+++ client/header/activity-panel/activity-card/placeholder.js
@@ -1,55 +1,55 @@
 /**
  * External dependencies
  */
-import classnames from 'classnames';
-import { Component } from '@wordpress/element';
 import PropTypes from 'prop-types';
-import { range } from 'lodash';
 
-class ActivityCardPlaceholder extends Component {
-	render() {
-		const {
-			className,
-			hasAction,
-			hasDate,
-			hasSubtitle,
-			lines,
-		} = this.props;
-		const cardClassName = classnames(
-			'woocommerce-activity-card is-loading',
-			className
-		);
+const ActivityCardPlaceholder = ( {
+	className,
+	hasAction,
+	hasDate,
+	hasSubtitle,
+	lines,
+} ) => {
+	const cardClasses = [
+		'woocommerce-activity-card',
+		'is-loading',
+	];
 
-		return (
-			<div className={ cardClassName } aria-hidden>
-				<span className="woocommerce-activity-card__icon">
-					<span className="is-placeholder" />
-				</span>
-				<div className="woocommerce-activity-card__header">
-					<div className="woocommerce-activity-card__title is-placeholder" />
-					{ hasSubtitle && (
-						<div className="woocommerce-activity-card__subtitle is-placeholder" />
-					) }
-					{ hasDate && (
-						<div className="woocommerce-activity-card__date">
-							<span className="is-placeholder" />
-						</div>
-					) }
-				</div>
-				<div className="woocommerce-activity-card__body">
-					{ range( lines ).map( ( i ) => (
-						<span className="is-placeholder" key={ i } />
-					) ) }
-				</div>
-				{ hasAction && (
-					<div className="woocommerce-activity-card__actions">
+	if ( className ) {
+		cardClasses.push( className );
+	}
+
+	const cardClassName = cardClasses.join( ' ' );
+
+	return (
+		<div className={ cardClassName } aria-hidden>
+			<span className="woocommerce-activity-card__icon">
+				<span className="is-placeholder" />
+			</span>
+			<div className="woocommerce-activity-card__header">
+				<div className="woocommerce-activity-card__title is-placeholder" />
+				{ hasSubtitle && (
+					<div className="woocommerce-activity-card__subtitle is-placeholder" />
+				) }
+				{ hasDate && (
+					<div className="woocommerce-activity-card__date">
 						<span className="is-placeholder" />
 					</div>
 				) }
 			</div>
-		);
-	}
-}
+			<div className="woocommerce-activity-card__body">
+				{ Array.from( { length: lines }, ( v, i ) => (
+					<span className="is-placeholder" key={ i } />
+				) ) }
+			</div>
+			{ hasAction && (
+				<div className="woocommerce-activity-card__actions">
+					<span className="is-placeholder" />
+				</div>
+			) }
+		</div>
+	);
+};
 
 ActivityCardPlaceholder.propTypes = {
 	className: PropTypes.string,
