93d4dc856ed7ba487708a2e2a0719ea36f530c6a
Matt Sherman <matt@jam123.com>
2020-05-04 15:24:15 -0400

    Add test for List item `data-list-item-tag`.

--- packages/components/src/list/test/index.js
+++ packages/components/src/list/test/index.js
@@ -78,4 +78,36 @@ describe( 'List', () => {
 		expect( renderedItems[ 2 ].dataset.linkType ).toBe( 'external' );
 		expect( renderedItems[ 3 ].dataset.linkType ).toBe( 'external' );
 	} );
+
+	it( 'should set `data-list-item-tag` on items', () => {
+		const listItems = [
+			{
+				title: 'Add products',
+				href: '/post-new.php?post_type=product',
+				linkType: 'wp-admin',
+				listItemTag: 'add-product',
+			},
+			{
+				title: 'Market my store',
+				href: '/admin.php?page=wc-admin&path=%2Fmarketing',
+				linkType: 'wc-admin',
+				listItemTag: 'marketing',
+			},
+			{
+				title: 'WooCommerce.com',
+				href: 'https://woocommerce.com',
+				linkType: 'external',
+				listItemTag: 'woocommerce.com-site',
+			},
+		];
+
+		const { container } = render( <List items={ listItems } /> );
+		const renderedItems = getAllByRole( container, 'menuitem' );
+
+		expect( renderedItems[ 0 ].dataset.listItemTag ).toBe( 'add-product' );
+		expect( renderedItems[ 1 ].dataset.listItemTag ).toBe( 'marketing' );
+		expect( renderedItems[ 2 ].dataset.listItemTag ).toBe(
+			'woocommerce.com-site'
+		);
+	} );
 } );
