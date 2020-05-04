05e684908fe8dcb12cc3e24f3dcd111fde5d2aa1
Matt Sherman <matt@jam123.com>
2020-05-04 14:34:48 -0400

    Add unit test for `data-link-type` being set on List items.

--- packages/components/src/list/test/index.js
+++ packages/components/src/list/test/index.js
@@ -43,6 +43,39 @@ describe( 'List', () => {
 		const { container } = render( <List items={ listItems } /> );
 
 		fireEvent.click( getAllByRole( container, 'menuitem' )[ 1 ] );
+
 		expect( clickHandler ).toHaveBeenCalled();
+	} );
+
+	it( 'should set `data-link-type` on items', () => {
+		const listItems = [
+			{
+				title: 'Add products',
+				href: '/post-new.php?post_type=product',
+				linkType: 'wp-admin',
+			},
+			{
+				title: 'Market my store',
+				href: '/admin.php?page=wc-admin&path=%2Fmarketing',
+				linkType: 'wc-admin',
+			},
+			{
+				title: 'WooCommerce.com',
+				href: 'https://woocommerce.com',
+				linkType: 'external',
+			},
+			{
+				title: 'WordPress.org',
+				href: 'https://wordpress.org',
+			},
+		];
+
+		const { container } = render( <List items={ listItems } /> );
+		const renderedItems = getAllByRole( container, 'menuitem' );
+
+		expect( renderedItems[ 0 ].dataset.linkType ).toBe( 'wp-admin' );
+		expect( renderedItems[ 1 ].dataset.linkType ).toBe( 'wc-admin' );
+		expect( renderedItems[ 2 ].dataset.linkType ).toBe( 'external' );
+		expect( renderedItems[ 3 ].dataset.linkType ).toBe( 'external' );
 	} );
 } );
