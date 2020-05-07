175b9832721cf0fe6521d21f1ef600246ae86397
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-05-07 10:21:28 -0600

    Specify dependencies in useSelect() call.

--- packages/data/src/user-preferences/use-user-preferences.js
+++ packages/data/src/user-preferences/use-user-preferences.js
@@ -14,34 +14,37 @@ import { useSelect } from '@wordpress/data';
 import { STORE_NAME } from './constants';
 
 export const useUserPreferences = ( preferenceKeys = [] ) => {
-	const { isRequesting, userPreferences } = useSelect( ( select ) => {
-		const {
-			getCurrentUser,
-			hasStartedResolution,
-			hasFinishedResolution,
-		} = select( STORE_NAME );
+	const { isRequesting, userPreferences } = useSelect(
+		( select ) => {
+			const {
+				getCurrentUser,
+				hasStartedResolution,
+				hasFinishedResolution,
+			} = select( STORE_NAME );
 
-		const user = getCurrentUser();
-		let wooMeta = user.woocommerce_meta || {};
+			const user = getCurrentUser();
+			let wooMeta = user.woocommerce_meta || {};
 
-		if ( preferenceKeys.length ) {
-			wooMeta = pick( wooMeta, preferenceKeys );
-		}
-
-		const userData = mapValues( wooMeta, ( data ) => {
-			if ( ! data || data.length === 0 ) {
-				return '';
+			if ( preferenceKeys.length ) {
+				wooMeta = pick( wooMeta, preferenceKeys );
 			}
-			return JSON.parse( data );
-		} );
 
-		return {
-			isRequesting:
-				hasStartedResolution( 'getCurrentUser' ) &&
-				! hasFinishedResolution( 'getCurrentUser' ),
-			userPreferences: userData,
-		};
-	} );
+			const userData = mapValues( wooMeta, ( data ) => {
+				if ( ! data || data.length === 0 ) {
+					return '';
+				}
+				return JSON.parse( data );
+			} );
+
+			return {
+				isRequesting:
+					hasStartedResolution( 'getCurrentUser' ) &&
+					! hasFinishedResolution( 'getCurrentUser' ),
+				userPreferences: userData,
+			};
+		},
+		[ preferenceKeys ]
+	);
 
 	return {
 		isRequesting,
