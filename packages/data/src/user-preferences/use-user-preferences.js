bc578e0e67270aad75c7cc4176d296fa59fcf507
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-05-08 13:34:17 -0600

    Add user prefs update method.

--- packages/data/src/user-preferences/use-user-preferences.js
+++ packages/data/src/user-preferences/use-user-preferences.js
@@ -1,12 +1,12 @@
 /**
  * External dependencies
  */
-import { mapValues } from 'lodash';
+import { mapValues, pick } from 'lodash';
 
 /**
  * WordPress dependencies
  */
-import { useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -16,9 +16,10 @@ import { STORE_NAME } from './constants';
 /**
  * Custom react hook for retrieving thecurrent user's WooCommerce preferences.
  * 
- * This is a wrapper around @wordpress/core-data's getCurrentUser().
+ * This is a wrapper around @wordpress/core-data's getCurrentUser() and saveUser().
  */
 export const useUserPreferences = () => {
+	// Use getCurrentUser() to get WooCommerce meta values.
 	const { isRequesting, userPreferences } = useSelect(
 		( select ) => {
 			const {
@@ -48,8 +49,47 @@ export const useUserPreferences = () => {
 		}
 	);
 
+	// Use saveUser() to update WooCommerce meta values.
+	const { receiveCurrentUser, saveUser } = useDispatch( STORE_NAME );
+
+	const updateUserPrefs = async ( { id, ...userData } ) => {
+		// Whitelist our meta fields.
+		const userDataFields = [
+			'categories_report_columns',
+			'coupons_report_columns',
+			'customers_report_columns',
+			'orders_report_columns',
+			'products_report_columns',
+			'revenue_report_columns',
+			'taxes_report_columns',
+			'variations_report_columns',
+			'dashboard_sections',
+			'dashboard_chart_type',
+			'dashboard_chart_interval',
+			'dashboard_leaderboard_rows',
+			'activity_panel_inbox_last_read',
+			'homepage_stats',
+		];
+
+		// Prep valid fields for update.
+		const metaData = mapValues(
+			pick( userData, userDataFields ),
+			JSON.stringify
+		);
+
+		// Update the `woocommerce_meta` user field.
+		const updatedUser = await saveUser( { id, woocommerce_meta: metaData } );
+
+		// @todo - handle error updating here.
+		// console.log( { updatedUser } );
+
+		// Propogate the updated User object to the store.
+		receiveCurrentUser( updatedUser );
+	};
+
 	return {
 		isRequesting,
 		...userPreferences,
+		updateUserPrefs,
 	};
 };
