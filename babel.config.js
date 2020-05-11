309bae84bcbca1e40d70ad26cd4f4059737c6cf5
Ron Rennick <ron@ronandandrea.com>
2020-05-11 16:09:34 -0300

    POC e2e testing using e2e-environment

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
+const { babelConfig: e2eBabelConfig } = require( '@woocommerce/e2e-environment' );
+
 module.exports = function( api ) {
 	api.cache( true );
 
 	return {
-		presets: [ '@wordpress/babel-preset-default' ],
+		...e2eBabelConfig,
+		presets: [
+			...e2eBabelConfig.presets,
+			'@wordpress/babel-preset-default',
+		],
 		plugins: [
 			'@babel/plugin-transform-async-to-generator',
 			'transform-class-properties',
