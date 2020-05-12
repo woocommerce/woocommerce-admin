6218a13057c777cc30f6bcb51b44662740b6822a
Ron Rennick <ron@ronandandrea.com>
2020-05-12 13:57:53 -0300

    update to 0.1.4, tweak configuration

--- tests/e2e/docker/initialize.sh
+++ tests/e2e/docker/initialize.sh
@@ -4,4 +4,5 @@ echo "Initializing WooCommerce E2E"
 
 wp plugin install woocommerce --activate
 wp theme install twentynineteen --activate
+wp plugin activate woocommerce-admin
 wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer --path=/var/www/html
