bf68fb6a01e22154a6f4e33267e57d60d6cfbcd0
Fernando Marichal <contacto@fernandomarichal.com>
2020-05-20 11:32:35 -0300

    Added test cases for getUnreadNotesCount and hasValidNotes

--- client/header/activity-panel/panels/inbox/test/index.js
+++ client/header/activity-panel/panels/inbox/test/index.js
@@ -7,6 +7,7 @@ import { shallow } from 'enzyme';
  * Internal dependencies
  */
 import InboxNoteCard from '../card';
+import { getUnreadNotesCount, hasValidNotes } from '../utils';
 
 describe( 'InboxNoteCard', () => {
 	const note = {
@@ -31,6 +32,7 @@ describe( 'InboxNoteCard', () => {
 		layout: 'plain',
 		image: '',
 		date_created_gmt: '2020-05-10T16:57:31',
+		is_deleted: false,
 	};
 	const lastRead = 1589285995243;
 
@@ -140,5 +142,66 @@ describe( 'InboxNoteCard', () => {
 			}
 		);
 		expect( card.children() ).toHaveLength( 0 );
+	} );
+} );
+
+const notes = [
+	{
+		id: 1,
+		date_created_gmt: '2019-05-10T16:57:31',
+		is_deleted: false,
+	},
+	{
+		id: 2,
+		date_created_gmt: '2020-05-12T16:57:31',
+		is_deleted: false,
+	},
+	{
+		id: 3,
+		date_created_gmt: '2020-05-14T16:57:31',
+		is_deleted: false,
+	},
+	{
+		id: 4,
+		date_created_gmt: '2020-05-15T16:57:31',
+		is_deleted: false,
+	},
+];
+
+describe( 'getUnreadNotesCount', () => {
+	const lastRead = 1589285995243;
+
+	test( 'should return 3, 1 of the notes was read', () => {
+		const unreadCount = getUnreadNotesCount( notes, lastRead );
+		expect( unreadCount ).toEqual( 3 );
+	} );
+
+	test( 'should return 2, 1 of the notes was read and 1 is deleted', () => {
+		notes[ 3 ].is_deleted = true;
+		const unreadCount = getUnreadNotesCount( notes, lastRead );
+		expect( unreadCount ).toEqual( 2 );
+	} );
+
+	test( 'should return 1, 2 of the notes were read and 1 is deleted', () => {
+		notes[ 1 ].date_created_gmt = '2020-05-05T16:57:31';
+		const unreadCount = getUnreadNotesCount( notes, lastRead );
+		expect( unreadCount ).toEqual( 1 );
+	} );
+
+	test( 'should return 0, 2 of the notes were read and 2 are deleted', () => {
+		notes[ 2 ].is_deleted = true;
+		const unreadCount = getUnreadNotesCount( notes, lastRead );
+		expect( unreadCount ).toEqual( 0 );
+	} );
+} );
+
+describe( 'hasValidNotes', () => {
+	test( 'should return true, 2 notes are deleted', () => {
+		expect( hasValidNotes( notes ) ).toBeTruthy();
+	} );
+	test( 'should return false, 4 notes are deleted', () => {
+		notes[ 0 ].is_deleted = true;
+		notes[ 3 ].is_deleted = true;
+		expect( hasValidNotes( notes ) ).toBeTruthy();
 	} );
 } );
