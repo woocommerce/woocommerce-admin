34f9332ee854381d7c3169806d34e8c9dd50e7d4
Jason Conroy <jason@findingsimple.com>
2020-06-04 16:25:13 +0930

    Convert RecommendedExtensionsItem to a functional component

--- client/marketing/components/recommended-extensions/item.js
+++ client/marketing/components/recommended-extensions/item.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { Component } from '@wordpress/element';
 import PropTypes from 'prop-types';
 
 /**
@@ -12,32 +11,33 @@ import { ProductIcon } from '../../components/';
 import { recordEvent } from 'lib/tracks';
 import { getInAppPurchaseUrl } from 'lib/in-app-purchase';
 
-class RecommendedExtensionsItem extends Component {
+const RecommendedExtensionsItem = ( {
+	title,
+	description,
+	icon,
+	url,
+} ) => {
 
-	onProductClick = () => {
-		const { title } = this.props;
+	const onProductClick = () => {
 		recordEvent( 'marketing_recommended_extension', { name: title } );
 	}
 
-	render() {
-		const { title, description, icon, url } = this.props;
-		const classNameBase = 'woocommerce-marketing-recommended-extensions-item';
-		const connectURL = getInAppPurchaseUrl( url );
+	const classNameBase = 'woocommerce-marketing-recommended-extensions-item';
+	const connectURL = getInAppPurchaseUrl( url );
 
-		return (
-			<a href={ connectURL }
-				className={ classNameBase }
-				onClick={ this.onProductClick }
-			>
-				<ProductIcon src={ icon } />
+	return (
+		<a href={ connectURL }
+			className={ classNameBase }
+			onClick={ onProductClick }
+		>
+			<ProductIcon src={ icon } />
 
-				<div className={ `${ classNameBase }__text` }>
-					<h4>{ title }</h4>
-					<p>{ description }</p>
-				</div>
-			</a>
-		)
-	}
+			<div className={ `${ classNameBase }__text` }>
+				<h4>{ title }</h4>
+				<p>{ description }</p>
+			</div>
+		</a>
+	)
 }
 
 RecommendedExtensionsItem.propTypes = {
