c759ec98c6ad9b5603a113e93ac44a6ef7d5a6a8
Rebecca Scott <me@becdetat.com>
2020-06-09 14:55:06 +1000

    Add "not setting up for client" rule

--- src/Notes/WC_Admin_Notes_First_Product.php
+++ src/Notes/WC_Admin_Notes_First_Product.php
@@ -34,6 +34,19 @@ class WC_Admin_Notes_First_Product {
 			return;
 		}
 
+		$onboarding_profile = get_option( 'woocommerce_onboarding_profile', array() );
+
+		// Confirm that $onboarding_profile is set.
+		if ( empty( $onboarding_profile ) ) {
+			return;
+		}
+
+		// Make sure that the person who filled out the OBW was not setting up
+		// the store for their customer/client.
+		if ( $onboarding_profile['setup_client'] ) {
+			return;
+		}
+
 		// Don't show if there are products.
 		$query    = new \WC_Product_Query(
 			array(
