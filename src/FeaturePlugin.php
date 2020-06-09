590e918f043826d0d590085e391de4cfd8a69dbc
Joshua Flowers <joshuatf@gmail.com>
2020-06-09 15:15:38 +0300

    Add note on first order creation

--- src/FeaturePlugin.php
+++ src/FeaturePlugin.php
@@ -18,6 +18,7 @@ use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Tracking_Opt_In;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_WooCommerce_Payments;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Install_JP_And_WCS_Plugins;
 use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Draw_Attention;
+use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_First_Order;
 use \Automattic\WooCommerce\Admin\RemoteInboxNotifications\RemoteInboxNotificationsEngine;
 
 /**
@@ -190,6 +191,7 @@ class FeaturePlugin {
 		new WC_Admin_Notes_WooCommerce_Payments();
 		new WC_Admin_Notes_Install_JP_And_WCS_Plugins();
 		new WC_Admin_Notes_Draw_Attention();
+		new WC_Admin_Notes_First_Order();
 
 		// Initialize RemoteInboxNotificationsEngine.
 		RemoteInboxNotificationsEngine::init();
