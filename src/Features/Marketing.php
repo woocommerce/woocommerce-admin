facae0080d23c1544da15481b3bdc6cb679da930
Jason Conroy <jason@findingsimple.com>
2020-06-11 08:55:39 +0930

    Move coupon menu, adding a note for customers

--- src/Features/Marketing.php
+++ src/Features/Marketing.php
@@ -15,6 +15,9 @@ use Automattic\WooCommerce\Admin\Loader;
  * Contains backend logic for the Marketing feature.
  */
 class Marketing {
+
+	use CouponsMovedTrait;
+
 	/**
 	 * Name of recommended plugins transient.
 	 *
@@ -60,7 +63,61 @@ class Marketing {
 
 		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
 		add_filter( 'woocommerce_shared_settings', array( $this, 'component_settings' ), 30 );
+
 		add_action( 'admin_enqueue_scripts', array( $this, 'maybe_add_marketing_coupon_script' ) );
+		add_action( 'woocommerce_register_post_type_shop_coupon', array( $this, 'move_coupons' ) );
+//		add_action( 'admin_head', array( $this, 'fix_coupon_menu_highlight' ), 99 );
+		add_action( 'admin_menu', array( $this, 'maybe_add_coupon_menu_redirect' ) );
+	}
+
+	/**
+	 * Maybe add menu item back in original spot to help people transition
+	 */
+	public function maybe_add_coupon_menu_redirect() {
+
+		if ( ! $this->should_display_legacy_menu() ) {
+			return;
+		}
+
+		add_submenu_page(
+			'woocommerce',
+			__( 'Coupons', 'woocommerce-admin' ),
+			__( 'Coupons', 'woocommerce-admin' ),
+			'manage_options',
+			'coupons-moved',
+			[ $this, 'coupon_menu_moved' ]
+		);
+	}
+
+	/**
+	 * Call back for transition menu item
+	 */
+	public function coupon_menu_moved() {
+		wp_safe_redirect( $this->get_legacy_coupon_url(), 301 );
+		exit();
+	}
+
+	/**
+	 * Modify registered post type shop_coupon
+	 *
+	 * @param array $args Array of post type parameters.
+	 *
+	 * @return array the filtered parameters.
+	 */
+	public function move_coupons( $args ) {
+		$args['show_in_menu'] = current_user_can( 'manage_woocommerce' ) ? $this->get_management_url( 'marketing' ) : true;
+		return $args;
+	}
+
+	/**
+	 * Undo WC modifications to $parent_file for 'shop_coupon'
+	 */
+	public function fix_coupon_menu_highlight() {
+		global $parent_file, $post_type;
+
+		if ( 'shop_coupon' === $post_type ) {
+			$parent_file = $this->get_management_url( 'marketing' ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride
+		}
 	}
 
 	/**
