33568002c01f85595dc582bfbf7a356fdb1b525e
Jason Conroy <jason@findingsimple.com>
2020-06-10 21:45:12 +0930

    Add new functions to get by category.

--- client/marketing/data/selectors.js
+++ client/marketing/data/selectors.js
@@ -6,11 +6,18 @@ export function getActivatingPlugins( state ) {
 	return state.activatingPlugins;
 }
 
+export function getRecommendedPluginsByCategory( state, category ) {
+	return state.recommendedPlugins[ category ] || [];
+}
+
 export function getRecommendedPlugins( state ) {
 	return state.recommendedPlugins;
+}
+
+export function getRecommendedPostsByCategory( state, category ) {
+	return state.blogPosts[ category ] || [];
 }
 
 export function getBlogPosts( state ) {
 	return state.blogPosts;
 }
-
