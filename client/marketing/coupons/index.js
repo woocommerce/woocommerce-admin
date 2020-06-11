13d03083d919fc3da70372cb71943f44d478abec
Jason Conroy <jason@findingsimple.com>
2020-06-11 11:20:31 +0930

    Translate title and descriptions

--- client/marketing/coupons/index.js
+++ client/marketing/coupons/index.js
@@ -1,4 +1,9 @@
 /**
+ * External depenencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * WooCommerce dependencies
  */
 import { getSetting } from '@woocommerce/wc-admin-settings';
@@ -13,11 +18,11 @@ import '../data';
 
 const CouponsOverview = () => {
 	const allowMarketplaceSuggestions = getSetting( 'allowMarketplaceSuggestions', false );
-
+__( 'WooCommerce knowledge base', 'woocommerce-admin' )
 	return (
 		<div className="woocommerce-marketing-coupons">
-			{ allowMarketplaceSuggestions && <RecommendedExtensions title="Recommended coupon extensions" description="Take your coupon marketing to the next level with our recommended coupon extensions." /> }
-			<KnowledgeBase category="coupons" description="Learn the ins and outs of successful coupon marketing from the experts at WooCommerce." />
+			{ allowMarketplaceSuggestions && <RecommendedExtensions title={ __( 'Recommended coupon extensions' ) } description={ __( 'Take your coupon marketing to the next level with our recommended coupon extensions.' ) } /> }
+			<KnowledgeBase category="coupons" description={ __( 'Learn the ins and outs of successful coupon marketing from the experts at WooCommerce.' ) } />
 		</div>
 	);
 };
