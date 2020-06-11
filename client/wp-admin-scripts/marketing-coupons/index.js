f7cbb306469f44eeeaa2caff07e88a336908778e
Jeremy Pry <jeremy.pry@gmail.com>
2020-06-11 16:27:53 -0400

    Remove extraneous string and add trailing new lines

--- client/wp-admin-scripts/marketing-coupons/index.js
+++ client/wp-admin-scripts/marketing-coupons/index.js
@@ -11,7 +11,6 @@ import CouponsOverview from '../../marketing/coupons';
 const postForm = document.getElementById( 'posts-filter' );
 
 if ( postForm ) {
-
 	const couponRoot = document.createElement( 'div' );
 	couponRoot.setAttribute( 'id', 'coupon-root' );
 
