aceeb84e263f2f7805b67cfbdff31755206db6c6
Matt Sherman <matt@jam123.com>
2020-06-12 16:51:03 -0400

    Make all tests wait until promises have completed, to avoid interaction between tests

--- packages/components/src/plugins/test/index.js
+++ packages/components/src/plugins/test/index.js
@@ -10,33 +10,62 @@ import { Button } from '@wordpress/components';
 
 import { Plugins } from '../index.js';
 
+// This function will cause Jest to wait until all Promises have completed
+// See: https://stackoverflow.com/questions/37408834/testing-with-reacts-jest-and-enzyme-when-simulated-clicks-call-a-function-that
+function tick() {
+	return new Promise( ( resolve ) => {
+		setTimeout( resolve, 0 );
+	} );
+}
+
 describe( 'Rendering', () => {
-	it( 'should render nothing when autoInstalling', () => {
+	it( 'should render nothing when autoInstalling', async () => {
+		const installPlugins = jest.fn().mockResolvedValue( { success: true } );
+		const activatePlugins = jest.fn().mockResolvedValue( {
+			success: true,
+			data: {
+				activated: [ 'jetpack' ],
+			},
+		} );
+		const createNotice = jest.fn();
+		const onComplete = jest.fn();
+
 		const pluginsWrapper = shallow(
 			<Plugins
 				autoInstall
 				pluginSlugs={ [ 'jetpack' ] }
-				onComplete={ () => {} }
+				onComplete={ onComplete }
+				installPlugins={ installPlugins }
+				activatePlugins={ activatePlugins }
+				createNotice={ createNotice }
 			/>
 		);
-		const buttons = pluginsWrapper.find( Button );
 
+		await tick();
+
+		const buttons = pluginsWrapper.find( Button );
 		expect( buttons.length ).toBe( 0 );
 	} );
 
-	it( 'should render a continue button when no pluginSlugs are given', () => {
+	it( 'should render a continue button when no pluginSlugs are given', async () => {
 		const pluginsWrapper = shallow(
 			<Plugins pluginSlugs={ [] } onComplete={ () => {} } />
 		);
+
+		await tick();
+
 		const continueButton = pluginsWrapper.find( Button );
 		expect( continueButton.length ).toBe( 1 );
 		expect( continueButton.text() ).toBe( 'Continue' );
 	} );
 
-	it( 'should render install and no thanks buttons', () => {
+	it( 'should render install and no thanks buttons', async () => {
 		const pluginsWrapper = shallow(
 			<Plugins pluginSlugs={ [ 'jetpack' ] } onComplete={ () => {} } />
 		);
+
+		await tick();
+
 		const buttons = pluginsWrapper.find( Button );
 		expect( buttons.length ).toBe( 2 );
 		expect( buttons.at( 0 ).text() ).toBe( 'Install & enable' );
@@ -55,6 +84,9 @@ describe( 'Installing and activating', () => {
 	const onComplete = jest.fn();
 
 	beforeEach( () => {
+		installPlugins.mockClear();
+		activatePlugins.mockClear();
+
 		pluginsWrapper = shallow(
 			<Plugins
 				pluginSlugs={ [ 'jetpack' ] }
@@ -68,7 +100,9 @@ describe( 'Installing and activating', () => {
 		const installButton = pluginsWrapper.find( Button ).at( 0 );
 		installButton.simulate( 'click' );
 
-		expect( installAndActivatePlugins ).toHaveBeenCalledWith( [ 'jetpack' ] );
+		expect( installAndActivatePlugins ).toHaveBeenCalledWith( [
+			'jetpack',
+		] );
 	} );
 
 	it( 'should call the onComplete callback', async () => {
@@ -93,6 +127,9 @@ describe( 'Installing and activating errors', () => {
 	const onError = jest.fn();
 
 	beforeEach( () => {
+		installPlugins.mockClear();
+		activatePlugins.mockClear();
+
 		pluginsWrapper = shallow(
 			<Plugins
 				pluginSlugs={ [ 'jetpack' ] }
@@ -113,6 +150,8 @@ describe( 'Installing and activating errors', () => {
 	it( 'should call the onError callback', async () => {
 		const installButton = pluginsWrapper.find( Button ).at( 0 );
 		installButton.simulate( 'click' );
+
+		await tick();
 
 		expect( onError ).toHaveBeenCalledWith( errors );
 	} );
