woocommerce/woocommerce-admin

commit 5b88ee0b82282a168d62fb8128375828a72ad2a1

Migrate user store to wp.data (#4505)
* Initial user preferences custom hook (wc meta).

* Organize dependencies.

* Specify dependencies in useSelect() call.

* Remove specifying preference keys.

There's currently no performance benefit to be had.

* Add HoC for hydrating current user data.

* Add user prefs update method.

* Export user prefs hook and HoC in data package.

* Handle error condition when updating user preferences.

* Use closure to get user ID for prefs update.

* Refactor analytics dashboard to use new user hook.

* Refactor ReportTable component to use new user hook.

* Move prop access to top of function component.

* Add todo for potential manual resolution logic.

* Refactor DashboardCharts component to use new user preferences hook.

* Move code out of functional component where possible.

* Fix chart interval query property.

* Refactor Leaderboards component to use new user preferences hook.

* Move code out of functional component where possible.

* Refactor Inbox panel to use user prefs hook.

@todo - updating last read isn't working.

* Use user prefs hook in StatsOverview component.

* Export user preferences store name.

* Use new store for unread inbox indicator.

* Hydrate current user data higher up in the app.

* Update "last read" timestamp in inbox panel.

* Fix StatsOverview test.

* Remove unused dispatch method from StorePerformance component.

* Remove now defunct user methods from wc-api spec.

* Add tests for isRequesting.

* JSON decode WC meta on updated user object.

* Test user prefs retrieval and save.

* Remove todo comment.

* Use user prefs hook in Jetpack install CTA on homepage.

client/dashboard/customizable.js
@@ -2,11 +2,10 @@
  * External dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
-import { Component, Fragment, Suspense, lazy } from '@wordpress/element';
+import { Fragment, Suspense, lazy, useState } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import { partial, get } from 'lodash';
 import { Dropdown, Button, Icon } from '@wordpress/components';
-import { withDispatch } from '@wordpress/data';
 import { applyFilters } from '@wordpress/hooks';
 import { Icon as WPIcon, plusCircleFilled } from '@wordpress/icons';
 
@@ -14,7 +13,10 @@ import { Icon as WPIcon, plusCircleFilled } from '@wordpress/icons';
  * WooCommerce dependencies
  */
 import { H, Spinner } from '@woocommerce/components';
-import { SETTINGS_STORE_NAME } from '@woocommerce/data';
+import {
+	SETTINGS_STORE_NAME,
+	useUserPreferences,
+} from '@woocommerce/data';
 
 /**
  * Internal dependencies
@@ -39,54 +41,68 @@ const TaskList = lazy( () =>
 const DASHBOARD_FILTERS_FILTER = 'woocommerce_admin_dashboard_filters';
 const filters = applyFilters( DASHBOARD_FILTERS_FILTER, [] );
 
-class CustomizableDashboard extends Component {
-	constructor( props ) {
-		super( props );
-
-		this.state = {
-			sections: this.mergeSectionsWithDefaults( props.userPrefSections ),
-		};
-
-		this.onMove = this.onMove.bind( this );
-		this.updateSection = this.updateSection.bind( this );
+const mergeSectionsWithDefaults = ( prefSections ) => {
+	if ( ! prefSections || prefSections.length === 0 ) {
+		return defaultSections;
 	}
+	const defaultKeys = defaultSections.map( ( section ) => section.key );
+	const prefKeys = prefSections.map( ( section ) => section.key );
+	const keys = new Set( [ ...prefKeys, ...defaultKeys ] );
+	const sections = [];
 
-	mergeSectionsWithDefaults( prefSections ) {
-		if ( ! prefSections || prefSections.length === 0 ) {
-			return defaultSections;
+	keys.forEach( ( key ) => {
+		const defaultSection = defaultSections.find(
+			( section ) => section.key === key
+		);
+		if ( ! defaultSection ) {
+			return;
 		}
-		const defaultKeys = defaultSections.map( ( section ) => section.key );
-		const prefKeys = prefSections.map( ( section ) => section.key );
-		const keys = new Set( [ ...prefKeys, ...defaultKeys ] );
-		const sections = [];
+		const prefSection = prefSections.find(
+			( section ) => section.key === key
+		);
 
-		keys.forEach( ( key ) => {
-			const defaultSection = defaultSections.find(
-				( section ) => section.key === key
-			);
-			if ( ! defaultSection ) {
-				return;
-			}
-			const prefSection = prefSections.find(
-				( section ) => section.key === key
-			);
-
-			sections.push( {
-				...defaultSection,
-				...prefSection,
-			} );
+		sections.push( {
+			...defaultSection,
+			...prefSection,
 		} );
+	} );
 
-		return sections;
+	return sections;
+}
+
+const CustomizableDashboard = ( {
+	defaultDateRange,
+	path,
+	query,
+	taskListComplete,
+	taskListHidden,
+} ) => {
+	const { isRequesting, updateUserPreferences, ...userPrefs } = useUserPreferences();
+	const [ dashSections, setSections ] = useState(
+		isRequesting
+			? false
+			: mergeSectionsWithDefaults( userPrefs.dashboard_sections )
+	);
+
+	// Update sections when the request is finished (and they weren't hydrated).
+	if ( ! isRequesting && dashSections === false ) {
+		setSections( mergeSectionsWithDefaults( userPrefs.dashboard_sections ) );
 	}
 
-	updateSections( newSections ) {
-		this.setState( { sections: newSections } );
-		this.props.updateCurrentUserData( { dashboard_sections: newSections } );
+	const sections = dashSections || defaultSections;
+
+	const isTaskListEnabled = isOnboardingEnabled() && ! taskListHidden;
+
+	const isDashboardShown =
+		! isTaskListEnabled || ( ! query.task && taskListComplete );
+
+	const updateSections = ( newSections ) => {
+		setSections( newSections );
+		updateUserPreferences( { dashboard_sections: newSections } );
 	}
 
-	updateSection( updatedKey, newSettings ) {
-		const newSections = this.state.sections.map( ( section ) => {
+	const updateSection = ( updatedKey, newSettings ) => {
+		const newSections = sections.map( ( section ) => {
 			if ( section.key === updatedKey ) {
 				return {
 					...section,
@@ -95,32 +111,31 @@ class CustomizableDashboard extends Component {
 			}
 			return section;
 		} );
-		this.updateSections( newSections );
+		updateSections( newSections );
 	}
 
-	onChangeHiddenBlocks( updatedKey ) {
+	const onChangeHiddenBlocks = ( updatedKey ) => {
 		return ( updatedHiddenBlocks ) => {
-			this.updateSection( updatedKey, {
+			updateSection( updatedKey, {
 				hiddenBlocks: updatedHiddenBlocks,
 			} );
 		};
 	}
 
-	onSectionTitleUpdate( updatedKey ) {
+	const onSectionTitleUpdate = ( updatedKey ) => {
 		return ( updatedTitle ) => {
 			recordEvent( 'dash_section_rename', { key: updatedKey } );
-			this.updateSection( updatedKey, { title: updatedTitle } );
+			updateSection( updatedKey, { title: updatedTitle } );
 		};
 	}
 
-	toggleVisibility( key, onToggle ) {
+	const toggleVisibility = ( key, onToggle ) => {
 		return () => {
 			if ( onToggle ) {
 				// Close the dropdown before setting state so an action is not performed on an unmounted component.
 				onToggle();
 			}
 			// When toggling visibility, place section at the end of the array.
-			const sections = [ ...this.state.sections ];
 			const index = sections.findIndex( ( s ) => key === s.key );
 			const toggledSection = sections.splice( index, 1 ).shift();
 			toggledSection.isVisible = ! toggledSection.isVisible;
@@ -134,12 +149,11 @@ class CustomizableDashboard extends Component {
 				} );
 			}
 
-			this.updateSections( sections );
+			updateSections( sections );
 		};
 	}
 
-	onMove( index, change ) {
-		const sections = [ ...this.state.sections ];
+	const onMove = ( index, change ) => {
 		const movedSection = sections.splice( index, 1 ).shift();
 		const newIndex = index + change;
 
@@ -153,7 +167,7 @@ class CustomizableDashboard extends Component {
 		) {
 			// Yes, lets insert.
 			sections.splice( newIndex, 0, movedSection );
-			this.updateSections( sections );
+			updateSections( sections );
 
 			const eventProps = {
 				key: movedSection.key,
@@ -162,12 +176,11 @@ class CustomizableDashboard extends Component {
 			recordEvent( 'dash_section_order_change', eventProps );
 		} else {
 			// No, lets try the next one.
-			this.onMove( index, change + change );
+			onMove( index, change + change );
 		}
 	}
 
-	renderAddMore() {
-		const { sections } = this.state;
+	const renderAddMore = () => {
 		const hiddenSections = sections.filter(
 			( section ) => section.isVisible === false
 		);
@@ -199,7 +212,7 @@ class CustomizableDashboard extends Component {
 								return (
 									<Button
 										key={ section.key }
-										onClick={ this.toggleVisibility(
+										onClick={ toggleVisibility(
 											section.key,
 											onToggle
 										) }
@@ -229,9 +242,7 @@ class CustomizableDashboard extends Component {
 		);
 	}
 
-	renderDashboardReports() {
-		const { query, path, defaultDateRange } = this.props;
-		const { sections } = this.state;
+	const renderDashboardReports = () => {
 		const { period, compare, before, after } = getDateParamsFromQuery(
 			query,
 			defaultDateRange
@@ -269,17 +280,17 @@ class CustomizableDashboard extends Component {
 								component={ section.component }
 								hiddenBlocks={ section.hiddenBlocks }
 								key={ section.key }
-								onChangeHiddenBlocks={ this.onChangeHiddenBlocks(
+								onChangeHiddenBlocks={ onChangeHiddenBlocks(
 									section.key
 								) }
-								onTitleUpdate={ this.onSectionTitleUpdate(
+								onTitleUpdate={ onSectionTitleUpdate(
 									section.key
 								) }
 								path={ path }
 								query={ query }
 								title={ section.title }
-								onMove={ partial( this.onMove, index ) }
-								onRemove={ this.toggleVisibility(
+								onMove={ partial( onMove, index ) }
+								onRemove={ toggleVisibility(
 									section.key
 								) }
 								isFirst={
@@ -296,46 +307,35 @@ class CustomizableDashboard extends Component {
 					}
 					return null;
 				} ) }
-				{ this.renderAddMore() }
+				{ renderAddMore() }
 			</Fragment>
 		);
 	}
 
-	render() {
-		const { query, taskListHidden, taskListComplete } = this.props;
-
-		const isTaskListEnabled =
-			isOnboardingEnabled() &&
-			! taskListHidden &&
-			! window.wcAdminFeatures.homepage;
-
-		const isDashboardShown =
-			! isTaskListEnabled || ( ! query.task && taskListComplete );
-
-		return (
-			<Fragment>
-				{ isTaskListEnabled && (
-					<Suspense fallback={ <Spinner /> }>
-						<TaskList query={ query } />
-					</Suspense>
-				) }
-				{ isDashboardShown && this.renderDashboardReports() }
-			</Fragment>
-		);
-	}
+	return (
+		<Fragment>
+			{ isTaskListEnabled && (
+				<Suspense fallback={ <Spinner /> }>
+					<TaskList
+						query={ query }
+						inline={ isDashboardShown }
+					/>
+				</Suspense>
+			) }
+			{ isDashboardShown && renderDashboardReports() }
+		</Fragment>
+	);
 }
 
 export default compose(
 	withSelect( ( select ) => {
-		const { getCurrentUserData, getOptions } = select( 'wc-api' );
+		const { getOptions } = select( 'wc-api' );
 
-		const userData = getCurrentUserData();
 		const { woocommerce_default_date_range: defaultDateRange } = select(
 			SETTINGS_STORE_NAME
 		).getSetting( 'wc_admin', 'wcAdminSettings' );
 
 		const withSelectData = {
-			userPrefSections: userData.dashboard_sections,
 			defaultDateRange,
 		};
 
@@ -353,12 +353,5 @@ export default compose(
 		}
 
 		return withSelectData;
-	} ),
-	withDispatch( ( dispatch ) => {
-		const { updateCurrentUserData } = dispatch( 'wc-api' );
-
-		return {
-			updateCurrentUserData,
-		};
 	} )
 )( CustomizableDashboard );

commit 255551ee7e2efc94a1313111b155cb08b50509fe

Disable task list on analytics dashboard when homepage is enabled.
Regression from 5b88ee0b82282a168d62fb8128375828a72ad2a1.

client/dashboard/customizable.js
@@ -73,6 +73,7 @@ const mergeSectionsWithDefaults = ( prefSections ) => {
 
 const CustomizableDashboard = ( {
 	defaultDateRange,
+	homepageEnabled,
 	path,
 	query,
 	taskListComplete,
@@ -92,7 +93,7 @@ const CustomizableDashboard = ( {
 
 	const sections = dashSections || defaultSections;
 
-	const isTaskListEnabled = isOnboardingEnabled() && ! taskListHidden;
+	const isTaskListEnabled = ! homepageEnabled && isOnboardingEnabled() && ! taskListHidden;
 
 	const isDashboardShown =
 		! isTaskListEnabled || ( ! query.task && taskListComplete );

src/Features/AnalyticsDashboard.php
@@ -45,6 +45,7 @@ class AnalyticsDashboard {
 		add_action( 'admin_menu', array( $this, 'register_page' ) );
 		// priority is 20 to run after https://github.com/woocommerce/woocommerce/blob/a55ae325306fc2179149ba9b97e66f32f84fdd9c/includes/admin/class-wc-admin-menus.php#L165.
 		add_action( 'admin_head', array( $this, 'update_link_structure' ), 20 );
+		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
 	}
 
 	/**
@@ -76,6 +77,18 @@ class AnalyticsDashboard {
 				'homepage_stats',
 			)
 		);
+	}
+
+	/**
+	 * Preload options to prime state of the application.
+	 *
+	 * @param array $options Array of options to preload.
+	 * @return array
+	 */
+	public function preload_options( $options ) {
+		$options[] = 'woocommerce_homescreen_enabled';
+
+		return $options;
 	}
 
 	/**