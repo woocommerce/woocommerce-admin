0b95057787b228267614fd6b0294f7d439421ebc
Darren Ethier <darren@roughsmootheng.in>
2020-06-27 16:07:06 -0400

    implement eslint-plugin (switch scripts to use wp-scripts etc)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended',
-		'prettier',
-		'prettier/react',
-		'plugin:testing-library/recommended',
-		'plugin:testing-library/react',
-		'plugin:jest-dom/recommended',
-	],
-	env: {
-		'jest/globals': true,
-	},
-	globals: {
-		wcSettings: true,
-	},
-	plugins: [ 'jest', 'jest-dom', 'testing-library' ],
-	rules: {
-		'@wordpress/dependency-group': 'off',
-		'valid-jsdoc': 'off',
-		radix: 'error',
-		yoda: [ 'error', 'never' ],
-	},
+	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
 };
