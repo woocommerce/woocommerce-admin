84401aaeeb6370b0579e73474bdc646b11501042
Darren Ethier <darren@roughsmootheng.in>
2020-06-27 17:00:17 -0400

    temporary convert rules to warnings until they are all handled

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
 	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
+	rules: {
+		// temporary conversion to warnings until the below are all handled.
+		'jsdoc/check-param-names': 'warn',
+		'jsdoc/require-param': 'warn',
+		'jsdoc/require-property': 'warn',
+		'jsdoc/no-undefined-types': 'warn',
+		'jsdoc/require-param-type': 'warn',
+		'jsdoc/require-property-description': 'warn',
+		'jsdoc/require-property-name': 'warn',
+		'jsdoc/check-property-names': 'warn',
+		'jsdoc/require-property-type': 'warn',
+		'@woocommerce/dependency-group': 'warn',
+	},
 };
