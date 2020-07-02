0b8dfc3864244c007e0c145694c08b9e3cc2ef3b
Timmy Crawford <timmydcrawford@gmail.com>
2020-07-02 15:36:09 -0700

    Check for permissions on note action to install plugin.

--- src/Notes/WC_Admin_Notes_WooCommerce_Payments.php
+++ src/Notes/WC_Admin_Notes_WooCommerce_Payments.php
@@ -130,7 +130,7 @@ class WC_Admin_Notes_WooCommerce_Payments {
 	 * @param WC_Admin_Note $note Note being acted upon.
 	 */
 	public function install( $note ) {
-		if ( self::NOTE_NAME === $note->get_name() ) {
+		if ( self::NOTE_NAME === $note->get_name() && current_user_can( 'install_plugins' ) ) {
 			$install_request = array( 'plugins' => self::PLUGIN_SLUG );
 			$installer       = new \Automattic\WooCommerce\Admin\API\Plugins();
 			$result          = $installer->install_plugins( $install_request );
