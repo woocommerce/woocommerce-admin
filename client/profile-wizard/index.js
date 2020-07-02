df03b0970c81626a7376f7ee6c8ff5733cd5adaf
Fernando Marichal <contacto@fernandomarichal.com>
2020-07-02 12:07:50 -0300

    Event name changed
    This commit renames the event name we were recording since it was wrong.

--- client/profile-wizard/index.js
+++ client/profile-wizard/index.js
@@ -88,7 +88,10 @@ class ProfileWizard extends Component {
 			step: this.getCurrentStep().key,
 		} );
 
-		updateProfileItems( { skipped: false, step: this.getCurrentStep().key } );
+		updateProfileItems( {
+			skipped: false,
+			step: this.getCurrentStep().key,
+		} );
 
 		// Track plugins if already installed.
 		if (
@@ -228,11 +231,12 @@ class ProfileWizard extends Component {
 
 	async skipProfiler() {
 		const { updateProfileItems } = this.props;
-		await updateProfileItems( { skipped: true, step: this.getCurrentStep().key } );
-		recordEvent( 'wcadmin_storeprofiler_store_details_skip' );
-		window.location = getAdminLink(
-			'admin.php?page=wc-admin'
-		);
+		await updateProfileItems( {
+			skipped: true,
+			step: this.getCurrentStep().key,
+		} );
+		recordEvent( 'storeprofiler_store_details_skip' );
+		window.location = getAdminLink( 'admin.php?page=wc-admin' );
 	}
 
 	render() {
