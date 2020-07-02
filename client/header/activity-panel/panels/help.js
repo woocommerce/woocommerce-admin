36490e53427a49cd216878cc36ad668b2b5711a1
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-07-01 21:10:53 -0400

    Add help links for other tasks.
    Still a WIP.

--- client/header/activity-panel/panels/help.js
+++ client/header/activity-panel/panels/help.js
@@ -18,7 +18,76 @@ import { List, Section } from '@woocommerce/components';
 import { recordEvent } from 'lib/tracks';
 // import './style.scss';
 
-function getProductItems() {
+function getAppearanceItems() {
+	return [
+		{
+			title: __( 'Showcase your products and tailor your shopping experience using Blocks', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/woocommerce-blocks/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Manage Store Notice, Catalog View and Product Images', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/woocommerce-customizer/?utm_source=help_panel',
+		},
+		{
+			title: __( 'How to choose and change a theme', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/choose-change-theme/?utm_source=help_panel',
+		},
+	];
+}
+
+function getPaymentsItems() {
+	return [
+		{
+			title: __( 'Which Payment Option is Right for Me?', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/premium-payment-gateway-extensions/?utm_source=help_panel',
+		},
+		// TODO: if WC Pay is an option.
+		{
+			title: __( 'WooCommerce Payments Start Up Guide', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/payments//?utm_source=help_panel',
+		},
+		// TODO: if WC Pay is an option.
+		{
+			title: __( 'WooCommerce Payments FAQs', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/documentation/woocommerce-payments/woocommerce-payments-faqs/?utm_source=help_panel',
+		},
+		// TODO: if Stripe is an option.
+		{
+			title: __( 'Stripe Setup and Configuration', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/stripe/?utm_source=help_panel',
+		},
+		// TODO: if PayPal is an option.
+		{
+			title: __( 'PayPal Checkout Setup and Configuration', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/paypal-express-checkout/?utm_source=help_panel',
+		},
+		// TODO: if Square is an option.
+		{
+			title: __( 'Square - Get started', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/woocommerce-square/?utm_source=help_panel',
+		},
+		// TODO: if Klarna is an option.
+		{
+			title: __( 'Klarna - Introduction', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/klarna-payments/?utm_source=help_panel',
+		},
+		// TODO: if Payfast is an option.
+		{
+			title: __( 'PayFast Setup and Configuration', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/payfast-payment-gateway/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Direct Bank Transfer (BACS)', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/bacs/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Cash on Delivery', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/cash-on-delivery/?utm_source=help_panel',
+		},
+	];
+}
+
+function getProductsItems() {
 	return [
 		{
 			title: __( 'Adding and Managing Products', 'woocommerce-admin' ),
@@ -39,11 +108,60 @@ function getProductItems() {
 	];
 }
 
+function getShippingItems() {
+	return [
+		{
+			title: __( 'Setting up Shipping Zones', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/setting-up-shipping-zones/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Core Shipping Options', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/documentation/plugins/woocommerce/getting-started/shipping/core-shipping-options/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Product Shipping Classes', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/product-shipping-classes/?utm_source=help_panel',
+		},
+		// TODO: (if WCS is active)
+		{
+			title: __( 'WooCommerce Shipping setup and configuration', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/woocommerce-services/#section-3/?utm_source=help_panel',
+		},
+		{
+			title: __( 'Learn more about configuring your shipping settings', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/documentation/plugins/woocommerce/getting-started/shipping/?utm_source=help_panel',
+		},
+	];
+}
+
+function getTaxItems() {
+	return [
+		{
+			title: __( 'Setting up Taxes in WooCommerce', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/setting-up-taxes-in-woocommerce/?utm_source=help_panel',
+		},
+		// TODO: (if WCS is active)
+		{
+			title: __( 'Automated Tax calculation using WooCommerce Services', 'woocommerce-admin' ),
+			link: 'https://docs.woocommerce.com/document/woocommerce-services/?utm_source=help_panel#section-10',
+		},
+	];
+}
+
 function getItems( taskName ) {
 	switch ( taskName ) {
 		case 'products':
+			return getProductsItems();
+		case 'appearance':
+			return getAppearanceItems();
+		case 'shipping':
+			return getShippingItems();
+		case 'tax':
+			return getTaxItems();
+		case 'payments':
+			return getPaymentsItems();
 		default:
-			return getProductItems();
+			return [];
 	}
 }
 
@@ -95,7 +213,7 @@ const HelpPanel = ( props ) => {
 HelpPanel.defaultProps = {
 	getSetting,
 	recordEvent,
-	taskName: 'products',
+	taskName: 'shipping',
 };
 
 export default HelpPanel;
