6f9e12a2bf49e12127af5c40c47d3eccb675e7e5
Timmy Crawford <timmydcrawford@gmail.com>
2020-07-06 15:09:40 -0700

    Adding feature-config.php directory to release

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -178,7 +178,7 @@ if ( ! function_exists( __NAMESPACE__ . '\autoloader' ) ) {
 /**
  * Prepare all the classes for autoloading.
  */
-function enqueue_packages_0d39d1a57231a14dc08b29868454cae0() {
+function enqueue_packages_aa7c057ec64d117715ad390aff9a965b() {
 	$class_map = require_once dirname( __FILE__ ) . '/composer/autoload_classmap_package.php';
 	foreach ( $class_map as $class_name => $class_info ) {
 		enqueue_package_class( $class_name, $class_info['version'], $class_info['path'] );
@@ -202,4 +202,4 @@ function enqueue_packages_0d39d1a57231a14dc08b29868454cae0() {
 		file_loader(); // Either WordPress is not loaded or plugin is doing it wrong. Either way we'll load the files so nothing breaks.
 	}
 }
-enqueue_packages_0d39d1a57231a14dc08b29868454cae0();
+enqueue_packages_aa7c057ec64d117715ad390aff9a965b();
