fb49d2762ea28e353c2cd39fc46fa9ce6588712e
Timmy Crawford <timmydcrawford@gmail.com>
2020-07-08 09:34:54 -0700

    Adding feature-config.php directory to release

--- includes/feature-config.php
+++ includes/feature-config.php
@@ -8,7 +8,7 @@ if ( ! function_exists( 'wc_admin_get_feature_config' ) ) {
 			'analytics' => true,
 			'analytics-dashboard' => true,
 			'analytics-dashboard/customizable' => true,
-			'coupons' => true,
+			'coupons' => false,
 			'devdocs' => false,
 			'marketing' => true,
 			'navigation' => false,
@@ -16,6 +16,7 @@ if ( ! function_exists( 'wc_admin_get_feature_config' ) ) {
 			'remote-inbox-notifications' => false,
 			'shipping-label-banner' => true,
 			'store-alerts' => true,
+			'unminified-js' => false,
 			'wcpay' => true,
 			'homescreen' => true,
 		);
