95d81e297fa1dc0f347412071441fd59ed8303ae
Jason Conroy <jason@findingsimple.com>
2020-07-15 00:16:55 +0930

    Add hover effect for coupon icons

--- client/marketing/components/product-icon/icons/library/bulb.js
+++ client/marketing/components/product-icon/icons/library/bulb.js
@@ -5,9 +5,9 @@ import { SVG, Path } from '@wordpress/primitives';
 
 const bulb = (
 	<SVG width="36" height="36" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<Path stroke="#3858E9" stroke-width="1.5" d="M15 21.75h6"/>
-		<Path d="M21.5355 18.5356c1.9527-1.9527 1.9527-5.1185 0-7.0711-1.9526-1.95263-5.1184-1.95263-7.071 0-1.9527 1.9526-1.9527 5.1184 0 7.0711" stroke="#3858E9" stroke-width="1.5" stroke-linecap="round"/>
-		<Path stroke="#3858E9" stroke-width="1.5" d="M16 24.75h4"/>
+		<Path class="stroke" stroke="#3858E9" stroke-width="1.5" d="M15 21.75h6"/>
+		<Path class="stroke" stroke="#3858E9" stroke-width="1.5" stroke-linecap="round" d="M21.5355 18.5356c1.9527-1.9527 1.9527-5.1185 0-7.0711-1.9526-1.95263-5.1184-1.95263-7.071 0-1.9527 1.9526-1.9527 5.1184 0 7.0711"/>
+		<Path class="stroke" stroke="#3858E9" stroke-width="1.5" d="M16 24.75h4"/>
 	</SVG>
 );
 
