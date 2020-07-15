7c5432ce3ca833bbee0615a4da20bf0cb4e90540
Jason Conroy <jason@findingsimple.com>
2020-07-16 05:32:06 +0930

    Adjust icon export

--- client/marketing/components/product-icon/icons/index.js
+++ client/marketing/components/product-icon/icons/index.js
@@ -3,22 +3,26 @@ export { default as blank } from './library/blank';
 // Amazon & Ebay Integration
 export { default as amazonEbayIntegration } from './library/amazon-ebay';
 export { default as woocommerceAmazonEbayIntegration } from './library/amazon-ebay';
+
 // AutomateWoo
 export { default as automatewoo } from './library/automatewoo';
 export { default as automatewooAlt } from './library/automatewoo-alt';
+
 // Facebook
 export { default as facebook } from './library/facebook';
 export { default as facebookForWoocommerce } from './library/facebook';
+
 // Google Ads
 export { default as googleAds } from './library/google';
 export { default as klikenMarketingForGoogle } from './library/google';
+
 // Hubspot
 export { default as hubspotForWoocommerce } from './library/hubspot';
+
 // Mailchimp
 export { default as mailchimpForWoocommerce } from './library/mailchimp';
+
 // Coupons
 export { default as woocommerceFreeGiftCoupons } from './library/gift';
-export { people as woocommerceGroupCoupons } from '@wordpress/icons';
 export { default as woocommerceSmartCoupons } from './library/tip';
-export { currencyDollar as woocommerceStoreCredit } from '@wordpress/icons';
-export { link as woocommerceUrlCoupons } from '@wordpress/icons';
+export { people as woocommerceGroupCoupons, currencyDollar as woocommerceStoreCredit, link as woocommerceUrlCoupons  } from '@wordpress/icons';
