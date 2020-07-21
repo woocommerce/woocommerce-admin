6fb151b505275aaa2f395df76b723415619b9e75
Rebecca Scott <me@becdetat.com>
2020-07-21 18:20:51 +1000

    Stub out the steps

--- src/Features/OnboardingSetUpShipping.php
+++ src/Features/OnboardingSetUpShipping.php
@@ -28,5 +28,37 @@ class OnboardingSetUpShipping {
 	 * Set up shipping.
 	 */
 	public static function on_onboarding_profile_completed() {
+		// Only do this if there are no existing shipping methods.
+
+		self::turn_on_printing_shipping_labels();
+		self::set_up_free_local_shipping();
+		self::disable_international_shipping();
+		self::trigger_review_shipping_settings_note();
+		self::track_shipping_automatically_set_up_event();
 	}
+
+	/**
+	 * Turn on printing of shipping labels.
+	 */
+	private static function turn_on_printing_shipping_labels() {}
+
+	/**
+	 * Set up free local shipping.
+	 */
+	private static function set_up_free_local_shipping() {}
+
+	/**
+	 * Disable international shipping.
+	 */
+	private static function disable_international_shipping() {}
+
+	/**
+	 * Trigger the "Review your shipping settings" admin note.
+	 */
+	private static function trigger_review_shipping_settings_note() {}
+
+	/**
+	 * Track the shipping_automatically_set_up event.
+	 */
+	private static function track_shipping_automatically_set_up_event() {}
 }
