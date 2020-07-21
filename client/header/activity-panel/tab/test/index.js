bfeff01a659bd9e22f085f02c73fa679e13cc1cd
Sam Seay <samueljseay@gmail.com>
2020-07-21 17:10:13 +1200

    Add some more tab tests

--- client/header/activity-panel/tab/test/index.js
+++ client/header/activity-panel/tab/test/index.js
@@ -1,13 +1,14 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import PagesIcon from 'gridicons/dist/pages';
 
 import { Tab } from '../index';
 
 describe( 'ActivityPanel Tab', () => {
-	it( 'displays an icon, title and unread status based on props', () => {
+	it( 'displays a title and unread status based on props', () => {
 		const { getByText } = render(
 			<Tab
-				icon={ <PagesIcon /> }
+				icon={ null }
 				title={ 'Hello World' }
 				name={ 'overview' }
 				unread
@@ -22,9 +23,113 @@ describe( 'ActivityPanel Tab', () => {
 		expect( getByText( 'unread activity' ) ).not.toBeNull();
 	} );
 
-	// it( 'does not display unread status if unread is false' );
+	it( 'renders the node passed to icon', () => {
+		const { getByText } = render(
+			<Tab
+				icon={ <div>Fake icon</div> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread
+				selected
+				isPanelOpen
+				index={ 0 }
+				onTabClick={ () => {} }
+			/>
+		);
 
-	// it( 'is tabbable if its closed or if its closed and the first item', () => {} );
+		expect( getByText( 'Fake icon' ) ).not.toBeNull();
+	} );
+
+	it( 'does not display unread status if unread is false', () => {
+		const { queryByText } = render(
+			<Tab
+				icon={ <PagesIcon /> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread={ false }
+				selected
+				isPanelOpen
+				index={ 0 }
+				onTabClick={ () => {} }
+			/>
+		);
+
+		expect( queryByText( 'unread activity' ) ).toBeNull();
+	} );
+
+	it( 'is tabbable if its selected or if its closed and the first item', () => {
+		const { getByRole, rerender } = render(
+			<Tab
+				icon={ <PagesIcon /> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread={ false }
+				selected
+				isPanelOpen={ false }
+				index={ 1 }
+				onTabClick={ () => {} }
+			/>
+		);
+
+		let tab = getByRole( 'tab' );
+
+		// Tab index is set to null if its the currently selected item, or the panel is closed and the item is the first item.
+		expect( tab ).not.toHaveAttribute( 'tabindex' );
+
+		rerender(
+			<Tab
+				icon={ <PagesIcon /> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread={ false }
+				selected={ false }
+				isPanelOpen={ false }
+				index={ 0 }
+				onTabClick={ () => {} }
+			/>
+		);
+
+		tab = getByRole( 'tab' );
+
+		expect( tab ).not.toHaveAttribute( 'tabindex' );
+	} );
+
+	it( 'is not tabbable if its not selected, or its open', () => {
+		const { getByRole, rerender } = render(
+			<Tab
+				icon={ <PagesIcon /> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread={ false }
+				selected={ false }
+				isPanelOpen={ false }
+				index={ 1 }
+				onTabClick={ () => {} }
+			/>
+		);
+
+		let tab = getByRole( 'tab' );
+
+		// Tab index is not set if its the currently selected item, or the panel is closed and the item is the first item.
+		expect( tab ).toHaveAttribute( 'tabindex', '-1' );
+
+		rerender(
+			<Tab
+				icon={ <PagesIcon /> }
+				title={ 'Hello World' }
+				name={ 'overview' }
+				unread={ false }
+				selected={ false }
+				isPanelOpen={ true }
+				index={ 1 }
+				onTabClick={ () => {} }
+			/>
+		);
+
+		tab = getByRole( 'tab' );
+
+		expect( tab ).toHaveAttribute( 'tabindex', '-1' );
+	} );
 
 	// it( 'calls the onTabClick handler if a tab is clicked', () => {} );
 
