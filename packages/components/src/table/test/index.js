9716856b016da82b635b584898dbca94860f832f
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-08-10 13:04:14 -0400

    Add test coverage for default callback props.

--- packages/components/src/table/test/index.js
+++ packages/components/src/table/test/index.js
@@ -3,6 +3,7 @@
  */
 import { render, screen } from '@testing-library/react';
 import { toHaveClass } from '@testing-library/jest-dom/matchers';
+import userEvent from '@testing-library/user-event';
 
 /**
  * Internal dependencies
@@ -44,5 +45,33 @@ describe( 'TableCard', () => {
 		);
 
 		expect( screen.getByRole( 'group' ) ).not.toHaveClass( 'is-loading' );
+	} );
+
+	it( 'should not error with default callback props', () => {
+		render(
+			<TableCard
+				title="Revenue"
+				headers={ mockHeaders }
+				isLoading={ false }
+				rows={ mockData }
+				rowsPerPage={ 1 }
+				totalRows={ 5 }
+			/>
+		);
+
+		// Trigger a query change (next page).
+		userEvent.click(
+			screen.getByLabelText( 'Next Page', { selector: 'button' } )
+		);
+
+		// Trigger a column change.
+		userEvent.click(
+			screen.getByTitle( 'Choose which values to display', {
+				selector: 'button',
+			} )
+		);
+
+		// We shouldn't get here if an error occurred.
+		expect( true ).toBe( true );
 	} );
 } );
