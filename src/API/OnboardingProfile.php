2b24747fe7c6fdb26f608031b3d44a8d52a6bd7b
Joshua T Flowers <joshuatf@gmail.com>
2020-08-12 15:57:55 +0300

    Fix industry args type in REST API (#4974)
    * Fix industry args type in REST API
    
    * Only allow object for industry param
    
    * Fix onboarding industry object shape in test case.
    
    Co-authored-by: Jeff Stieler <jeff.m.stieler@gmail.com>

--- src/API/OnboardingProfile.php
+++ src/API/OnboardingProfile.php
@@ -249,7 +249,7 @@ class OnboardingProfile extends \WC_REST_Data_Controller {
 				'readonly'          => true,
 				'validate_callback' => 'rest_validate_request_arg',
 				'items'             => array(
-					'type' => 'json',
+					'type' => 'object',
 				),
 			),
 			'product_types'       => array(
