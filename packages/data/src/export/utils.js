95496107bdfae806692c109120a8521ab6ac5563
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-08-17 10:15:20 -0400

    Use getResourceName() util for more DRYness.

--- packages/data/src/export/utils.js
+++ packages/data/src/export/utils.js
@@ -3,9 +3,14 @@
  */
 import crypto from 'crypto';
 
+/**
+ * Internal dependencies
+ */
+import { getResourceName } from '../utils';
+
 export const hashExportArgs = ( args ) => {
 	return crypto
 		.createHash( 'md5' )
-		.update( JSON.stringify( args ) )
+		.update( getResourceName( 'export', args ) )
 		.digest( 'hex' );
 };
