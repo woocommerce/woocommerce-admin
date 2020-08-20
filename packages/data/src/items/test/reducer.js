4832f6130de90b442e77a246e6ff7ea45e7d3bbb
Paul Sealock <psealock@gmail.com>
2020-08-20 12:07:55 +1200

    tests

--- packages/data/src/items/test/reducer.js
+++ packages/data/src/items/test/reducer.js
@@ -3,59 +3,60 @@
  */
 import reducer from '../reducer';
 import TYPES from '../action-types';
+import { getResourceName } from '../../utils';
 
 const defaultState = {
-	reviews: {},
+	items: {},
 	errors: {},
 	data: {},
 };
 
-describe( 'reviews reducer', () => {
+describe( 'items reducer', () => {
 	it( 'should return a default state', () => {
 		const state = reducer( undefined, {} );
 		expect( state ).toEqual( defaultState );
 		expect( state ).not.toBe( defaultState );
 	} );
 
-	it( 'should handle UPDATE_REVIEWS', () => {
-		const reviews = [
-			{ id: 1, review: 'Yum!' },
-			{ id: 2, review: 'Dynamite!' },
+	it( 'should handle UPDATE_ITEMS', () => {
+		const items = [
+			{ id: 1, title: 'Yum!' },
+			{ id: 2, title: 'Dynamite!' },
 		];
 		const totalCount = 45;
 		const query = { status: 'flavortown' };
+		const itemType = 'BBQ';
 		const state = reducer( defaultState, {
-			type: TYPES.UPDATE_REVIEWS,
-			reviews,
+			type: TYPES.UPDATE_ITEMS,
+			items,
+			itemType,
 			query,
 			totalCount,
 		} );
 
-		const stringifiedQuery = JSON.stringify( query );
+		const resourceName = getResourceName( itemType, query );
 
-		expect( state.reviews[ stringifiedQuery ].data ).toHaveLength( 2 );
-		expect(
-			state.reviews[ stringifiedQuery ].data.includes( 1 )
-		).toBeTruthy();
-		expect(
-			state.reviews[ stringifiedQuery ].data.includes( 2 )
-		).toBeTruthy();
+		expect( state.items[ resourceName ].data ).toHaveLength( 2 );
+		expect( state.items[ resourceName ].data.includes( 1 ) ).toBeTruthy();
+		expect( state.items[ resourceName ].data.includes( 2 ) ).toBeTruthy();
 
-		expect( state.reviews[ stringifiedQuery ].totalCount ).toBe( 45 );
-		expect( state.data[ '1' ] ).toBe( reviews[ 0 ] );
-		expect( state.data[ '2' ] ).toBe( reviews[ 1 ] );
+		expect( state.items[ resourceName ].totalCount ).toBe( 45 );
+		expect( state.data[ resourceName ][ '1' ] ).toBe( items[ 0 ] );
+		expect( state.data[ resourceName ][ '2' ] ).toBe( items[ 1 ] );
 	} );
 
 	it( 'should handle SET_ERROR', () => {
 		const query = { status: 'flavortown' };
+		const itemType = 'BBQ';
+		const resourceName = getResourceName( itemType, query );
 		const error = 'Baaam!';
 		const state = reducer( defaultState, {
 			type: TYPES.SET_ERROR,
+			itemType,
 			query,
 			error,
 		} );
 
-		const stringifiedQuery = JSON.stringify( query );
-		expect( state.errors[ stringifiedQuery ] ).toBe( error );
+		expect( state.errors[ resourceName ] ).toBe( error );
 	} );
 } );
