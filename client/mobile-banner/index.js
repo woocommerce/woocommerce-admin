a687b692174041017702b6809b3a4880e5d6e3f9
Sam Seay <samueljseay@gmail.com>
2020-08-20 16:00:45 +1200

    Add analytics and dismissal button.

--- client/mobile-banner/index.js
+++ client/mobile-banner/index.js
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import React, { useEffect } from '@wordpress/element';
-import { Button } from '@wordpress/components';
+import React, { useEffect, useState } from '@wordpress/element';
+import { Button, Icon } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -11,8 +11,10 @@ import { __ } from '@wordpress/i18n';
 import { platform, ANDROID_PLATFORM } from '../lib/platform';
 import { AppIcon } from './app-icon';
 import './style.scss';
+import { recordEvent } from '../lib/tracks';
 
 const SHOW_APP_BANNER_MODIFIER_CLASS = 'woocommerce-layout__show-app-banner';
+const TRACKING_EVENT_NAME = 'wcadmin_mobile_banner_click';
 
 export const MobileAppBanner = () => {
 	useEffect( () => {
@@ -31,18 +33,26 @@ export const MobileAppBanner = () => {
 
 		return () => {
 			if ( layout ) {
-				// This is a hack to allow the mobile banner to work in the context of the header which is
-				// position fixed. This can be refactored away when we move away from the activity panel
-				// in future.
 				layout.classList.remove( SHOW_APP_BANNER_MODIFIER_CLASS );
 			}
 		};
 	}, [] );
 
+	const [ isDismissed, setDismissed ] = useState( false );
+
 	// On iOS we use the Smart App Banner meta tag so only display this on Android.
-	if ( platform() === ANDROID_PLATFORM ) {
+	if ( platform() === ANDROID_PLATFORM && ! isDismissed ) {
 		return (
 			<div className="woocommerce-mobile-app-banner">
+				<Icon
+					icon="no-alt"
+					onClick={ () => {
+						setDismissed( true );
+						recordEvent( TRACKING_EVENT_NAME, {
+							button: 'dismiss',
+						} );
+					} }
+				/>
 				<AppIcon />
 				<div className="woocommerce-mobile-app-banner__description">
 					<p className="woocommerce-mobile-app-banner__description__text">
@@ -62,6 +72,11 @@ export const MobileAppBanner = () => {
 				<Button
 					href="http://play.google.com/store/apps/details?id=com.woocommerce.android"
 					isSecondary
+					onClick={ () => {
+						recordEvent( TRACKING_EVENT_NAME, {
+							button: 'install',
+						} );
+					} }
 				>
 					{ __( 'Install', 'woocommerce-admin' ) }
 				</Button>
