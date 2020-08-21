f928183dde39287efb33c84bdbd3722a6488ed31
Sam Seay <samueljseay@gmail.com>
2020-08-21 16:17:55 +1200

    Refactor the Header component from class to function.

--- client/header/test/index.js
+++ client/header/test/index.js
@@ -1,12 +1,12 @@
 /**
  * External dependencies
  */
-import { shallow } from 'enzyme';
+import { render, fireEvent } from '@testing-library/react';
 
 /**
  * Internal dependencies
  */
-import Header from '../index.js';
+import { Header } from '../index.js';
 
 const encodedBreadcrumb = [
 	[ 'admin.php?page=wc-settings', 'Settings' ],
@@ -14,16 +14,38 @@ const encodedBreadcrumb = [
 ];
 
 describe( 'Header', () => {
-	test( 'should render decoded breadcrumb name', () => {
-		const header = shallow(
-			<Header sections={ encodedBreadcrumb } isEmbedded={ true } />,
-			{
-				disableLifecycleMethods: true,
+	beforeEach( () => {
+		// Mock RAF to be synchronous for testing
+		jest.spyOn( window, 'requestAnimationFrame' ).mockImplementation(
+			( cb ) => {
+				cb();
 			}
 		);
-		expect( header.text().includes( 'Accounts &amp; Privacy' ) ).toBe(
-			false
+
+		// Disable the ActivityPanel so it isn't tested here
+		window.wcAdminFeatures[ 'activity-panels' ] = false;
+	} );
+
+	afterEach( () => {
+		window.requestAnimationFrame.mockRestore();
+	} );
+
+	it( 'should render decoded breadcrumb name', () => {
+		const { queryByText } = render(
+			<Header sections={ encodedBreadcrumb } isEmbedded={ true } />
 		);
-		expect( header.text().includes( 'Accounts & Privacy' ) ).toBe( true );
+		expect( queryByText( 'Accounts &amp; Privacy' ) ).toBe( null );
+		expect( queryByText( 'Accounts & Privacy' ) ).not.toBe( null );
+	} );
+
+	it( 'should only have the is-scrolled class if the page is scrolled', () => {
+		const { container } = render(
+			<Header sections={ encodedBreadcrumb } isEmbedded={ false } />
+		);
+
+		const topLevelElement = container.firstChild;
+		expect( topLevelElement.classList ).not.toContain( 'is-scrolled' );
+		fireEvent.scroll( window, { target: { scrollY: 200 } } );
+		expect( topLevelElement.classList ).toContain( 'is-scrolled' );
 	} );
 } );
