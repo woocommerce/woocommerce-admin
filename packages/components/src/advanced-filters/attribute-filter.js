bcc4476a6daaa7ebae53c73903fcc30456b00767
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-08-21 18:22:46 -0400

    Add attribute term selection control.

--- packages/components/src/advanced-filters/attribute-filter.js
+++ packages/components/src/advanced-filters/attribute-filter.js
@@ -6,7 +6,7 @@ import { SelectControl as Select, Spinner } from '@wordpress/components';
 import { partial } from 'lodash';
 import interpolateComponents from 'interpolate-components';
 import classnames from 'classnames';
-import { useEffect, useState } from '@wordpress/element';
+import { Fragment, useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
 /**
@@ -23,7 +23,7 @@ const AttributeFilter = ( props ) => {
 	const { key: filterKey, rule, value } = filter;
 	const { labels, rules } = config;
 
-	const [ attributes, setAttributes ] = useState();
+	const [ attributes, setAttributes ] = useState( [] );
 
 	// Fetch all product attributes on mount.
 	useEffect( () => {
@@ -31,22 +31,48 @@ const AttributeFilter = ( props ) => {
 			path: '/wc/v3/products/attributes',
 		} )
 			.then( ( attrs ) =>
-				attrs.map( ( { name, slug } ) => ( {
-					key: slug,
+				attrs.map( ( { id, name } ) => ( {
+					key: id.toString(),
 					label: name,
 				} ) )
 			)
 			.then( setAttributes );
 	}, [] );
 
-	const [ selectedAttribute, setSelectedAttribute ] = useState();
+	const [ selectedAttribute, setSelectedAttribute ] = useState(
+		Array.isArray( value ) ? value[ 0 ] : ''
+	);
 
 	// Set selected attribute from filter value (in query string).
 	useEffect( () => {
-		if ( Array.isArray( value ) && value[ 0 ] !== selectedAttribute ) {
+		if ( Array.isArray( value ) ) {
 			setSelectedAttribute( value[ 0 ] );
 		}
-	}, [ value, selectedAttribute ] );
+	}, [ value ] );
+
+	const [ attributeTerms, setAttributeTerms ] = useState( [] );
+
+	// Fetch all product attributes on mount.
+	useEffect( () => {
+		if ( ! selectedAttribute ) {
+			return;
+		}
+		setAttributeTerms( [] );
+		apiFetch( {
+			path: `/wc/v3/products/attributes/${ selectedAttribute }/terms`,
+		} )
+			.then( ( terms ) =>
+				terms.map( ( { id, name } ) => ( {
+					key: id.toString(),
+					label: name,
+				} ) )
+			)
+			.then( setAttributeTerms );
+	}, [ selectedAttribute ] );
+
+	const [ selectedAttributeTerm, setSelectedAttributeTerm ] = useState(
+		Array.isArray( value ) ? value[ 1 ] || '' : ''
+	);
 
 	const screenReaderText = getScreenReaderText( filter, config );
 
@@ -86,21 +112,40 @@ const AttributeFilter = ( props ) => {
 								) }
 							>
 								{ attributes ? (
-									<SelectControl
-										label="Attribute name"
-										isSearchable
-										showAllOnFocus
-										options={ attributes }
-										selected={ selectedAttribute || '' }
-										onChange={ ( attr ) => {
-											setSelectedAttribute( attr );
-											onFilterChange(
-												filterKey,
-												'value',
-												[ attr ]
-											);
-										} }
-									/>
+									<Fragment>
+										<SelectControl
+											label="Attribute name"
+											isSearchable
+											showAllOnFocus
+											options={ attributes }
+											selected={ selectedAttribute }
+											onChange={ ( attr ) => {
+												setSelectedAttribute( attr );
+												onFilterChange(
+													filterKey,
+													'value',
+													[ attr ]
+												);
+											} }
+										/>
+										<SelectControl
+											label="Attribute value"
+											isSearchable
+											showAllOnFocus
+											options={ attributeTerms }
+											selected={ selectedAttributeTerm }
+											onChange={ ( term ) => {
+												setSelectedAttributeTerm(
+													term
+												);
+												onFilterChange(
+													filterKey,
+													'value',
+													[ selectedAttribute, term ]
+												);
+											} }
+										/>
+									</Fragment>
 								) : (
 									<Spinner />
 								) }
