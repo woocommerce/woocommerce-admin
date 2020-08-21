8a8f2c05d663038a0cbc18b0f4f5bf63f4ae862f
Rebecca Scott <me@becdetat.com>
2020-08-21 16:48:29 +1000

    Break OBW test into subtests

--- tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.js
+++ tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.js
@@ -17,14 +17,12 @@ import { completeBenefitsSection } from './complete-benefits-section';
  * This tests a default, happy path for the onboarding wizard.
  */
 describe( 'Store owner can complete onboarding wizard', () => {
-	it( 'can complete the onboarding wizard', async () => {
-		await StoreOwnerFlow.login();
-		await StoreOwnerFlow.startProfileWizard();
-		await completeStoreDetailsSection();
-		await completeIndustrySection();
-		await completeProductTypesSection();
-		await completeBusinessSection();
-		await completeThemeSelectionSection();
-		await completeBenefitsSection();
-	} );
+	it( 'can log in', StoreOwnerFlow.login );
+	it( 'can start the profile wizard', StoreOwnerFlow.startProfileWizard );
+	it( 'can complete the store details section', completeStoreDetailsSection );
+	it( 'can complete the industry section', completeIndustrySection );
+	it( 'can complete the product types section', completeProductTypesSection );
+	it( 'can complete the business section', completeBusinessSection );
+	it( 'can complete the theme selection section', completeThemeSelectionSection );
+	it( 'can complete the benefits section', completeBenefitsSection );
 } );
