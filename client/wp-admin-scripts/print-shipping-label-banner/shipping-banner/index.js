4fa26da88e098831a8472209a7c0e4cc724f9b7b
Chris Shultz <chris.shultz@automattic.com>
2020-08-25 18:47:46 -0700

    Get app store function must be async to support dynamic js loading.

--- client/wp-admin-scripts/print-shipping-label-banner/shipping-banner/index.js
+++ client/wp-admin-scripts/print-shipping-label-banner/shipping-banner/index.js
@@ -253,106 +253,113 @@ export class ShippingBanner extends Component {
 	};
 
 	openWcsModal() {
-		if ( window.wcsGetAppStore ) {
-			const wcsStore = window.wcsGetAppStore(
-				'wc-connect-create-shipping-label'
-			);
-			const state = wcsStore.getState();
-			const { orderId } = this.state;
-			const siteId = state.ui.selectedSiteId;
+		if ( window.wcsGetAppStoreAsync ) {
+			window
+				.wcsGetAppStoreAsync( 'wc-connect-create-shipping-label' )
+				.then( ( wcsStore ) => {
+					const state = wcsStore.getState();
+					const { orderId } = this.state;
+					const siteId = state.ui.selectedSiteId;
 
-			const wcsStoreUnsubscribe = wcsStore.subscribe( () => {
-				const latestState = wcsStore.getState();
+					const wcsStoreUnsubscribe = wcsStore.subscribe( () => {
+						const latestState = wcsStore.getState();
 
-				const shippingLabelState = get(
-					latestState,
-					[
-						'extensions',
-						'woocommerce',
-						'woocommerceServices',
-						siteId,
-						'shippingLabel',
-						orderId,
-					],
-					null
-				);
+						const shippingLabelState = get(
+							latestState,
+							[
+								'extensions',
+								'woocommerce',
+								'woocommerceServices',
+								siteId,
+								'shippingLabel',
+								orderId,
+							],
+							null
+						);
 
-				const labelSettingsState = get(
-					latestState,
-					[
-						'extensions',
-						'woocommerce',
-						'woocommerceServices',
-						siteId,
-						'labelSettings',
-					],
-					null
-				);
+						const labelSettingsState = get(
+							latestState,
+							[
+								'extensions',
+								'woocommerce',
+								'woocommerceServices',
+								siteId,
+								'labelSettings',
+							],
+							null
+						);
 
-				const packageState = get(
-					latestState,
-					[
-						'extensions',
-						'woocommerce',
-						'woocommerceServices',
-						siteId,
-						'packages',
-					],
-					null
-				);
+						const packageState = get(
+							latestState,
+							[
+								'extensions',
+								'woocommerce',
+								'woocommerceServices',
+								siteId,
+								'packages',
+							],
+							null
+						);
 
-				const locationsState = get( latestState, [
-					'extensions',
-					'woocommerce',
-					'sites',
-					siteId,
-					'data',
-					'locations',
-				] );
+						const locationsState = get( latestState, [
+							'extensions',
+							'woocommerce',
+							'sites',
+							siteId,
+							'data',
+							'locations',
+						] );
 
-				if (
-					shippingLabelState &&
-					labelSettingsState &&
-					labelSettingsState.meta &&
-					packageState &&
-					locationsState
-				) {
-					if (
-						shippingLabelState.loaded &&
-						labelSettingsState.meta.isLoaded &&
-						packageState.isLoaded &&
-						isArray( locationsState ) &&
-						! this.state.isWcsModalOpen
-					) {
-						if ( window.jQuery ) {
-							this.setState( { isWcsModalOpen: true } );
-							window
-								.jQuery( '.shipping-label__new-label-button' )
-								.click();
+						if (
+							shippingLabelState &&
+							labelSettingsState &&
+							labelSettingsState.meta &&
+							packageState &&
+							locationsState
+						) {
+							if (
+								shippingLabelState.loaded &&
+								labelSettingsState.meta.isLoaded &&
+								packageState.isLoaded &&
+								isArray( locationsState ) &&
+								! this.state.isWcsModalOpen
+							) {
+								if ( window.jQuery ) {
+									this.setState( { isWcsModalOpen: true } );
+									window
+										.jQuery(
+											'.shipping-label__new-label-button'
+										)
+										.click();
+								}
+								wcsStore.dispatch( {
+									type: 'NOTICE_CREATE',
+									notice: {
+										duration: 10000,
+										status: 'is-success',
+										text: __(
+											'Plugin installed and activated',
+											'woocommerce-admin'
+										),
+									},
+								} );
+							} else if (
+								shippingLabelState.showPurchaseDialog
+							) {
+								wcsStoreUnsubscribe();
+								if ( window.jQuery ) {
+									window
+										.jQuery( '#woocommerce-order-label' )
+										.show();
+								}
+							}
 						}
-						wcsStore.dispatch( {
-							type: 'NOTICE_CREATE',
-							notice: {
-								duration: 10000,
-								status: 'is-success',
-								text: __(
-									'Plugin installed and activated',
-									'woocommerce-admin'
-								),
-							},
-						} );
-					} else if ( shippingLabelState.showPurchaseDialog ) {
-						wcsStoreUnsubscribe();
-						if ( window.jQuery ) {
-							window.jQuery( '#woocommerce-order-label' ).show();
-						}
-					}
-				}
-			} );
+					} );
 
-			document.getElementById(
-				'woocommerce-admin-print-label'
-			).style.display = 'none';
+					document.getElementById(
+						'woocommerce-admin-print-label'
+					).style.display = 'none';
+				} );
 		}
 	}
 
