afd57b4af8dbcc82316386ee764a2ce22bbe7d4d
Sam Seay <samueljseay@gmail.com>
2020-09-07 10:51:10 +1200

    Import utils StoreOwner flow

--- tests/e2e/specs/onboarding/onboarding-profiler.test.js
+++ tests/e2e/specs/onboarding/onboarding-profiler.test.js
@@ -5,7 +5,7 @@
 /**
  * Internal dependencies
  */
-import { StoreOwnerFlow } from '@woocommerce/e2e-tests/utils/flows';
+import { StoreOwnerFlow } from '../../utils/flows';
 
 const config = require( 'config' );
 const baseUrl = config.get( 'url' );
@@ -21,14 +21,20 @@ describe( 'Store owner can enable or disable onboarding', () => {
 
 		// Click the help tab
 		await expect( page ).toClick( '#contextual-help-link' );
-		await expect( page ).toClick( '#tab-link-woocommerce_onboard_tab a', { text: 'Setup wizard' } );
+		await expect( page ).toClick( '#tab-link-woocommerce_onboard_tab a', {
+			text: 'Setup wizard',
+		} );
 
 		await Promise.all( [
 			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
-			expect( page ).toClick( '#tab-panel-woocommerce_onboard_tab a', { text: 'Enable' } ),
+			expect( page ).toClick( '#tab-panel-woocommerce_onboard_tab a', {
+				text: 'Enable',
+			} ),
 		] );
 
-		await expect( page ).toMatchElement( '.woocommerce-profile-wizard__body' );
+		await expect( page ).toMatchElement(
+			'.woocommerce-profile-wizard__body'
+		);
 	} );
 
 	it( 'walks through the profiler', async () => {
@@ -39,17 +45,28 @@ describe( 'Store owner can enable or disable onboarding', () => {
 
 		// Start step.
 		await expect( page ).toClick( 'button', { text: 'Get started' } );
-		await expect( page ).toMatchElement( '.woocommerce-profile-wizard__usage-modal' );
+		await expect( page ).toMatchElement(
+			'.woocommerce-profile-wizard__usage-modal'
+		);
 		await expect( page ).toClick( 'button', { text: 'Continue' } );
 
 		// Skip the Jetpack connection for testing purposes.
-		await page.goto( baseUrl + 'wp-admin/admin.php?page=wc-admin&step=store-details', {
-			waitUntil: 'networkidle0',
-		} );
+		await page.goto(
+			baseUrl + 'wp-admin/admin.php?page=wc-admin&step=store-details',
+			{
+				waitUntil: 'networkidle0',
+			}
+		);
 
 		// Store details.
-		await expect( page ).toFill( '#inspector-text-control-0', '123 Peachtree Lane' );
-		await expect( page ).toFill( '.woocommerce-select-control__control-input', 'United States (US) -- Georgia' );
+		await expect( page ).toFill(
+			'#inspector-text-control-0',
+			'123 Peachtree Lane'
+		);
+		await expect( page ).toFill(
+			'.woocommerce-select-control__control-input',
+			'United States (US) -- Georgia'
+		);
 		await page.keyboard.press( 'Enter' );
 		await expect( page ).toFill( '#inspector-text-control-2', 'Atlanta' );
 		await expect( page ).toFill( '#inspector-text-control-3', '30312' );
@@ -57,32 +74,54 @@ describe( 'Store owner can enable or disable onboarding', () => {
 
 		// Industry.
 		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
-		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:first-child input' );
+		await expect( page ).toClick(
+			'.woocommerce-profile-wizard__checkbox:first-child input'
+		);
 		await expect( page ).toClick( 'button', { text: 'Continue' } );
 
 		// Product types
 		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
-		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:first-child input' );
-		await expect( page ).toClick( '.woocommerce-profile-wizard__checkbox:nth-child(2) input' );
+		await expect( page ).toClick(
+			'.woocommerce-profile-wizard__checkbox:first-child input'
+		);
+		await expect( page ).toClick(
+			'.woocommerce-profile-wizard__checkbox:nth-child(2) input'
+		);
 		await expect( page ).toClick( 'button', { text: 'Continue' } );
 
 		// Business details.
 		await Promise.all( [
 			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
-			expect( page ).toMatchElement( '.woocommerce-card__body div:first-child .woocommerce-select-control__control' ),
+			expect( page ).toMatchElement(
+				'.woocommerce-card__body div:first-child .woocommerce-select-control__control'
+			),
 		] );
-		await expect( page ).toClick( '.woocommerce-card__body div:first-child .woocommerce-select-control__control' );
-		await expect( page ).toClick( '#woocommerce-select-control__option-1-0' );
-		await expect( page ).toClick( '.woocommerce-card__body div:nth-child(2) .woocommerce-select-control__control' );
-		await expect( page ).toClick( '#woocommerce-select-control__option-2-no' );
+		await expect( page ).toClick(
+			'.woocommerce-card__body div:first-child .woocommerce-select-control__control'
+		);
+		await expect( page ).toClick(
+			'#woocommerce-select-control__option-1-0'
+		);
+		await expect( page ).toClick(
+			'.woocommerce-card__body div:nth-child(2) .woocommerce-select-control__control'
+		);
+		await expect( page ).toClick(
+			'#woocommerce-select-control__option-2-no'
+		);
 		await expect( page ).toClick( 'button', { text: 'Continue' } );
 
 		// Theme.
 		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
-		await expect( page ).toClick( '.woocommerce-profile-wizard__theme:nth-child(2) button', { text: 'Choose' } );
+		await expect(
+			page
+		).toClick( '.woocommerce-profile-wizard__theme:nth-child(2) button', {
+			text: 'Choose',
+		} );
 		await expect( page ).toClick( 'button', { text: "I'll do it later" } );
 
 		// Make sure the profiler is closed.
-		await expect( page ).toMatchElement( 'body:not(.woocommerce-profile-wizard__body)' );
+		await expect( page ).toMatchElement(
+			'body:not(.woocommerce-profile-wizard__body)'
+		);
 	} );
 } );
