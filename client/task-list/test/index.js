985f8d3e39b9012db51fd65de74a05db76878f5b
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2020-09-11 12:46:23 +0800

    Refactor element queries in test for performance
    A small performance improvement is made by querying the rendered container instead of the whole screen.

--- client/task-list/test/index.js
+++ client/task-list/test/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render, screen } from '@testing-library/react';
+import { render, findByText, queryByTestId } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import apiFetch from '@wordpress/api-fetch';
 
@@ -53,20 +53,24 @@ describe( 'TaskDashboard', () => {
 	it( 'renders a dismiss button for tasks that are optional and incomplete', async () => {
 		apiFetch.mockResolvedValue( {} );
 
-		render( <TaskDashboard query={ {} } /> );
+		const { container } = render( <TaskDashboard query={ {} } /> );
 
 		// Wait for the task list to render.
-		expect( await screen.findByText( 'Finish setup' ) ).toBeDefined();
+		expect( await findByText( container, 'Finish setup' ) ).toBeDefined();
 
 		// The `optional` task has a dismiss button.
 		expect(
-			screen.queryByTestId( 'optional-dismiss-button' )
+			queryByTestId( container, 'optional-dismiss-button' )
 		).toBeDefined();
 
 		// The `required` task does not have a dismiss button.
-		expect( screen.queryByTestId( 'required-dismiss-button' ) ).toBeNull();
+		expect(
+			queryByTestId( container, 'required-dismiss-button' )
+		).toBeNull();
 
 		// The `completed` task does not have a dismiss button.
-		expect( screen.queryByTestId( 'completed-dismiss-button' ) ).toBeNull();
+		expect(
+			queryByTestId( container, 'completed-dismiss-button' )
+		).toBeNull();
 	} );
 } );
