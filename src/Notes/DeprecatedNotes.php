8bf9836981f0fcf7caba4f8e5908d579697f9fff
Rebecca Scott <me@becdetat.com>
2020-09-16 10:09:25 +1000

    Rename admin notes classes and file names to fit conventions

---
+++ src/Notes/DeprecatedNotes.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Define deprecated classes to support changing the naming convention of
+ * admin notes.
+ */
+
+namespace Automattic\WooCommerce\Admin\Notes;
+
+// phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
+
+/**
+ * WC_Admin_Note (deprecated, use Note instead.
+ */
+class WC_Admin_Note extends Note {}
+
+/**
+ * WC_Admin_Notes (deprecated, use Notes instead).
+ */
+class WC_Admin_Notes extends Notes {}
+
+/**
+ * WC_Admin_Notes_Choose_Niche (deprecated, use Choose_Niche instead).
+ */
+class WC_Admin_Notes_Choose_Niche extends Choose_Niche {}
+
+/**
+ * WC_Admin_Notes_Coupon_Page_Moved (deprecated, use Coupon_Page_Moved instead).
+ */
+class WC_Admin_Notes_Coupon_Page_Moved extends Coupon_Page_Moved {}
+
+/**
+ * WC_Admin_Notes_Customize_Store_With_Blocks (deprecated, use Customize_Store_With_Blocks instead).
+ */
+class WC_Admin_Notes_Customize_Store_With_Blocks extends Customize_Store_With_Blocks {}
+
+/**
+ * WC_Admin_Notes_Deactivate_Plugin (deprecated, use Deactivate_Plugin instead).
+ */
+class WC_Admin_Notes_Deactivate_Plugin extends Deactivate_Plugin {}
+
+/**
+ * WC_Admin_Notes_Draw_Attention (deprecated, use Draw_Attention instead).
+ */
+class WC_Admin_Notes_Draw_Attention extends Draw_Attention {}
+
+/**
+ * WC_Admin_Notes_Edit_Products_On_The_Move (deprecated, use Edit_Products_On_The_Move instead).
+ */
+class WC_Admin_Notes_Edit_Products_On_The_Move extends Edit_Products_On_The_Move {}
+
+/**
+ * WC_Admin_Notes_EU_VAT_Number (deprecated, use EU_VAT_Number instead).
+ */
+class WC_Admin_Notes_EU_VAT_Number extends EU_VAT_Number {}
+
+/**
+ * WC_Admin_Notes_Facebook_Marketing_Expert (deprecated, use Facebook_Marketing_Expert instead).
+ */
+class WC_Admin_Notes_Facebook_Marketing_Expert extends Facebook_Marketing_Expert {}
+
+/**
+ * WC_Admin_Notes_First_Product (deprecated, use First_Product instead).
+ */
+class WC_Admin_Notes_First_Product extends First_Product {}
+
+/**
+ * WC_Admin_Notes_Giving_Feedback_Notes (deprecated, use Giving_Feedback_Notes instead).
+ */
+class WC_Admin_Notes_Giving_Feedback_Notes extends Giving_Feedback_Notes {}
+
+/**
+ * WC_Admin_Notes_Historical_Data (deprecated, use Historical_Data instead).
+ */
+class WC_Admin_Notes_Historical_Data extends Historical_Data {}
+
+/**
+ * WC_Admin_Notes_Home_Screen_Feedback (deprecated, use Home_Screen_Feedback instead).
+ */
+class WC_Admin_Notes_Home_Screen_Feedback extends Home_Screen_Feedback {}
+
+/**
+ * WC_Admin_Notes_Insight_First_Sale (deprecated, use Insight_First_Sale instead).
+ */
+class WC_Admin_Notes_Insight_First_Sale extends Insight_First_Sale {}
+
+/**
+ * WC_Admin_Notes_Install_JP_And_WCS_Plugins (deprecated, use Install_JP_And_WCS_Plugins instead).
+ */
+class WC_Admin_Notes_Install_JP_And_WCS_Plugins extends Install_JP_And_WCS_Plugins {}
+
+/**
+ * WC_Admin_Notes_Launch_Checklist (deprecated, use Launch_Checklist instead).
+ */
+class WC_Admin_Notes_Launch_Checklist extends Launch_Checklist {}
+
+/**
+ * WC_Admin_Notes_Learn_More_About_Product_Settings (deprecated, use Learn_More_About_Product_Settings instead).
+ */
+class WC_Admin_Notes_Learn_More_About_Product_Settings extends Learn_More_About_Product_Settings {}
+
+/**
+ * WC_Admin_Notes_Marketing (deprecated, use Marketing instead).
+ */
+class WC_Admin_Notes_Marketing extends Marketing {}
+
+/**
+ * WC_Admin_Notes_Migrate_From_Shopify (deprecated, use Migrate_From_Shopify instead).
+ */
+class WC_Admin_Notes_Migrate_From_Shopify extends Migrate_From_Shopify {}
+
+/**
+ * WC_Admin_Notes_Mobile_App (deprecated, use Mobile_App instead).
+ */
+class WC_Admin_Notes_Mobile_App extends Mobile_App {}
+
+/**
+ * WC_Admin_Notes_Need_Some_Inspiration (deprecated, use Need_Some_Inspiration instead).
+ */
+class WC_Admin_Notes_Need_Some_Inspiration extends Need_Some_Inspiration {}
+
+/**
+ * WC_Admin_Notes_New_Sales_Record (deprecated, use New_Sales_Record instead).
+ */
+class WC_Admin_Notes_New_Sales_Record extends New_Sales_Record {}
+
+/**
+ * WC_Admin_Notes_Onboarding_Email_Marketing (deprecated, use Onboarding_Email_Marketing instead).
+ */
+class WC_Admin_Notes_Onboarding_Email_Marketing extends Onboarding_Email_Marketing {}
+
+/**
+ * WC_Admin_Notes_Onboarding_Payments (deprecated, use Onboarding_Payments instead).
+ */
+class WC_Admin_Notes_Onboarding_Payments extends Onboarding_Payments {}
+
+/**
+ * WC_Admin_Notes_Onboarding_Profiler (deprecated, use Onboarding_Profiler instead).
+ */
+class WC_Admin_Notes_Onboarding_Profiler extends Onboarding_Profiler {}
+
+/**
+ * WC_Admin_Notes_Online_Clothing_Store (deprecated, use Online_Clothing_Store instead).
+ */
+class WC_Admin_Notes_Online_Clothing_Store extends Online_Clothing_Store {}
+
+/**
+ * WC_Admin_Notes_Order_Milestones (deprecated, use Order_Milestones instead).
+ */
+class WC_Admin_Notes_Order_Milestones extends Order_Milestones {}
+
+/**
+ * WC_Admin_Notes_Performance_On_Mobile (deprecated, use Performance_On_Mobile instead).
+ */
+class WC_Admin_Notes_Performance_On_Mobile extends Performance_On_Mobile {}
+
+/**
+ * WC_Admin_Notes_Personalize_Store (deprecated, use Personalize_Store instead).
+ */
+class WC_Admin_Notes_Personalize_Store extends Personalize_Store {}
+
+/**
+ * WC_Admin_Notes_Real_Time_Order_Alerts (deprecated, use Real_Time_Order_Alerts instead).
+ */
+class WC_Admin_Notes_Real_Time_Order_Alerts extends Real_Time_Order_Alerts {}
+
+/**
+ * WC_Admin_Notes_Review_Shipping_Settings (deprecated, use Review_Shipping_Settings instead).
+ */
+class WC_Admin_Notes_Review_Shipping_Settings extends Review_Shipping_Settings {}
+
+/**
+ * WC_Admin_Notes_Selling_Online_Courses (deprecated, use Selling_Online_Courses instead).
+ */
+class WC_Admin_Notes_Selling_Online_Courses extends Selling_Online_Courses {}
+
+/**
+ * WC_Admin_Notes_Set_Up_Additional_Payment_Types (deprecated, use Set_Up_Additional_Payment_Types instead).
+ */
+class WC_Admin_Notes_Set_Up_Additional_Payment_Types extends Set_Up_Additional_Payment_Types {}
+
+/**
+ * WC_Admin_Notes_Start_Dropshipping_Business (deprecated, use Start_Dropshipping_Business instead).
+ */
+class WC_Admin_Notes_Start_Dropshipping_Business extends Start_Dropshipping_Business {}
+
+/**
+ * WC_Admin_Notes_Test_Checkout (deprecated, use Test_Checkout instead).
+ */
+class WC_Admin_Notes_Test_Checkout extends Test_Checkout {}
+
+/**
+ * WC_Admin_Notes_Tracking_Opt_In (deprecated, use Tracking_Opt_In instead).
+ */
+class WC_Admin_Notes_Tracking_Opt_In extends Tracking_Opt_In {}
+
+/**
+ * WC_Admin_Notes_Woo_Subscriptions_Notes (deprecated, use Woo_Subscriptions_Notes instead).
+ */
+class WC_Admin_Notes_Woo_Subscriptions_Notes extends Woo_Subscriptions_Notes {}
+
+/**
+ * WC_Admin_Notes_WooCommerce_Payments (deprecated, use WooCommerce_Payments instead).
+ */
+class WC_Admin_Notes_WooCommerce_Payments extends WooCommerce_Payments {}
+
+/**
+ * WC_Admin_Notes_WooCommerce_Subscriptions (deprecated, use WooCommerce_Subscriptions instead).
+ */
+class WC_Admin_Notes_WooCommerce_Subscriptions extends WooCommerce_Subscriptions {}
