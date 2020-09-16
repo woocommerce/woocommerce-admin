1d8cbcae48be73984dd199a1431fc2795a2b32fc
Rebecca Scott <me@becdetat.com>
2020-09-16 17:03:41 +1000

    Add Apple Pay admin note (details TBD)

--- src/Events.php
+++ src/Events.php
@@ -38,6 +38,7 @@ use \Automattic\WooCommerce\Admin\Notes\Facebook_Marketing_Expert;
 use \Automattic\WooCommerce\Admin\Notes\Test_Checkout;
 use \Automattic\WooCommerce\Admin\Notes\Edit_Products_On_The_Move;
 use \Automattic\WooCommerce\Admin\Notes\Performance_On_Mobile;
+use \Automattic\WooCommerce\Admin\Notes\Apple_Pay_TBD;
 
 /**
  * WC_Admin_Events Class.
@@ -109,6 +110,7 @@ class Events {
 		Test_Checkout::possibly_add_note();
 		Edit_Products_On_The_Move::possibly_add_note();
 		Performance_On_Mobile::possibly_add_note();
+		Apple_Pay_TBD::possibly_add_note();
 
 		if ( Loader::is_feature_enabled( 'remote-inbox-notifications' ) ) {
 			DataSourcePoller::read_specs_from_data_sources();
---
+++ src/Notes/ApplePayTBD.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * WooCommerce Admin TBD Apple Pay Note Provider.
+ *
+ * Adds a note to the merchant's inbox TBD something apple pay.
+ */
+
+namespace Automattic\WooCommerce\Admin\Notes;
+
+defined( 'ABSPATH' ) || exit;
+
+use \Automattic\WooCommerce\Admin\PluginsHelper;
+
+/**
+ * Apple_Pay_TBD
+ */
+class Apple_Pay_TBD {
+	/**
+	 * Note traits.
+	 */
+	use NoteTraits;
+
+	/**
+	 * Name of the note for use in the database.
+	 */
+	const NOTE_NAME = 'wc-admin-apple-pay-tbd';
+
+	/**
+	 * Get the note.
+	 *
+	 * @return Note|null
+	 */
+	public static function get_note() {
+		// Only show if Stripe plugin is active.
+		if ( ! PluginsHelper::is_plugin_active( 'woocommerce-gateway-stripe/woocommerce-gateway-stripe.php' ) ) {
+			return;
+		}
+
+		// Only show if in the US.
+		$base_location = wc_get_base_location();
+		if ( ! $base_location ) {
+			return;
+		}
+		if ( 'US' !== $base_location['country'] ) {
+			return;
+		}
+
+		$note = new Note();
+
+		$note->set_title( __( 'Title TBD', 'woocommerce-admin' ) );
+		$note->set_content( __( 'Content TBD', 'woocommerce-admin' ) );
+		$note->set_content_data( (object) array() );
+		$note->set_type( Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+		$note->set_name( self::NOTE_NAME );
+		$note->set_source( 'woocommerce-admin' );
+		$note->add_action(
+			'learn-more',
+			__( 'Learn more', 'woocommerce-admin' ),
+			'https://woocommerce.com/apple-pay/?utm_source=inbox'
+		);
+
+		return $note;
+	}
+}
