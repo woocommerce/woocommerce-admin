6aff3fc2e05abab626c7e18725c970878c4312f2
Sam Seay <samueljseay@gmail.com>
2020-10-01 11:54:45 +1300

    Simplify to a single lint-staged config file.

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'*.scss': [ 'npm run lint:css' ],
+	'*.js': [
+		'wp-scripts format-js ./packages ./client',
+		'npm run lint:js',
+		'npm run test -- --bail --findRelatedTests',
+	],
+	'*.php': [ 'php -d display_errors=1 -l', 'composer run-script phpcs' ],
+};
