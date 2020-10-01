3442eb4d33e2b37408e0913ab66dca49eef37c2a
Sam Seay <samueljseay@gmail.com>
2020-10-01 13:25:13 +1300

    Attempt not listing files so that lint-staged will run on staged files only.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	'*.scss': [ 'npm run lint:css' ],
 	'(client|packages)/**/*.js': [
-		'wp-scripts format-js ./packages ./client',
-		'npm run lint:js',
+		'wp-scripts format-js',
+		'wp-scripts lint-js',
 		'npm run test -- --bail --findRelatedTests',
 	],
 	'*.php': [ 'php -d display_errors=1 -l', 'composer run-script phpcs' ],
