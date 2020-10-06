7071447acbb768b8de3076e21cca7e141aa59d04
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-10-06 08:54:56 -0400

    Migrate ActivityCard tests to RTL.

--- client/header/activity-panel/activity-card/test/index.js
+++ client/header/activity-panel/activity-card/test/index.js
@@ -3,9 +3,10 @@
  */
 import { Button } from '@wordpress/components';
 import Gridicon from 'gridicons';
-import { shallow } from 'enzyme';
 import moment from 'moment';
 import { Gravatar } from '@woocommerce/components';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 /**
  * Internal dependencies
@@ -14,34 +15,36 @@ import { ActivityCard } from '../';
 
 describe( 'ActivityCard', () => {
 	test( 'should have correct title', () => {
-		const card = (
+		const { getByRole } = render(
 			<ActivityCard title="Inbox message">
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card.props.title ).toBe( 'Inbox message' );
+		expect(
+			getByRole( 'heading', { name: 'Inbox message' } )
+		).not.toBeEmptyDOMElement();
 	} );
 
 	test( 'should render a basic card', () => {
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard title="Inbox message">
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render an unread bubble on a card', () => {
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard title="Inbox message" unread>
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render a custom icon on a card', () => {
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard
 				title="Inbox message"
 				icon={ <Gridicon icon="customize" /> }
@@ -49,11 +52,11 @@ describe( 'ActivityCard', () => {
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render a gravatar on a card', () => {
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard
 				title="Inbox message"
 				icon={ <Gravatar user="admin@local.test" /> }
@@ -61,23 +64,23 @@ describe( 'ActivityCard', () => {
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render a timestamp on a card', () => {
 		// We're generating this via moment to ensure it's always "3 days ago".
 		const threeDaysAgo = moment().subtract( 3, 'days' ).format();
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard title="Inbox message" date={ threeDaysAgo }>
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render an action on a card', () => {
 		const noop = () => {};
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard
 				title="Inbox message"
 				actions={
@@ -89,12 +92,12 @@ describe( 'ActivityCard', () => {
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 
 	test( 'should render multiple actions on a card', () => {
 		const noop = () => {};
-		const card = shallow(
+		const { container } = render(
 			<ActivityCard
 				title="Inbox message"
 				actions={ [
@@ -109,6 +112,6 @@ describe( 'ActivityCard', () => {
 				This card has some content
 			</ActivityCard>
 		);
-		expect( card ).toMatchSnapshot();
+		expect( container ).toMatchSnapshot();
 	} );
 } );
