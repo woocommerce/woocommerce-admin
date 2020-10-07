06aa5469128763f4914270e95b225fe6bea1be24
Sam Seay <samueljseay@gmail.com>
2020-10-07 13:13:32 +1300

    Fix a bug in tax automation logic and track the event in analytics

--- src/Features/OnboardingAutomateTaxes.php
+++ src/Features/OnboardingAutomateTaxes.php
@@ -55,9 +55,10 @@ class OnboardingAutomateTaxes {
 			$wcs_tos_accepted = \WC_Connect_Options::get_option( 'tos_accepted' );
 		}
 
-		if ( $jetpack_connected && $wcs_version && $wcs_tos_accepted ) {
+		if ( $jetpack_connected && $wcs_version && $wcs_tos_accepted && self::automated_tax_is_supported() ) {
 			update_option( 'wc_connect_taxes_enabled', 'yes' );
 			update_option( 'woocommerce_calc_taxes', 'yes' );
+			self::track_tax_automation();
 		}
 	}
 
@@ -66,5 +67,12 @@ class OnboardingAutomateTaxes {
 	 */
 	private static function automated_tax_is_supported() {
 		return in_array( WC()->countries->get_base_country(), \OnboardingTasks::get_automated_tax_supported_countries(), true );
+	}
+
+	/**
+	 * Track when a user has tax automation enabled.
+	 */
+	private static function track_tax_automation() {
+		wc_admin_record_tracks_event( 'tasklist_task_completed', array( 'task_name' => 'tax_automated' ) );
 	}
 }
