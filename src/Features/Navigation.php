7d7a7ee4ca7f6704ee21236100e3a1c437fe827c
Paul Sealock <psealock@gmail.com>
2020-10-08 14:56:39 +1300

    feature class

---
+++ src/Features/Navigation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Navigation Experience
+ *
+ * @package Woocommerce Admin
+ */
+
+namespace Automattic\WooCommerce\Admin\Features;
+
+/**
+ * Contains logic for the Navigation
+ */
+class Navigation {
+	/**
+	 * Hook into WooCommerce.
+	 */
+	public function __construct() {
+		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
+	}
+
+	/**
+	 * Preload options to prime state of the application.
+	 *
+	 * @param array $options Array of options to preload.
+	 * @return array
+	 */
+	public function preload_options( $options ) {
+		$options[] = 'woocommerce_navigation_enabled';
+
+		return $options;
+	}
+}
