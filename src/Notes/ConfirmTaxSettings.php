b82d2513257bc22f6f0bab799dac229a3482f5d7
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-10-09 12:33:25 -0400

    Fix note class name. (#5317)
    Missed in https://github.com/woocommerce/woocommerce-admin/pull/5314.

--- src/Notes/ConfirmTaxSettings.php
+++ src/Notes/ConfirmTaxSettings.php
@@ -30,7 +30,7 @@ class Confirm_Tax_Settings {
 	 * @return Note
 	 */
 	public static function get_note() {
-		$note = new Note();
+		$note = new WC_Admin_Note();
 
 		$note->set_title( __( 'Confirm tax settings', 'woocommerce-admin' ) );
 		$note->set_content( __( 'Automated tax calculations are enabled on your store through WooCommerce Shipping & Tax. Learn more about automated taxes <a href="https://docs.woocommerce.com/document/woocommerce-services/#section-12">here</a>.', 'woocommerce-admin' ) );
