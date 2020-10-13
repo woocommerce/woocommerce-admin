780137e07407ee29effa0c05f29102069004f578
Paul Sealock <psealock@gmail.com>
2020-10-13 14:03:17 +1300

    add back in check is_feature_enabled

--- src/Features/Navigation/Navigation.php
+++ src/Features/Navigation/Navigation.php
@@ -21,6 +21,7 @@ class Navigation {
 	 */
 	public function __construct() {
 		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
+		add_filter( 'woocommerce_admin_features', array( $this, 'maybe_remove_nav_feature' ), 0 );
 
 		if ( Loader::is_feature_enabled( 'navigation' ) ) {
 			add_action( 'in_admin_header', array( __CLASS__, 'embed_navigation' ) );
@@ -29,6 +30,18 @@ class Navigation {
 			CoreMenu::instance()->init();
 			Screen::instance()->init();
 		}
+	}
+
+	/**
+	 * Overwrites the allowed features array using a local `feature-config.php` file.
+	 *
+	 * @param array $features Array of feature slugs.
+	 */
+	public function maybe_remove_nav_feature( $features ) {
+		if ( in_array( 'navigation', $features, true ) && 'yes' !== get_option( 'woocommerce_navigation_enabled', 'no' ) ) {
+			$features = array_diff( $features, array( 'navigation' ) );
+		}
+		return $features;
 	}
 
 	/**
