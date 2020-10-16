db10515a9906a0d74d077c3f1fa75fa1146797d3
Rebecca Scott <me@becdetat.com>
2020-10-16 11:58:36 +1000

    Moved notice (snackbar) into @woocommerce/customer-effort-score

--- client/customer-effort-score-tracks/index.js
+++ client/customer-effort-score-tracks/index.js
@@ -1,12 +1,8 @@
 /**
  * External dependencies
  */
-import { useState } from '@wordpress/element';
 import PropTypes from 'prop-types';
 import { recordEvent } from '@woocommerce/tracks';
-import { compose } from '@wordpress/compose';
-import { withDispatch } from '@wordpress/data';
-import { __ } from '@wordpress/i18n';
 import CustomerEffortScore from '@woocommerce/customer-effort-score';
 
 /**
@@ -14,22 +10,11 @@ import CustomerEffortScore from '@woocommerce/customer-effort-score';
  * customer effort score.
  *
  * @param {Object}   props                  Component props.
- * @param {boolean}  props.initiallyVisible Whether or not the tracks modal is initially visible.
  * @param {string}   props.trackName        The name sent to Tracks.
  * @param {Object}   props.trackProps       Additional props sent to Tracks.
  * @param {string}   props.label            The label displayed in the modal.
- * @param {Function} props.createNotice     Create a notice (snackbar)
  */
-function CustomerEffortScoreTracks( {
-	initiallyVisible,
-	trackName,
-	trackProps,
-	label,
-	createNotice,
-} ) {
-	const [ showModal, setShowModal ] = useState( false );
-	const [ visible, setVisible ] = useState( initiallyVisible );
-
+function CustomerEffortScoreTracks( { trackName, trackProps, label } ) {
 	const trackCallback = ( score ) => {
 		recordEvent( trackName, {
 			score,
@@ -37,41 +22,12 @@ function CustomerEffortScoreTracks( {
 		} );
 	};
 
-	if ( ! showModal ) {
-		createNotice( 'success', label, {
-			actions: [
-				{
-					label: __( 'Give feedback', 'woocommerce-admin' ),
-					onClick: () => setShowModal( true ),
-				},
-			],
-		} );
-
-		return null;
-	}
-
-	if ( ! initiallyVisible ) {
-		return null;
-	}
-
 	return (
-		<CustomerEffortScore
-			trackCallback={ trackCallback }
-			visible={ visible }
-			toggleVisible={ () => setVisible( ! visible ) }
-			label={ label }
-		/>
+		<CustomerEffortScore trackCallback={ trackCallback } label={ label } />
 	);
 }
 
 CustomerEffortScoreTracks.propTypes = {
-	/**
-	 * Whether or not the tracks is initially visible. True is used for when
-	 * this is loaded on page load (in client/index.js). False is used if the
-	 * tracks modal is loaded as part of the layout and displayed
-	 * programmatically.
-	 */
-	initiallyVisible: PropTypes.bool,
 	/**
 	 * The name sent to Tracks.
 	 */
@@ -86,12 +42,4 @@ CustomerEffortScoreTracks.propTypes = {
 	label: PropTypes.string.isRequired,
 };
 
-export default compose(
-	withDispatch( ( dispatch ) => {
-		const { createNotice } = dispatch( 'core/notices' );
-
-		return {
-			createNotice,
-		};
-	} )
-)( CustomerEffortScoreTracks );
+export default CustomerEffortScoreTracks;
--- packages/customer-effort-score/src/index.js
+++ packages/customer-effort-score/src/index.js
@@ -3,6 +3,9 @@
  */
 import { useState } from '@wordpress/element';
 import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
+import { compose } from '@wordpress/compose';
+import { withDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -16,26 +19,36 @@ import PropTypes from 'prop-types';
  *
  * @param {Object}   props               Component props.
  * @param {Function} props.trackCallback Function to call when the modal is activated.
- * @param {boolean}  props.visible       Whether or not the tracks modal is visible.
- * @param {Function} props.toggleVisible Callback to toggle the visible prop.
  * @param {string}   props.label         The label displayed in the modal.
+ * @param {Function} props.createNotice     Create a notice (snackbar)
  */
-function CustomerEffortScore( {
-	trackCallback,
-	visible,
-	toggleVisible,
-	label,
-} ) {
+function CustomerEffortScore( { trackCallback, label, createNotice } ) {
+	const [ showModal, setShowModal ] = useState( false );
 	const [ score, setScore ] = useState( 0 );
+	const [ shouldCreateNotice, setShouldCreateNotice ] = useState( true );
 
-	if ( ! visible ) {
+	if ( shouldCreateNotice ) {
+		createNotice( 'success', label, {
+			actions: [
+				{
+					label: __( 'Give feedback', 'woocommerce-admin' ),
+					onClick: () => setShowModal( true ),
+				},
+			],
+		} );
+
+		setShouldCreateNotice( false );
+
+		return null;
+	}
+
+	if ( ! showModal ) {
 		return null;
 	}
 
 	function close() {
 		setScore( 3 ); // TODO let this happen in the UI
-
-		toggleVisible();
+		setShowModal( false );
 		trackCallback( score );
 	}
 
@@ -52,20 +65,17 @@ CustomerEffortScore.propTypes = {
 	 */
 	trackCallback: PropTypes.func.isRequired,
 	/**
-	 * Whether or not the tracks is visible. True is used for when
-	 * this is loaded on page load (in client/index.js). False is used if the
-	 * tracks modal is loaded as part of the layout and displayed
-	 * programmatically.
-	 */
-	visible: PropTypes.bool.isRequired,
-	/**
-	 * Callback to toggle the visible prop.
-	 */
-	toggleVisible: PropTypes.func.isRequired,
-	/**
 	 * The label displayed in the modal.
 	 */
 	label: PropTypes.string.isRequired,
 };
 
-export default CustomerEffortScore;
+export default compose(
+	withDispatch( ( dispatch ) => {
+		const { createNotice } = dispatch( 'core/notices' );
+
+		return {
+			createNotice,
+		};
+	} )
+)( CustomerEffortScore );
