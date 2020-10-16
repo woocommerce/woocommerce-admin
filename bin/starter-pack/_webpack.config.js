4a8b9c05c6cb167b5c63e6fd16778aefff694690
Rebecca Scott <me@becdetat.com>
2020-10-16 17:30:12 +1000

    Tidy up _webpack.config.js

--- bin/starter-pack/_webpack.config.js
+++ bin/starter-pack/_webpack.config.js
@@ -1,42 +1,6 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
 
-const requestToExternal = ( request ) => {
-	const wcDepMap = {
-		'@woocommerce/components': [ 'window', 'wc', 'components' ],
-		'@woocommerce/csv-export': [ 'window', 'wc', 'csvExport' ],
-		'@woocommerce/currency': [ 'window', 'wc', 'currency' ],
-		'@woocommerce/customer-effort-score': [ 'window', 'wc', 'customerEffortScore' ],
-		'@woocommerce/date': [ 'window', 'wc', 'date' ],
-		'@woocommerce/navigation': [ 'window', 'wc', 'navigation' ],
-		'@woocommerce/number': [ 'window', 'wc', 'number' ],
-		'@woocommerce/settings': [ 'window', 'wc', 'wcSettings' ],
-		'@woocommerce/tracks': [ 'window', 'wc', 'tracks' ],
-	};
-
-	if ( wcDepMap[ request ] ) {
-		return wcDepMap[ request ];
-	}
-};
-
-const requestToHandle = ( request ) => {
-	const wcHandleMap = {
-		'@woocommerce/components': 'wc-components',
-		'@woocommerce/csv-export': 'wc-csv',
-		'@woocommerce/currency': 'wc-currency',
-		'@woocommerce/customer-effort-score': 'wc-customer-effort-score',
-		'@woocommerce/date': 'wc-date',
-		'@woocommerce/navigation': 'wc-navigation',
-		'@woocommerce/number': 'wc-number',
-		'@woocommerce/settings': 'wc-settings',
-		'@woocommerce/tracks': 'wc-tracks',
-	};
-
-	if ( wcHandleMap[ request ] ) {
-		return wcHandleMap[ request ];
-	}
-};
-
 module.exports = {
 	...defaultConfig,
 	plugins: [
