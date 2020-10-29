aa8413f9aac4ace726aa538a10cb46c67d52a43b
Sam Seay <samueljseay@gmail.com>
2020-10-29 22:51:26 +1300

    Use the default waitForSelector to fix flakyness.

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -1,6 +1,18 @@
 const path = require( 'path' );
 const { jestConfig: baseE2Econfig } = require( '@woocommerce/e2e-environment' );
 
+global.it = async function ( name, func ) {
+	return await test( name, async () => {
+		try {
+			await func();
+		} catch ( e ) {
+			await fs.ensureDir( 'e2e/screenshots' );
+			await page.screenshot( { path: `e2e/screenshots/${ name }.png` } );
+			throw e;
+		}
+	} );
+};
+
 module.exports = {
 	...baseE2Econfig,
 	// Specify the path of your project's E2E tests here.
