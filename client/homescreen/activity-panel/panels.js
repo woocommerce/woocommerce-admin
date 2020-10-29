ee46d73059f4f618fffa1dca01d7dcd4cd136ed1
Fernando Marichal <contacto@fernandomarichal.com>
2020-10-29 14:27:48 -0300

    Added test panels

--- client/homescreen/activity-panel/panels.js
+++ client/homescreen/activity-panel/panels.js
@@ -20,6 +20,36 @@ export function getAllPanels( { countUnreadOrders } ) {
 			),
 			title: __( 'Orders', 'woocommerce-admin' ),
 		},
+		{
+			className: 'woocommerce-homescreen-card',
+			count: countUnreadOrders,
+			id: 'orders-panel-test1',
+			initialOpen: false,
+			panel: (
+				<OrdersPanel hasActionableOrders={ countUnreadOrders > 0 } />
+			),
+			title: __( 'Test 1', 'woocommerce-admin' ),
+		},
+		{
+			className: 'woocommerce-homescreen-card',
+			count: countUnreadOrders,
+			id: 'orders-panel-test2',
+			initialOpen: false,
+			panel: (
+				<OrdersPanel hasActionableOrders={ countUnreadOrders > 0 } />
+			),
+			title: __( 'Test 2', 'woocommerce-admin' ),
+		},
+		{
+			className: 'woocommerce-homescreen-card',
+			count: countUnreadOrders,
+			id: 'orders-panel-test3',
+			initialOpen: false,
+			panel: (
+				<OrdersPanel hasActionableOrders={ countUnreadOrders > 0 } />
+			),
+			title: __( 'Test 3', 'woocommerce-admin' ),
+		},
 		// Add another panel row here
 	];
 }
