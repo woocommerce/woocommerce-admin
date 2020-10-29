064d8f8e2e83e87983a12414d61d6b045cd58c5c
Sam Seay <samueljseay@gmail.com>
2020-10-29 23:11:26 +1300

    Fix up the benefits test.

--- tests/e2e/specs/activate-and-setup/complete-benefits-section.js
+++ tests/e2e/specs/activate-and-setup/complete-benefits-section.js
@@ -4,7 +4,9 @@
 
 export async function completeBenefitsSection() {
 	// Wait for Benefits section to appear
-	await page.waitForSelector( '.woocommerce-profile-wizard__header-title' );
+	await page.waitForSelector(
+		'.woocommerce-profile-wizard__container.benefits'
+	);
 
 	// Wait for "No thanks" button to become active
 	await page.waitForSelector( 'button.is-secondary:not(:disabled)' );
