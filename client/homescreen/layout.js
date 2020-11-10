9957ec380c17d87dec59e5503e0f9c3285ff1599
Sam Seay <samueljseay@gmail.com>
2020-11-10 15:45:14 +1300

    Revert accidental log changes.

--- client/homescreen/layout.js
+++ client/homescreen/layout.js
@@ -155,23 +155,12 @@ export default compose(
 			getOption( 'woocommerce_task_list_welcome_modal_dismissed' ) ===
 			'yes';
 
-		console.log(
-			'raw opt value',
-			getOption( 'woocommerce_task_list_welcome_modal_dismissed' )
-		);
-
-		console.log( 'it equal yes?', welcomeModalDismissed );
-
 		const welcomeModalDismissedIsResolving = isResolving( 'getOption', [
 			'woocommerce_task_list_welcome_modal_dismissed',
 		] );
 
-		console.log( 'isresolve? ', welcomeModalDismissedIsResolving );
-
 		const shouldShowWelcomeModal =
 			! welcomeModalDismissedIsResolving && ! welcomeModalDismissed;
-
-		console.log( 'final value? ', shouldShowWelcomeModal );
 
 		const defaultHomescreenLayout =
 			getOption( 'woocommerce_default_homepage_layout' ) ||
