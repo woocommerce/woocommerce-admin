7ea44712f27dfcef1c0bbc701223847a270f3d26
Tomek Wytrębowicz <tomalecpub@gmail.com>
2020-11-12 01:36:49 +0100

    Add placeholder for RecommendedExtensionsItem.
    Addresses https://github.com/woocommerce/woocommerce-admin/issues/5375.

---
+++ client/marketing/components/recommended-extensions/placeholder.js
@@ -0,0 +1,28 @@
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+const RecommendedExtensionsPlaceholder = () => {
+	const classNameBase =
+		'is-loading woocommerce-marketing-recommended-extensions-item';
+
+	return (
+		<div className={ classNameBase } aria-hidden="true">
+			<div className="woocommerce-admin-marketing-product-icon">
+				<span className="is-placeholder" />
+			</div>
+
+			<div className={ `${ classNameBase }__text` }>
+				<h4 className="is-placeholder"></h4>
+				<p>
+					<span className="is-placeholder" key={ 1 } />
+					<span className="is-placeholder" key={ 2 } />
+					<span className="is-placeholder" key={ 3 } />
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default RecommendedExtensionsPlaceholder;
