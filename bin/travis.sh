fb547e7628c6695cf54e8fb2f1d70770561c52d3
Sam Seay <samueljseay@gmail.com>
2020-11-16 10:40:45 +1300

    Fix the travis builds.

--- bin/travis.sh
+++ bin/travis.sh
@@ -4,6 +4,11 @@
 if [ "$1" == 'before' ]; then
 	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 	npm run build:feature-config
+	
+	# Jetpack Autoloader breaks in composer 2.0.7, until thats
+	# resolved we need to temporarily fix the composer version
+	# to 2.0.6
+	composer self-update 2.0.6
 	if [[ "$COMPOSER_DEV" == "1" ]]; then
 		composer install
 	else
