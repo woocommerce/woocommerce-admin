8fe9cf8fbd92cea20f23ccfbb3d7a0a1e7b165b9
Tomek Wytrębowicz <tomalecpub@gmail.com>
2020-11-19 00:36:10 +0100

    Use template literals for KnowledgebaseCardPostPlaceholder className concatenation.
    Addresses https://github.com/woocommerce/woocommerce-admin/pull/5611#discussion_r525408680.

--- client/marketing/components/knowledge-base/placeholder.js
+++ client/marketing/components/knowledge-base/placeholder.js
@@ -8,14 +8,14 @@ const KnowledgebaseCardPostPlaceholder = ( index ) => {
 
 	return (
 		<div
-			className={ 'is-loading ' + classNameBase }
+			className={ `is-loading ${ classNameBase }` }
 			key={ index }
 			aria-hidden="true"
 		>
-			<div className={ classNameBase + '-img is-placeholder' }></div>
-			<div className={ classNameBase + '-text' }>
+			<div className={ `${ classNameBase }-img is-placeholder` }></div>
+			<div className={ `${ classNameBase }-text` }>
 				<h3 className="is-placeholder" aria-hidden="true"></h3>
-				<p className={ classNameBase + '-meta is-placeholder' }></p>
+				<p className={ `${ classNameBase }-meta is-placeholder` }></p>
 			</div>
 		</div>
 	);
