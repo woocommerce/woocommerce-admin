d824e3d74051fd29d2eddc79f82841dee145e6a6
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-12-04 16:52:37 -0700

    Add endpoint to fetch custom attribute values by slug.

--- src/API/Init.php
+++ src/API/Init.php
@@ -64,6 +64,7 @@ class Init {
 			'Automattic\WooCommerce\Admin\API\Orders',
 			'Automattic\WooCommerce\Admin\API\Products',
 			'Automattic\WooCommerce\Admin\API\ProductAttributes',
+			'Automattic\WooCommerce\Admin\API\ProductAttributeTerms',
 			'Automattic\WooCommerce\Admin\API\ProductCategories',
 			'Automattic\WooCommerce\Admin\API\ProductVariations',
 			'Automattic\WooCommerce\Admin\API\ProductReviews',
