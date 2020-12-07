21db2b6e706f387a17bd7e6dc4d5fa8336e6b03e
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-12-07 10:28:40 -0700

    Add initial tests for Product Attributes endpoint.

---
+++ tests/api/product-attributes.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Product Attributes REST API Test
+ *
+ * @package WooCommerce\Admin\Tests\API
+ */
+
+/**
+ * Class WC_Tests_API_Product_Attributes
+ */
+class WC_Tests_API_Product_Attributes extends WC_REST_Unit_Test_Case {
+
+	/**
+	 * Endpoints.
+	 *
+	 * @var string
+	 */
+	protected $endpoint = '/wc-analytics/products/attributes';
+
+	/**
+	 * Setup test reports categories data.
+	 *
+	 * @since 3.5.0
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->user = $this->factory->user->create(
+			array(
+				'role' => 'administrator',
+			)
+		);
+	}
+
+	/**
+	 * Test route registration.
+	 */
+	public function test_register_routes() {
+		$routes = $this->server->get_routes();
+
+		$this->assertArrayHasKey( $this->endpoint, $routes );
+	}
+
+	/**
+	 * Test request without valid permissions.
+	 */
+	public function test_get_reports_without_permission() {
+		wp_set_current_user( 0 );
+		$response = $this->server->dispatch( new WP_REST_Request( 'GET', $this->endpoint ) );
+		$this->assertEquals( 401, $response->get_status() );
+	}
+
+	/**
+	 * Test schema.
+	 */
+	public function test_reports_schema() {
+		wp_set_current_user( $this->user );
+
+		$request    = new WP_REST_Request( 'OPTIONS', $this->endpoint );
+		$response   = $this->server->dispatch( $request );
+		$data       = $response->get_data();
+		$properties = $data['schema']['properties'];
+
+		$this->assertEquals( 6, count( $properties ) );
+		$this->assertArrayHasKey( 'id', $properties );
+		$this->assertEquals( array( 'integer', 'string' ), $properties['id']['type'] );
+		$this->assertArrayHasKey( 'name', $properties );
+		$this->assertArrayHasKey( 'slug', $properties );
+		$this->assertArrayHasKey( 'type', $properties );
+		$this->assertArrayHasKey( 'order_by', $properties );
+		$this->assertArrayHasKey( 'has_archives', $properties );
+	}
+}
