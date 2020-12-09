5aed26a57643a1525dd30105aeb8dcc5780e4f24
Joshua Flowers <joshuatf@gmail.com>
2020-12-09 16:26:36 -0500

    Include opt out styles

--- client/wp-admin-scripts/navigation-opt-out/index.js
+++ client/wp-admin-scripts/navigation-opt-out/index.js
@@ -7,6 +7,7 @@ import { render } from '@wordpress/element';
  * Internal dependencies
  */
 import NavigationOptOutContainer from './container';
+import './style.scss';
 
 const navigationOptOutRoot = document.createElement( 'div' );
 navigationOptOutRoot.setAttribute( 'id', 'navigation-opt-out-root' );
