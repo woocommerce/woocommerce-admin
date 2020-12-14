61a016629aedca4f1d4c2f2f610039e0f2fe3624
Paul Sealock <psealock@gmail.com>
2020-12-14 14:11:26 +1300

    Use WP 5.6 instead of 5.6.0

--- src/Features/Navigation/Init.php
+++ src/Features/Navigation/Init.php
@@ -41,7 +41,7 @@ class Init {
 		include_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 		$gutenberg_minimum_version = '9.0.0'; // https://github.com/WordPress/gutenberg/releases/tag/v9.0.0.
-		$wp_minimum_version        = '5.6.0';
+		$wp_minimum_version        = '5.6';
 		$has_gutenberg             = is_plugin_active( 'gutenberg/gutenberg.php' );
 		$gutenberg_version         = $has_gutenberg ? get_plugin_data( WP_PLUGIN_DIR . '/gutenberg/gutenberg.php' )['Version'] : false;
 
