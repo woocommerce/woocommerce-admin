53a19acea092bf4472d4624f932a3bbaaa257daf
millerf <fab@millerf.com>
2020-12-15 14:44:55 +0100

    Allow setting Jetpack connection when JETPACK_MASTER_USER isn't available

--- src/Features/OnboardingSetUpShipping.php
+++ src/Features/OnboardingSetUpShipping.php
@@ -63,8 +63,13 @@ class OnboardingSetUpShipping {
 			return;
 		}
 
-		$user_token        = \Jetpack_Data::get_access_token( JETPACK_MASTER_USER );
-		$jetpack_connected = isset( $user_token->external_user_id );
+		if ( defined('JETPACK_MASTER_USER') ) {
+			$user_token = \Jetpack_Data::get_access_token( JETPACK_MASTER_USER );
+			$jetpack_connected = isset( $user_token->external_user_id );
+		} else {
+			$jetpack_connected = apply_filters( 'woocommerce_admin_is_jetpack_connected', false);
+		}
+
 		$wcs_version       = \WC_Connect_Loader::get_wcs_version();
 		$wcs_tos_accepted  = \WC_Connect_Options::get_option( 'tos_accepted' );
 
