83b6bf461b0b4d78bd21a86bdb7c11bf79de50ca
Fernando Marichal <contacto@fernandomarichal.com>
2020-12-18 14:46:12 -0300

    Templates refactored

--- includes/emails/html-mechant-notification.php
+++ includes/emails/html-mechant-notification.php
@@ -14,12 +14,18 @@ do_action( 'woocommerce_email_header', $email_heading, $email );
 
 echo esc_html( $email_content );
 ?>
-
-<a href="<?php echo esc_url( $email_action->query ); ?>">
-	<?php
-		echo esc_html( $email_action->label );
-	?>
-</a>
+<?php foreach ( $email_actions as $an_action ) : ?>
+<div>
+	<a href="<?php echo esc_url( $an_action->query ); ?>">
+		<?php
+			echo esc_html( $an_action->label );
+		?>
+	</a>
+</div>
+<?php endforeach; ?>
+<div style="opacity: 0;">
+	<img src="<?php echo esc_url( $opened_tracking_url ); ?>" />
+</div>
 <?php
 /*
  * @hooked WC_Emails::email_footer() Output the email footer
