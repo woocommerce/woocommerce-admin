c6df6d04671a7c7c15b99f50b170b95e917e750c
Fernando Marichal <contacto@fernandomarichal.com>
2020-12-18 14:45:21 -0300

    Refactored MerchantEmailNotifications and NotificationEmail

--- src/MerchantEmailNotifications/MerchantEmailNotifications.php
+++ src/MerchantEmailNotifications/MerchantEmailNotifications.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 
 /**
  * Merchant email notifications.
- * this gets all non-sent notes type email and send them.
+ * This gets all non-sent notes type `email` and sends them.
  */
 class MerchantEmailNotifications {
 	/**
@@ -37,18 +37,41 @@ class MerchantEmailNotifications {
 				self::send_mechant_notification( $note );
 				$note->set_status( 'sent' );
 				$note->save();
+				wc_admin_record_tracks_event( 'wcadmin_email_note_sent', array( 'note_name' => $note->get_name() ) );
 			}
 		}
 	}
 
 	/**
-	 * Sends the notification to the merchant.
+	 * Send the notification to the merchant.
 	 *
 	 * @param object $note The note to send.
 	 */
 	public static function send_mechant_notification( $note ) {
 		\WC_Emails::instance();
-		$email = new NotificationEmail( $note );
-		$email->trigger( get_current_user_id() );
+		$users_emails = self::get_emails_to_notice( $note );
+		foreach ( $users_emails as $user_email ) {
+			if ( is_email( $user_email ) ) {
+				$email = new NotificationEmail( $note );
+				$email->trigger( $user_email );
+			}
+		}
+	}
+
+	/**
+	 * Get email addresses by role to notify.
+	 *
+	 * @param object $note The note to send.
+	 * @return array Emails to notify
+	 */
+	public static function get_emails_to_notice( $note ) {
+		$content_data = $note->get_content_data();
+		$role         = 'administrator';
+		if ( isset( $content_data->role ) ) {
+			$role = $content_data->role;
+		}
+		$args  = array( 'role' => $role );
+		$users = get_users( $args );
+		return array_column( $users, 'user_email' );
 	}
 }
