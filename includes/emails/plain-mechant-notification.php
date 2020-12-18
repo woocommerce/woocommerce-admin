19959e3a81b18cd41723c90bb27e8d539c96e0ac
Fernando Marichal <contacto@fernandomarichal.com>
2020-12-18 14:46:12 -0300

    Templates refactored

--- includes/emails/plain-mechant-notification.php
+++ includes/emails/plain-mechant-notification.php
@@ -11,10 +11,12 @@ echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
 echo esc_html( wp_strip_all_tags( $email_heading ) );
 echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-/* translators: %1$s: report name, %2$s: download URL */
 echo wp_kses_post( $email_content );
-echo wp_kses_post( sprintf( __( 'Please click here: %s', 'woocommerce-admin' ), $email_action->query ) );
 
+foreach ( $email_actions as $an_action ) {
+	/* translators: %1$s: action label, %2$s: action URL */
+	echo wp_kses_post( sprintf( __( '\n%1$s: %2$s', 'woocommerce-admin' ), $an_action->label, $an_action->query ) );
+}
 echo "\n\n----------------------------------------\n\n";
 
 echo wp_kses_post( apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) ) );
