7838cf1576cbe294b4117e21ad9e34f164256f2b
Fernando Marichal <contacto@fernandomarichal.com>
2020-12-30 14:53:55 -0300

    Fixed email styles

--- includes/emails/html-mechant-notification.php
+++ includes/emails/html-mechant-notification.php
@@ -13,10 +13,27 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_email_header', $email_heading, $email );
 
 echo esc_html( $email_content );
+
+$base_color       = get_option( 'woocommerce_email_base_color' );
+$base_text        = wc_light_or_dark( $base_color, '#202020', '#ffffff' );
+$container_styles = 'margin-top: 25px;';
+$buttons_styles   = "
+	font-style: normal;
+	font-weight: normal;
+	font-size: 13px;
+	line-height: 18px;
+	text-align: center;
+	color: {$base_text};
+	margin-right: 15px;
+	text-decoration: none;
+	background: {$base_color};
+	border: 1px solid {$base_color};
+	border-radius: 3px;
+	padding: 5px;";
 ?>
 <?php foreach ( $email_actions as $an_action ) : ?>
-<div>
-	<a href="<?php echo esc_url( $an_action->query ); ?>">
+<div style="<?php echo esc_attr( $container_styles ); ?>">
+	<a href="<?php echo esc_url( $an_action->query ); ?>" style="<?php echo esc_attr( $buttons_styles ); ?>">
 		<?php
 			echo esc_html( $an_action->label );
 		?>
