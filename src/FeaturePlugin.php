e9014ef95efe566611bbfb195faf17d64d82793e
Moon <moon.kyong@automattic.com>
2021-01-05 18:32:19 -0800

    Remove note from FeaturePlugin -- it is unncessary

--- src/FeaturePlugin.php
+++ src/FeaturePlugin.php
@@ -192,7 +192,6 @@ class FeaturePlugin {
 		new SetUpAdditionalPaymentTypes();
 		new TestCheckout();
 		new SellingOnlineCourses();
-		new InsightFirstProductAndPayment();
 
 		// Initialize RemoteInboxNotificationsEngine.
 		RemoteInboxNotificationsEngine::init();
