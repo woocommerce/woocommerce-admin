6d312700f113640179630a86a2575da6c1e5f4a6
Sam Seay <samueljseay@gmail.com>
2021-01-07 15:24:51 +1300

    Move to core js 3 for babel-preset-env, cement browser support options.

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,41 @@
-const { babelConfig: e2eBabelConfig } = require( '@woocommerce/e2e-environment' );
+const {
+	babelConfig: e2eBabelConfig,
+} = require( '@woocommerce/e2e-environment' );
 
-module.exports = function( api ) {
+module.exports = function ( api ) {
 	api.cache( true );
 
 	return {
-        ...e2eBabelConfig,
-        presets: [
-              ...e2eBabelConfig.presets,
-              '@wordpress/babel-preset-default',
-        ],
-        plugins: [
+		...e2eBabelConfig,
+		presets: [
+			'@wordpress/babel-preset-default',
+			[
+				'@babel/preset-env',
+				{
+					targets: {
+						node: 'current',
+						browsers: [
+							'> 1%',
+							'ie >= 11',
+							'last 1 Android versions',
+							'last 1 ChromeAndroid versions',
+							'last 2 Chrome versions',
+							'last 2 Firefox versions',
+							'last 2 Safari versions',
+							'last 2 iOS versions',
+							'last 2 Edge versions',
+							'last 2 Opera versions',
+						],
+					},
+					useBuiltIns: 'usage',
+					corejs: {
+						version: 3,
+						proposals: true,
+					},
+				},
+			],
+		],
+		plugins: [
 			/**
 			 * This allows arrow functions as class methods so that binding
 			 * methods to `this` in the constructor isn't required.
