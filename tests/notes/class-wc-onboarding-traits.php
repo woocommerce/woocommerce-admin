39b9ca3aa01bd697ff61505630e8f330d09f79d7
Sam Seay <samueljseay@gmail.com>
2021-01-18 15:54:48 +1300

    Have proper teardown/setup, and fix key name of option.

--- tests/notes/class-wc-onboarding-traits.php
+++ tests/notes/class-wc-onboarding-traits.php
@@ -15,12 +15,36 @@ class WC_Tests_Onboarding_Traits extends WC_Unit_Test_Case {
 	/** Host the traits class we are testing */
 	use OnboardingTraits;
 
+	/**
+	 * The options that we modify during this test suite.
+	 *
+	 * @var array
+	 */
+	private static $original_options;
+
+	/**
+	 * Setup for the whole test class.
+	 */
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		self::$original_options = get_option( 'woocommerce_onboarding_profile', array() );
+	}
+
+		/**
+		 * Cleans up test data once all test have run.
+		 */
+	public static function tearDownAfterClass() {
+		parent::tearDownAfterClass();
+
+		update_option( 'woocommerce_onboarding_profile', self::$original_options );
+	}
 
 	/**
 	 * Test revenue_is_within functionality.
 	 */
 	public function test_revenue_is_within() {
-		update_option( 'onboarding_profile', (object) array( 'revenue' => 1500 ) );
+		update_option( 'woocommerce_onboarding_profile', (object) array( 'revenue' => 1500 ) );
 
 		$this->assertEquals( self::revenue_is_within( 1200, 1500 ), true );
 		$this->assertEquals( self::revenue_is_within( 0, 1500 ), true );
@@ -32,10 +56,10 @@ class WC_Tests_Onboarding_Traits extends WC_Unit_Test_Case {
 	 * Test onboarding_started functionality.
 	 */
 	public function test_onboarding_started() {
-		update_option( 'onboarding_profile', null );
+		update_option( 'woocommerce_onboarding_profile', null );
 		$this->assertEquals( self::onboarding_profile_started(), false );
 
-		update_option( 'onboarding_profile', array() );
+		update_option( 'woocommerce_onboarding_profile', array() );
 		$this->assertEquals( self::onboarding_profile_started(), true );
 	}
 
@@ -43,13 +67,13 @@ class WC_Tests_Onboarding_Traits extends WC_Unit_Test_Case {
 	 * Test store_setup_for_client functionality.
 	 */
 	public function test_store_setup_for_client() {
-		update_option( 'onboarding_profile', (object) array( 'setup_client' => true ) );
+		update_option( 'woocommerce_onboarding_profile', (object) array( 'setup_client' => true ) );
 		$this->assertEquals( self::store_setup_for_client(), true );
 
-		update_option( 'onboarding_profile', null );
+		update_option( 'woocommerce_onboarding_profile', null );
 		$this->assertEquals( self::store_setup_for_client(), false );
 
-		update_option( 'onboarding_profile', (object) array( 'setup_client' => false ) );
+		update_option( 'woocommerce_onboarding_profile', (object) array( 'setup_client' => false ) );
 		$this->assertEquals( self::store_setup_for_client(), false );
 	}
 }
