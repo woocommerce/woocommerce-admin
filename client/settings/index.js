36e0a55c35e56514fc96badf2f6201620d1123d1
Joshua Flowers <joshuatf@gmail.com>
2021-01-19 14:13:49 -0500

    Retrieve settings from data store

--- client/settings/index.js
+++ client/settings/index.js
@@ -1,3 +1,24 @@
-export default ( {} ) => {
+/**
+ * External dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { SETTINGS_STORE_NAME } from '@woocommerce/data';
+import { Spinner } from '@wordpress/components';
+
+export default ( { params } ) => {
+	const { settings, isLoading } = useSelect( ( select ) => {
+		return {
+			isLoading: ! select(
+				SETTINGS_STORE_NAME
+			).hasFinishedResolution( 'getSettings', [ params.page ] ),
+			settings:
+				select( SETTINGS_STORE_NAME ).getSettings( params.page ) || {},
+		};
+	} );
+
+	if ( isLoading ) {
+		return <Spinner />;
+	}
+
 	return <div>Settings page</div>;
 };
