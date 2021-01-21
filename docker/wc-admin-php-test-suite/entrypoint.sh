ba22fab5019f037f68f8d8a60991760ef97fc910
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-01-21 17:51:26 +0800

    Add WooCommerce version selection
    The WC_VERSION environment variable is added to the docker-compose config. If the variable is not set on the shell, it defaults to the minimum supported version (4.5.0)
    
    The ENTRYPOINT is changed to automatically re-install the test suite when the version number is changed.

--- docker/wc-admin-php-test-suite/entrypoint.sh
+++ docker/wc-admin-php-test-suite/entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Function to install the test suite.
+function install {
+	# Delete previous state.
+	rm -f /tmp/.INSTALLED_WC_VERSION
+
+	# Run the install script.
+	bin/install-wp-tests.sh $DB_NAME $DB_USER $DB_PASS $DB_HOST latest true
+
+	# Remember the installed WooCommerce version.
+	echo "$WC_VERSION" > /tmp/.INSTALLED_WC_VERSION
+}
+
+# Run the install script if the installed WooCommerce version is unknown.
+if [ ! -f /tmp/.INSTALLED_WC_VERSION ]; then
+	install
+else
+	# Run the install script if the WooCommerce version has changed.
+	INSTALLED_WC_VERSION=`cat /tmp/.INSTALLED_WC_VERSION`
+	if [ "$WC_VERSION" != "$INSTALLED_WC_VERSION" ]; then
+		install
+	fi
+fi
+
 # Run the install script if the WordPress directory is not found.
 if [ ! -d /tmp/wordpress-tests-lib ]; then
-	bin/install-wp-tests.sh $DB_NAME $DB_USER $DB_PASS $DB_HOST latest true
+	install
 fi
 
 exec phpunit "$@"
