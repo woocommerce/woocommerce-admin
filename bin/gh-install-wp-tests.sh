bde90e30eac61f172afb20bb9790b5ea86aebc36
Sam Seay <samueljseay@gmail.com>
2021-01-26 21:43:30 +1300

    Simplify the install script for gh.

--- bin/gh-install-wp-tests.sh
+++ bin/gh-install-wp-tests.sh
@@ -138,26 +138,17 @@ install_deps() {
 	cd "woocommerce"
  	composer install --no-dev
 
+  # Activate WooCommerce
 	cd "$WP_CORE_DIR"
 	php wp-cli.phar plugin activate woocommerce
+	
+  # Install woocommerce-admin
+  cd "$WP_CORE_DIR/wp-content/plugins"
+  cp -R $GITHUB_WORKSPACE ./
 
-	# Install woocommerce-admin, the correct branch, if running from Travis CI.
-	if [ "${TRAVIS}" = "true" ]; then
-		# Default to the pull request branch if this is a PR, otherwise pushed branch.
-		BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-		# Default to the pull request repo if this is a PR, otherwise default.
-		REPO=${TRAVIS_PULL_REQUEST_SLUG:-$TRAVIS_REPO_SLUG}
-		BRANCH="$(sed 's/#/%23/' <<<$BRANCH)"
-		# Checkout plugin via Git so all files are gathered.
-		cd "$WP_CORE_DIR/wp-content/plugins"
-		git clone https://github.com/$REPO.git
-		cd woocommerce-admin
-		git fetch origin $BRANCH
-		git checkout -B $BRANCH origin/$BRANCH
-		# Activate the plugin
-		cd "$WP_CORE_DIR"
-		php wp-cli.phar plugin activate woocommerce-admin
-	fi
+  # Activate woocommerce-admin
+	cd "$WP_CORE_DIR"
+  php wp-cli.phar plugin activate woocommerce-admin
 
 	# Back to original dir
 	cd "$WORKING_DIR"
