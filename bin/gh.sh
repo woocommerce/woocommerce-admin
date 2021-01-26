1fbdb615e876e135c56e71b881c6e02c2f4cd21a
Sam Seay <samueljseay@gmail.com>
2021-01-26 21:52:50 +1300

    Build phpunit versioning into GH script.

--- bin/gh.sh
+++ bin/gh.sh
@@ -3,6 +3,6 @@ npm run build:feature-config
 composer install --no-dev
 	
 # phpunit ^7 is not supported in php 7.0
-if [[ "$PHPUNIT" == "6" ]]; then
-  curl -fsSL -o vendor/bin/phpunit https://phar.phpunit.de/phpunit-6.5.9.phar --create-dirs && chmod +x vendor/bin/phpunit
-fi
+# if [[ "$PHPUNIT" == "6" ]]; then
+#   curl -fsSL -o vendor/bin/phpunit https://phar.phpunit.de/phpunit-6.5.9.phar --create-dirs && chmod +x vendor/bin/phpunit
+# fi
