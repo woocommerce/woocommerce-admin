ec964368c4416b87bb25c39fb25579f80cf1227e
Sam Seay <samueljseay@gmail.com>
2021-01-26 23:01:35 +1300

    Restore phpunit script.

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
-# if [[ {$COMPOSER_DEV} == 1 ]]; then
-# 	./vendor/bin/phpunit --version
-# 	if [[ {$RUN_RANDOM} == 1 ]]; then
-# 		./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random
-# 	else
-# 		./vendor/bin/phpunit -c phpunit.xml.dist
-# 	fi
-# else
+if [[ {$COMPOSER_DEV} == 1 ]]; then
+	./vendor/bin/phpunit --version
+	if [[ {$RUN_RANDOM} == 1 ]]; then
+		./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random
+	else
+		./vendor/bin/phpunit -c phpunit.xml.dist
+	fi
+else
 	phpunit --version
 	phpunit -c phpunit.xml.dist
-# fi
+fi
 TEST_RESULT=$?
 cd "$WORKING_DIR"
 exit $TEST_RESULT
