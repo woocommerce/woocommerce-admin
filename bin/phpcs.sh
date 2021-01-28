a97abdae72e5f42bba41aa858e2b3ffd73286c48
Sam Seay <samueljseay@gmail.com>
2021-01-28 16:47:06 +1300

    Re-enable the php linting.

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
+CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
 
-if [[ ${RUN_PHPCS} == 1 ]]; then
-	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
-
-	if [ "$CHANGED_FILES" != "" ]; then
-		echo "Running Code Sniffer."
-		cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
-		./vendor/bin/phpcs --encoding=utf-8 -n -p $CHANGED_FILES
-	fi
+if [ "$CHANGED_FILES" != "" ]; then
+	echo "Running Code Sniffer."
+	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
+	./vendor/bin/phpcs --encoding=utf-8 -n -p $CHANGED_FILES
 fi
+
