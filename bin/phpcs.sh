18f54e3f0c449882b9a186a22fdc051b708e227b
Sam Seay <samueljseay@gmail.com>
2021-02-01 14:03:54 +1300

    Write a script to filter php files from the changed files.

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 composer install --no-dev
 
+PHP_FILES_CHANGED=""
+
+for FILE in $(echo $CHANGED_FILES | tr ',' '\n')
+do
+	if [[ $string =~ ".php" ]]; then
+		PHP_FILES_CHANGED += "$FILE,"
+	fi	
+done
+
 if [ "$CHANGED_FILES" != "" ]; then
 	echo "Running Code Sniffer."
 	./vendor/bin/phpcs --encoding=utf-8 -n -p $CHANGED_FILES
