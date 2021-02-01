f944b6344bfb31c8b19c58c0e487e01dd8f8b72a
Sam Seay <samueljseay@gmail.com>
2021-02-01 13:19:20 +1300

    Try move to dir before diff.

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $COMMIT_START...$COMMIT_END | grep \\\\.php | awk '{print}' ORS=' '`
 
 if [ "$CHANGED_FILES" != "" ]; then
