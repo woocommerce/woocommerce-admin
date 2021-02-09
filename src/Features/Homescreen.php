572b429b8c01e9d95d222753ce20aa3d25da5b82
Paul Sealock <psealock@gmail.com>
2021-02-09 17:06:50 +1300

    migrate to admin_menu

--- src/Features/Homescreen.php
+++ src/Features/Homescreen.php
@@ -40,8 +40,7 @@ class Homescreen {
 	public function __construct() {
 		add_filter( 'woocommerce_admin_get_user_data_fields', array( $this, 'add_user_data_fields' ) );
 		add_action( 'admin_menu', array( $this, 'register_page' ) );
-		// priority is 20 to run after https://github.com/woocommerce/woocommerce/blob/a55ae325306fc2179149ba9b97e66f32f84fdd9c/includes/admin/class-wc-admin-menus.php#L165.
-		add_action( 'admin_head', array( $this, 'update_link_structure' ), 20 );
+		add_action( 'admin_menu', array( $this, 'update_link_structure' ), 11 );
 		add_filter( 'woocommerce_admin_plugins_whitelist', array( $this, 'get_homescreen_allowed_plugins' ) );
 		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
 		add_filter( 'woocommerce_shared_settings', array( $this, 'component_settings' ), 20 );
