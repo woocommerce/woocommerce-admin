4054107e9b13b42061236a8c60f456fe97eb5c7f
Matt Sherman <matt@jam123.com>
2021-02-10 10:46:43 -0500

    Update comment to better document why priority is being set on admin_menu

--- src/Features/Homescreen.php
+++ src/Features/Homescreen.php
@@ -40,7 +40,7 @@ class Homescreen {
 	public function __construct() {
 		add_filter( 'woocommerce_admin_get_user_data_fields', array( $this, 'add_user_data_fields' ) );
 		add_action( 'admin_menu', array( $this, 'register_page' ) );
-		// priority is 20 to run after https://github.com/woocommerce/woocommerce/blob/a55ae325306fc2179149ba9b97e66f32f84fdd9c/includes/admin/class-wc-admin-menus.php#L165.
+		// priority is 20 to run after admin_menu hook for woocommerce runs, so that submenu is populated.
 		add_action( 'admin_menu', array( $this, 'update_link_structure' ), 20 );
 		add_filter( 'woocommerce_admin_plugins_whitelist', array( $this, 'get_homescreen_allowed_plugins' ) );
 		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
