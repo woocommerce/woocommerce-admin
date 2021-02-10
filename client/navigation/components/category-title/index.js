1e23e017d572f6fa55291c20f4fee4e5904aaf0b
Joshua Flowers <joshuatf@gmail.com>
2021-02-10 13:40:37 -0500

    Hide tooltips if previously shown

--- client/navigation/components/category-title/index.js
+++ client/navigation/components/category-title/index.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
-import { NAVIGATION_STORE_NAME } from '@woocommerce/data';
+import { NAVIGATION_STORE_NAME, OPTIONS_STORE_NAME } from '@woocommerce/data';
 import { recordEvent } from '@woocommerce/tracks';
 import { useDispatch, useSelect } from '@wordpress/data';
 
@@ -13,21 +13,36 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import './style.scss';
 import { HighlightTooltip } from '../../../header/activity-panel/highlight-tooltip';
 
+const tooltipHiddenOption = 'woocommerce_navigation_favorite_tooltip_hidden';
+
 export const CategoryTitle = ( { category } ) => {
 	const { id, title } = category;
 
-	const { favorites, isResolving } = useSelect( ( select ) => {
+	const {
+		favorites,
+		isOptionResolving,
+		isResolving,
+		isTooltipHidden,
+	} = useSelect( ( select ) => {
 		return {
 			favorites: select( NAVIGATION_STORE_NAME ).getFavorites(),
+			isOptionResolving: select(
+				OPTIONS_STORE_NAME
+			).isResolving( 'getOption', [ tooltipHiddenOption ] ),
 			isResolving: select( NAVIGATION_STORE_NAME ).isResolving(
 				'getFavorites'
 			),
+			isTooltipHidden:
+				select( OPTIONS_STORE_NAME ).getOption(
+					tooltipHiddenOption
+				) === 'yes',
 		};
 	} );
 
 	const { addFavorite, removeFavorite } = useDispatch(
 		NAVIGATION_STORE_NAME
 	);
+	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
 
 	const isFavorited = ( favorites || [] ).includes( id );
 	const className = 'woocommerce-navigation-category-title';
@@ -64,23 +79,30 @@ export const CategoryTitle = ( { category } ) => {
 									  )
 							}
 						/>
-						<HighlightTooltip
-							delay={ 1000 }
-							title={ __(
-								'Introducing favorites',
-								'woocommerce-admin'
-							) }
-							content={ __(
-								'You can now favorite your extensions to pin them in the top level of the navigation.',
-								'woocommerce-admin'
-							) }
-							closeButtonText={ __(
-								'Got it',
-								'woocommerce-admin'
-							) }
-							id="woocommerce-navigation-category-title__favorite-button"
-							useAnchor
-						/>
+						{ ! isTooltipHidden && ! isOptionResolving && (
+							<HighlightTooltip
+								delay={ 1000 }
+								title={ __(
+									'Introducing favorites',
+									'woocommerce-admin'
+								) }
+								content={ __(
+									'You can now favorite your extensions to pin them in the top level of the navigation.',
+									'woocommerce-admin'
+								) }
+								closeButtonText={ __(
+									'Got it',
+									'woocommerce-admin'
+								) }
+								id="woocommerce-navigation-category-title__favorite-button"
+								onClose={ () =>
+									updateOptions( {
+										[ tooltipHiddenOption ]: 'yes',
+									} )
+								}
+								useAnchor
+							/>
+						) }
 					</>
 				) }
 			</span>
