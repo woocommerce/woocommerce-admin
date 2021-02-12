571fa7e2b11a1a658002bf44addf5e0a0cf800f0
hsing <hsingyuc.7@gmail.com>
2021-02-11 19:23:33 -0500

    Handle PR feedback-2

--- client/wp-admin-scripts/onboarding-tax-notice/index.js
+++ client/wp-admin-scripts/onboarding-tax-notice/index.js
@@ -3,19 +3,21 @@
  */
 import { __ } from '@wordpress/i18n';
 import { dispatch } from '@wordpress/data';
+import domReady from '@wordpress/dom-ready';
 import { getAdminLink } from '@woocommerce/wc-admin-settings';
 
 /**
  * Returns a promise and resolves when the loader overlay no longer exists.
  *
- * @return {Promise} Promise for overlay existence.
+ * @param {HTMLElement} saveButton Save button DOM element.
+ * @return {Promise} Promise for save status.
  */
-const saveCompleted = () => {
-	if ( document.querySelector( '.blockUI.blockOverlay' ) !== null ) {
+const saveCompleted = ( saveButton ) => {
+	if ( saveButton && ! saveButton.disabled ) {
 		const promise = new Promise( ( resolve ) => {
 			window.requestAnimationFrame( resolve );
 		} );
-		return promise.then( () => saveCompleted() );
+		return promise.then( () => saveCompleted( saveButton ) );
 	}
 
 	return Promise.resolve( true );
@@ -31,32 +33,37 @@ const showTaxCompletionNotice = () => {
 		return;
 	}
 
-	saveCompleted().then( () => {
+	saveCompleted( saveButton ).then( () => {
 		// Check if a row was added successfully after WooCommerce removes invalid rows.
-		if ( ! document.querySelector( '.tips' ) ) {
+		if ( ! document.querySelector( '.wc_tax_rates .tips' ) ) {
 			return;
 		}
-		if ( saveButton.disabled ) {
-			saveButton.classList.add( 'has-tax' );
-			dispatch( 'core/notices' ).createSuccessNotice(
-				__( "You've added your first tax rate!", 'woocommerce-admin' ),
-				{
-					id: 'WOOCOMMERCE_ONBOARDING_TAX_NOTICE',
-					actions: [
-						{
-							url: getAdminLink( 'admin.php?page=wc-admin' ),
-							label: __( 'Continue setup.', 'woocommerce-admin' ),
-						},
-					],
-				}
-			);
-		}
+
+		saveButton.classList.add( 'has-tax' );
+		dispatch( 'core/notices' ).createSuccessNotice(
+			__( "You've added your first tax rate!", 'woocommerce-admin' ),
+			{
+				id: 'WOOCOMMERCE_ONBOARDING_TAX_NOTICE',
+				actions: [
+					{
+						url: getAdminLink( 'admin.php?page=wc-admin' ),
+						label: __( 'Continue setup.', 'woocommerce-admin' ),
+					},
+				],
+			}
+		);
 	} );
 };
 
-window.onload = () => {
+domReady( () => {
 	const saveButton = document.querySelector( '.woocommerce-save-button' );
-	if ( ! document.querySelector( '.wc_tax_rates .tips' ) && saveButton ) {
+
+	if (
+		window.htmlSettingsTaxLocalizeScript &&
+		window.htmlSettingsTaxLocalizeScript.rates &&
+		! window.htmlSettingsTaxLocalizeScript.rates.length &&
+		saveButton
+	) {
 		saveButton.addEventListener( 'click', showTaxCompletionNotice );
 	}
-};
+} );
