7acb680858d968d00d7b7a14039aa99b61ebbe36
Sam Seay <samueljseay@gmail.com>
2021-02-23 13:54:53 +1300

    Fix syntax errors and make the grep stricter.

--- bin/ci/lint-changelog.sh
+++ bin/ci/lint-changelog.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-$changelog_entry=$(sed -n '/^== Unreleased ==/,/^==/p' readme.txt | grep $PR_NUMBER)
+changelog_entry=$(sed -n '/^== Unreleased ==/,/^==/p' readme.txt | grep -w "#$PR_NUMBER")
 
 if [ -z "$changelog_entry" ]
 then
-  echo "Error: No changelog entry was provided for $PR_NUMBER"
+  echo "Error: No changelog entry was provided for #$PR_NUMBER"
   exit 1
 fi
