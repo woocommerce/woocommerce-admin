e0b35ca23ecdfa2055112e81dfacc1471c2f319e
Rebecca Scott <me@becdetat.com>
2021-02-25 17:02:14 +1000

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '2.0.2',
+    'version' => '2.0.2.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'woocommerce/woocommerce-admin',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.9.1',
+      'version' => '2.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd6ca2cc26ad6963e1be19b3338a9e98f40d9bd88',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.9.0',
+      'version' => '1.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b93bcf0fa1fccb0b7d176b0967d969691cd74cca',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'woocommerce/woocommerce-admin' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
