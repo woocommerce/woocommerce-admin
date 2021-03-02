ac64d25b04a8180d7f6c2e1f9b4b02d6f186fff8
Lourens Schep <lourensschep@gmail.com>
2021-03-02 13:04:31 -0400

    Update set up environment and jest package

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -1,11 +1,12 @@
 const path = require( 'path' );
+const { E2E_TIMEOUT } = process.env;
 // Eventually update e2e-environment to support playwright as well.
 // const { jestConfig: baseE2Econfig } = require( '@woocommerce/e2e-environment' );
 
 module.exports = {
 	preset: 'jest-playwright-preset',
 	roots: [ path.resolve( __dirname, '../specs' ) ],
-	testTimeout: 30000,
+	testTimeout: E2E_TIMEOUT || 60000,
 	setupFilesAfterEnv: [
 		path.resolve( __dirname, '../setup-env.js' ),
 		'expect-playwright',
