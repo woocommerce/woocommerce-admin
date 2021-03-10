a6ee1d258d8e9ddcb6645406dc2413f5606c3b19
Sam Seay <samueljseay@gmail.com>
2021-03-10 15:41:54 +1300

    Adjust expected prop count in test 🤦

--- tests/api/onboarding-profile.php
+++ tests/api/onboarding-profile.php
@@ -104,7 +104,7 @@ class WC_Tests_API_Onboarding_Profiles extends WC_REST_Unit_Test_Case {
 		$data       = $response->get_data();
 		$properties = $data['schema']['properties'];
 
-		$this->assertCount( 16, $properties );
+		$this->assertCount( 14, $properties );
 		$this->assertArrayHasKey( 'completed', $properties );
 		$this->assertArrayHasKey( 'skipped', $properties );
 		$this->assertArrayHasKey( 'industry', $properties );
