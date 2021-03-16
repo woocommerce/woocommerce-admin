bdd1d585ea16a3bbb8c16f757dcdda7788a05ce6
Moon <moon.kyong@automattic.com>
2021-03-15 22:03:17 -0700

    Add test for filtering selected extensions

--- client/profile-wizard/steps/business-details/flows/selective-bundle/index.js
+++ client/profile-wizard/steps/business-details/flows/selective-bundle/index.js
@@ -36,6 +36,26 @@ import './style.scss';
 const BUSINESS_DETAILS_TAB_NAME = 'business-details';
 const FREE_FEATURES_TAB_NAME = 'free-features';
 
+export const filterBusinessExtensions = ( extensionInstallationOptions ) => {
+	return (
+		Object.keys( extensionInstallationOptions )
+			.filter(
+				( key ) =>
+					extensionInstallationOptions[ key ] &&
+					key !== 'install_extensions'
+			)
+			.map( ( key ) => {
+				// Remove anything after :
+				// Please refer to selective-extensions-bundle/index.js
+				// installableExtensions variable
+				// this is to allow duplicate slugs (Tax & Shipping for example)
+				return key.split( ':' )[ 0 ];
+			} )
+			// remove duplicate
+			.filter( ( item, index, arr ) => arr.indexOf( item ) === index )
+	);
+};
+
 class BusinessDetails extends Component {
 	constructor() {
 		super();
@@ -69,21 +89,9 @@ class BusinessDetails extends Component {
 
 		const { getCurrencyConfig } = this.context;
 
-		const businessExtensions = Object.keys( extensionInstallationOptions )
-			.filter(
-				( key ) =>
-					extensionInstallationOptions[ key ] &&
-					key !== 'install_extensions'
-			)
-			.map( ( key ) => {
-				// Remove anything after :
-				// Please refer to selective-extensions-bundle/index.js
-				// installableExtensions variable
-				// this is to allow duplicate slugs (Tax & Shipping for example)
-				return key.split( ':' )[ 0 ];
-			} )
-			// remove duplicate
-			.filter( ( item, index, arr ) => arr.indexOf( item ) === index );
+		const businessExtensions = filterBusinessExtensions(
+			extensionInstallationOptions
+		);
 
 		recordEvent( 'storeprofiler_store_business_features_continue', {
 			product_number: productCount,
