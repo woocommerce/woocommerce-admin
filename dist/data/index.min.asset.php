4ea7490f77b41d6cbb3dadf59c1c1996bee85808
Joshua Flowers <joshuatf@gmail.com>
2021-03-22 18:02:25 -0400

    Adding /dist directory to release

---
+++ dist/data/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'wc-date', 'wc-navigation', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '06fec751f2f1fe965afde41ccdf7c048');
