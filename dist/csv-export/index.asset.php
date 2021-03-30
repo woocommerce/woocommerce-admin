c21563451e5b36635a2e3f9c528da8f61130f8ee
Joshua Flowers <joshuatf@gmail.com>
2021-03-30 16:53:23 -0400

    Adding /dist directory to release

---
+++ dist/csv-export/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment'), 'version' => 'e4491f432f511ac10a1e1e819d0e15c7');
---
+++ dist/customer-effort-score/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '98fb942d0273698fddd7a602d1404cf3');
