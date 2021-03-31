3ac9ce62aada41bc4029b1251bb15d6bf4638f66
Sam Seay <samueljseay@gmail.com>
2021-04-01 00:03:15 +1300

    Try some config tweaks.

--- tests/e2e/config/jest-puppeteer.config.js
+++ tests/e2e/config/jest-puppeteer.config.js
@@ -2,6 +2,7 @@ const { useE2EJestPuppeteerConfig } = require( '@woocommerce/e2e-environment' );
 
 const puppeteerConfig = useE2EJestPuppeteerConfig( {
 	launch: {
+		browserContext: 'incognito',
 		args: [ '--incognito' ],
 	},
 } );
