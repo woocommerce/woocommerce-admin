f8abaed640b94974eb4401a53f1d6b41ccbabb9b
Sam Seay <samueljseay@gmail.com>
2021-04-13 14:07:06 +1200

    Duplicate some styles in the higher level woocommerce-list-item element, to make them work in the new list.

--- packages/components/src/list/test/index.js
+++ packages/components/src/list/test/index.js
@@ -3,6 +3,7 @@
  */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { ExperimentalListItem } from '../experimental-list-item';
 
 /**
  * Internal dependencies
@@ -10,132 +11,193 @@ import userEvent from '@testing-library/user-event';
 import List from '../index';
 
 describe( 'List', () => {
-	it( 'should have aria roles for items', () => {
-		const clickHandler = jest.fn();
-		const listItems = [
-			{
-				title: 'WooCommerce.com',
-				href: 'https://woocommerce.com',
-			},
-			{
-				title: 'Click me!',
-				onClick: clickHandler,
-			},
-		];
+	describe( 'Experimental List', () => {
+		it( 'should render the new List which defaults to a nav component if items are not passed in', () => {
+			const { container } = render(
+				<List>
+					<div>Test</div>
+				</List>
+			);
 
-		render( <List items={ listItems } /> );
+			expect( container.querySelector( 'nav' ) ).toBeInTheDocument();
+		} );
 
-		expect( screen.getAllByRole( 'menuitem' ) ).toHaveLength( 2 );
+		it( 'should render children passed in', () => {
+			const { container } = render(
+				<List>
+					<div>Test</div>
+				</List>
+			);
+
+			expect( container ).toHaveTextContent( 'Test' );
+		} );
+
+		it( 'should allow overriding the wrapping element, and passing in arbitrary element props', () => {
+			const { container } = render(
+				<List component="ul" role="menu">
+					<div>Test</div>
+				</List>
+			);
+
+			expect( container.querySelector( 'ul' ) ).toBeInTheDocument();
+		} );
+
+		describe( 'ExperimentalListItem', () => {
+			it( 'should render children passed in', () => {
+				const { container } = render(
+					<ExperimentalListItem>
+						<div>Test</div>
+					</ExperimentalListItem>
+				);
+
+				expect( container ).toHaveTextContent( 'Test' );
+			} );
+
+			it( 'should allow overriding the wrapping element, and passing in arbitrary element props', () => {
+				const { container } = render(
+					<ExperimentalListItem
+						component="a"
+						href="https://example.com"
+					>
+						<div>Test</div>
+					</ExperimentalListItem>
+				);
+
+				expect(
+					container.querySelector( "a[href='https://example.com']" )
+				).toBeInTheDocument();
+			} );
+		} );
 	} );
 
-	it( 'should support `onClick` for items', () => {
-		const clickHandler = jest.fn();
-		const listItems = [
-			{
-				title: 'WooCommerce.com',
-				href: 'https://woocommerce.com',
-			},
-			{
-				title: 'Click me!',
-				onClick: clickHandler,
-			},
-		];
+	describe( 'Legacy List', () => {
+		it( 'should have aria roles for items', () => {
+			const clickHandler = jest.fn();
+			const listItems = [
+				{
+					title: 'WooCommerce.com',
+					href: 'https://woocommerce.com',
+				},
+				{
+					title: 'Click me!',
+					onClick: clickHandler,
+				},
+			];
 
-		render( <List items={ listItems } /> );
+			render( <List items={ listItems } /> );
 
-		userEvent.click(
-			screen.getByRole( 'menuitem', { name: 'Click me!' } )
-		);
+			expect( screen.getAllByRole( 'menuitem' ) ).toHaveLength( 2 );
+		} );
 
-		expect( clickHandler ).toHaveBeenCalled();
-	} );
+		it( 'should support `onClick` for items', () => {
+			const clickHandler = jest.fn();
+			const listItems = [
+				{
+					title: 'WooCommerce.com',
+					href: 'https://woocommerce.com',
+				},
+				{
+					title: 'Click me!',
+					onClick: clickHandler,
+				},
+			];
 
-	it( 'should set `data-link-type` on items', () => {
-		const listItems = [
-			{
-				title: 'Add products',
-				href: '/post-new.php?post_type=product',
-				linkType: 'wp-admin',
-			},
-			{
-				title: 'Market my store',
-				href: '/admin.php?page=wc-admin&path=%2Fmarketing',
-				linkType: 'wc-admin',
-			},
-			{
-				title: 'WooCommerce.com',
-				href: 'https://woocommerce.com',
-				linkType: 'external',
-			},
-			{
-				title: 'WordPress.org',
-				href: 'https://wordpress.org',
-			},
-		];
+			render( <List items={ listItems } /> );
 
-		render( <List items={ listItems } /> );
+			userEvent.click(
+				screen.getByRole( 'menuitem', { name: 'Click me!' } )
+			);
 
-		expect(
-			screen.getByRole( 'menuitem', { name: 'Add products' } ).dataset
-				.linkType
-		).toBe( 'wp-admin' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'Market my store' } ).dataset
-				.linkType
-		).toBe( 'wc-admin' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'WooCommerce.com' } ).dataset
-				.linkType
-		).toBe( 'external' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'WordPress.org' } ).dataset
-				.linkType
-		).toBe( 'external' );
-	} );
+			expect( clickHandler ).toHaveBeenCalled();
+		} );
 
-	it( 'should set `data-list-item-tag` on items', () => {
-		const listItems = [
-			{
-				title: 'Add products',
-				href: '/post-new.php?post_type=product',
-				linkType: 'wp-admin',
-				listItemTag: 'add-product',
-			},
-			{
-				title: 'Market my store',
-				href: '/admin.php?page=wc-admin&path=%2Fmarketing',
-				linkType: 'wc-admin',
-				listItemTag: 'marketing',
-			},
-			{
-				title: 'WooCommerce.com',
-				href: 'https://woocommerce.com',
-				linkType: 'external',
-				listItemTag: 'woocommerce.com-site',
-			},
-			{
-				title: 'WordPress.org',
-				href: 'https://wordpress.org',
-			},
-		];
+		it( 'should set `data-link-type` on items', () => {
+			const listItems = [
+				{
+					title: 'Add products',
+					href: '/post-new.php?post_type=product',
+					linkType: 'wp-admin',
+				},
+				{
+					title: 'Market my store',
+					href: '/admin.php?page=wc-admin&path=%2Fmarketing',
+					linkType: 'wc-admin',
+				},
+				{
+					title: 'WooCommerce.com',
+					href: 'https://woocommerce.com',
+					linkType: 'external',
+				},
+				{
+					title: 'WordPress.org',
+					href: 'https://wordpress.org',
+				},
+			];
 
-		render( <List items={ listItems } /> );
+			render( <List items={ listItems } /> );
 
-		expect(
-			screen.getByRole( 'menuitem', { name: 'Add products' } ).dataset
-				.listItemTag
-		).toBe( 'add-product' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'Market my store' } ).dataset
-				.listItemTag
-		).toBe( 'marketing' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'WooCommerce.com' } ).dataset
-				.listItemTag
-		).toBe( 'woocommerce.com-site' );
-		expect(
-			screen.getByRole( 'menuitem', { name: 'WordPress.org' } ).dataset
-				.listItemTag
-		).toBeUndefined();
+			expect(
+				screen.getByRole( 'menuitem', { name: 'Add products' } ).dataset
+					.linkType
+			).toBe( 'wp-admin' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'Market my store' } )
+					.dataset.linkType
+			).toBe( 'wc-admin' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'WooCommerce.com' } )
+					.dataset.linkType
+			).toBe( 'external' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'WordPress.org' } )
+					.dataset.linkType
+			).toBe( 'external' );
+		} );
+
+		it( 'should set `data-list-item-tag` on items', () => {
+			const listItems = [
+				{
+					title: 'Add products',
+					href: '/post-new.php?post_type=product',
+					linkType: 'wp-admin',
+					listItemTag: 'add-product',
+				},
+				{
+					title: 'Market my store',
+					href: '/admin.php?page=wc-admin&path=%2Fmarketing',
+					linkType: 'wc-admin',
+					listItemTag: 'marketing',
+				},
+				{
+					title: 'WooCommerce.com',
+					href: 'https://woocommerce.com',
+					linkType: 'external',
+					listItemTag: 'woocommerce.com-site',
+				},
+				{
+					title: 'WordPress.org',
+					href: 'https://wordpress.org',
+				},
+			];
+
+			render( <List items={ listItems } /> );
+
+			expect(
+				screen.getByRole( 'menuitem', { name: 'Add products' } ).dataset
+					.listItemTag
+			).toBe( 'add-product' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'Market my store' } )
+					.dataset.listItemTag
+			).toBe( 'marketing' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'WooCommerce.com' } )
+					.dataset.listItemTag
+			).toBe( 'woocommerce.com-site' );
+			expect(
+				screen.getByRole( 'menuitem', { name: 'WordPress.org' } )
+					.dataset.listItemTag
+			).toBeUndefined();
+		} );
 	} );
 } );
