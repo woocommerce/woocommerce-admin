d97be224eb93ed2b874d2f9b8f4b10725c56d21a
Joshua Flowers <joshuatf@gmail.com>
2021-04-14 18:15:35 -0400

    Add PluginInstaller class

---
+++ src/PluginsInstaller.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * PluginsInstaller
+ *
+ * Installer to allow plugin installation via URL query.
+ */
+
+namespace Automattic\WooCommerce\Admin;
+
+defined( 'ABSPATH' ) || exit;
+
+use Automattic\WooCommerce\Admin\API\Plugins;
+
+/**
+ * Class PluginsInstaller
+ */
+class PluginsInstaller {
+	/**
+	 * Constructor
+	 */
+	public static function init() {
+		add_action( 'admin_init', array( __CLASS__, 'possibly_install_activate_plugins' ) );
+	}
+
+	/**
+	 * Check if an install or activation is being requested via URL query.
+	 */
+	public static function possibly_install_activate_plugins() {
+		/* phpcs:disable WordPress.Security.NonceVerification.Recommended */
+		if ( ! isset( $_GET['plugin_action'] ) || ! isset( $_GET['plugins'] ) || ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+
+		$plugins       = sanitize_text_field( wp_unslash( $_GET['plugins'] ) );
+		$plugin_action = sanitize_text_field( wp_unslash( $_GET['plugin_action'] ) );
+		/* phpcs:enable WordPress.Security.NonceVerification.Recommended */
+
+		$plugins_api     = new Plugins();
+		$install_result  = null;
+		$activate_result = null;
+
+		switch ( $plugin_action ) {
+			case 'install':
+				$install_result = $plugins_api->install_plugins( array( 'plugins' => $plugins ) );
+				break;
+			case 'activate':
+				$activate_result = $plugins_api->activate_plugins( array( 'plugins' => $plugins ) );
+				break;
+			case 'install-activate':
+				$install_result  = $plugins_api->install_plugins( array( 'plugins' => $plugins ) );
+				$activate_result = $plugins_api->activate_plugins( array( 'plugins' => implode( ',', $install_result['data']['installed'] ) ) );
+				break;
+		}
+
+		self::display_results( $result );
+	}
+
+	/**
+	 * Display the results of installation and activation on the page.
+	 *
+	 * @param array $install_result Result of installation.
+	 * @param array $activate_result Result of activation.
+	 */
+	public static function display_results( $install_result, $activate_result ) {
+		if ( ! $install_result && ! $activate_result ) {
+			return;
+		}
+
+		// @todo Parse results to plugin names and messages to display in a notice.
+	}
+
+}
