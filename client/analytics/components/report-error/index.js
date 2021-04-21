ead3313da5ea2104f16ecdaf5bb323bfb5365a9d
Tomek Wytrębowicz <tomalecpub@gmail.com>
2021-04-21 19:35:48 +0200

    Change `ReportError` to a function component.

--- client/analytics/components/report-error/index.js
+++ client/analytics/components/report-error/index.js
@@ -2,36 +2,34 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import PropTypes from 'prop-types';
 import { EmptyContent } from '@woocommerce/components';
 
 /**
  * Component to render when there is an error in a report component due to data
  * not being loaded or being invalid.
+ *
+ * @param {Object} props React props.
+ * @param {string} [props.className] Additional class name to style the component.
  */
-class ReportError extends Component {
-	render() {
-		const { className } = this.props;
-
-		const title = __(
-			'There was an error getting your stats. Please try again.',
-			'woocommerce-admin'
-		);
-		const actionLabel = __( 'Reload', 'woocommerce-admin' );
-		const actionCallback = () => {
-			// @todo Add tracking for how often an error is displayed, and the reload action is clicked.
-			window.location.reload();
-		};
-		return (
-			<EmptyContent
-				className={ className }
-				title={ title }
-				actionLabel={ actionLabel }
-				actionCallback={ actionCallback }
-			/>
-		);
-	}
+function ReportError( { className } ) {
+	const title = __(
+		'There was an error getting your stats. Please try again.',
+		'woocommerce-admin'
+	);
+	const actionLabel = __( 'Reload', 'woocommerce-admin' );
+	const actionCallback = () => {
+		// @todo Add tracking for how often an error is displayed, and the reload action is clicked.
+		window.location.reload();
+	};
+	return (
+		<EmptyContent
+			className={ className }
+			title={ title }
+			actionLabel={ actionLabel }
+			actionCallback={ actionCallback }
+		/>
+	);
 }
 
 ReportError.propTypes = {
