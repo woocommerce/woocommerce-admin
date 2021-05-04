f108058858dea128421962e0d3a6165291f6f004
Joshua Flowers <joshuatf@gmail.com>
2021-05-04 18:16:33 -0400

    Only pass required setting keys instead of fields

--- src/Features/RemotePaymentMethods/PaymentGatewaysController.php
+++ src/Features/RemotePaymentMethods/PaymentGatewaysController.php
@@ -36,38 +36,14 @@ class PaymentGatewaysController {
 			$data['oauth_connection_url'] = $gateway->get_oauth_connection_url();
 		}
 
-		$data['setup_fields'] = self::get_setup_form_fields( $gateway );
+		if ( method_exists( $gateway, 'get_required_settings_keys' ) ) {
+			$data['required_settings_keys'] = $gateway->get_required_settings_keys();
+		}
 
 		$data['settings_url'] = admin_url( 'admin.php?page=wc-settings&tab=checkout&section=' . strtolower( $gateway->id ) );
 
 		$response->set_data( $data );
 
 		return $response;
-	}
-
-	/**
-	 * Get setup fields from keys.
-	 *
-	 * @param  WC_Payment_Gateway $gateway    Payment gateway object.
-	 * @return array
-	 */
-	public static function get_setup_form_fields( $gateway ) {
-		if ( ! method_exists( $gateway, 'get_setup_form_field_keys' ) ) {
-			return apply_filters( 'woocommerce_settings_api_setup_form_fields_' . $gateway->id, array(), $gateway );
-		}
-
-		$setup_form_field_keys = $gateway->get_setup_form_field_keys();
-		$form_fields           = $gateway->get_form_fields();
-		$setup_fields          = array();
-
-		foreach ( $setup_form_field_keys as $key ) {
-			if ( ! isset( $form_fields[ $key ] ) ) {
-				continue;
-			}
-
-			$setup_fields[ $key ] = $form_fields[ $key ];
-		}
-
-		return apply_filters( 'woocommerce_settings_api_setup_form_fields_' . $gateway->id, $setup_fields, $gateway );
 	}
 }
