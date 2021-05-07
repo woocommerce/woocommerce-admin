ca46ff5e30f64b889b3a8ec68dd7f6d69b30c60c
Ilyas Foo <foo.ilyas@gmail.com>
2021-05-07 11:51:23 +0800

    Bump version to 2.2.4

--- woocommerce-admin.php
+++ woocommerce-admin.php
@@ -7,7 +7,7 @@
  * Author URI: https://woocommerce.com/
  * Text Domain: woocommerce-admin
  * Domain Path: /languages
- * Version: 2.2.3
+ * Version: 2.2.4
  * Requires at least: 5.4
  * Requires PHP: 7.0
  *
