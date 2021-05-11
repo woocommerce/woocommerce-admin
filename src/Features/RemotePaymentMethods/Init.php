45554ab4e46cddea724696a825ff0b2d0e67b77b
Joshua Flowers <joshuatf@gmail.com>
2021-05-11 13:51:34 -0400

    Change ruleset to object

--- src/Features/RemotePaymentMethods/Init.php
+++ src/Features/RemotePaymentMethods/Init.php
@@ -86,7 +86,7 @@ class Init {
 				'content'    => __( 'The PayFast extension for WooCommerce enables you to accept payments by Credit Card and EFT via one of South Africa’s most popular payment gateways. No setup fees or monthly subscription costs.  Selecting this extension will configure your store to use South African rands as the selected currency.', 'woocommerce-admin' ),
 				'image'      => __( 'https =>//www.payfast.co.za/assets/images/payfast_logo_colour.svg', 'woocommerce-admin' ),
 				'plugins'    => array( 'woocommerce-payfast-gateway' ),
-				'is_visible' => array(
+				'is_visible' => (object) array(
 					'type'      => 'base_location_country',
 					'value'     => 'ZA',
 					'operation' => '=',
