5558bd3978ec5a2d12c55e6cee83bc154a515b48
Lourens Schep <lourensschep@gmail.com>
2021-05-12 11:49:32 -0300

    Fix another test

--- tests/wc-admin-helper.php
+++ tests/wc-admin-helper.php
@@ -40,7 +40,7 @@ class WC_Admin_Tests_Admin_Helper extends WP_UnitTestCase {
 	 */
 	public function test_is_wc_admin_active_in_date_range_with_invalid_range() {
 		$this->expectException( \InvalidArgumentException::class );
-		$this->expectExceptionMessage( '"random-range" range is not supported, use one of: week-1, week-2-4, month-1-3, month-3-6, month-6+' );
+		$this->expectExceptionMessage( '"random-range" range is not supported, use one of: week-1, week-1-4, month-1-3, month-3-6, month-6+' );
 
 		WCAdminHelper::is_wc_admin_active_in_date_range( 'random-range' );
 	}
