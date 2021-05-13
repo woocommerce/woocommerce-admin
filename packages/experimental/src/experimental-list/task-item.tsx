3f421d728aecb4290826101f4b762378ab8d8b3e
Lourens Schep <lourensschep@gmail.com>
2021-05-13 13:19:55 -0300

    Directly reference the EllipsisMenu for now to circumvent a circular dependency

--- packages/experimental/src/experimental-list/task-item.tsx
+++ packages/experimental/src/experimental-list/task-item.tsx
@@ -5,7 +5,7 @@ import { __ } from '@wordpress/i18n';
 import { Icon, check } from '@wordpress/icons';
 import { Button, Tooltip } from '@wordpress/components';
 import NoticeOutline from 'gridicons/dist/notice-outline';
-import { EllipsisMenu } from '@woocommerce/components';
+import EllipsisMenu from '@woocommerce/components/build/ellipsis-menu';
 import classnames from 'classnames';
 import { sanitize } from 'dompurify';
 
