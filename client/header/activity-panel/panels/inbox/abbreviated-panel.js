e93a39cbfdf5451ee382c4a50594bf50ecf8b646
Fernando Marichal <contacto@fernandomarichal.com>
2021-05-14 15:38:33 -0300

    Modified "critical alert" presentation

--- client/header/activity-panel/panels/inbox/abbreviated-panel.js
+++ client/header/activity-panel/panels/inbox/abbreviated-panel.js
@@ -4,7 +4,7 @@
 import { __, sprintf } from '@wordpress/i18n';
 import { Card, CardBody, Icon } from '@wordpress/components';
 import { Text } from '@woocommerce/experimental';
-import { Link, Pill } from '@woocommerce/components';
+import { Link } from '@woocommerce/components';
 import { recordEvent } from '@woocommerce/tracks';
 
 export const AbbreviatedPanel = ( {
@@ -20,42 +20,41 @@ export const AbbreviatedPanel = ( {
 			task: name,
 		} );
 	};
-	const criticalPill = () => {
+	const addCriticalAlert = () => {
 		if ( critical === 0 ) {
 			return;
 		}
 		return (
-			<Pill>
-				{ sprintf(
-					/* translators: Number of critical alerts */
-					__( '%d critical alert', 'woocommerce-admin' ),
-					critical
-				) }
-			</Pill>
+			<div>
+				<span
+					className={
+						'woocommerce-abbreviated-panel__critical-alert'
+					}
+				>
+					{ sprintf(
+						/* translators: Number of critical alerts */
+						__( '%d critical alert', 'woocommerce-admin' ),
+						critical
+					) }
+				</span>
+			</div>
 		);
 	};
 	return (
-		<Card className="woocommerce-abbreviated-panels__element">
+		<Card className="woocommerce-abbreviated-panel">
 			<CardBody size={ null }>
 				<Link
 					href={ href }
 					onClick={ trackAbbreviatedPanelClick }
 					type="wp-admin"
 				>
-					<div className="woocommerce-abbreviated-panels__icon">
+					<div className="woocommerce-abbreviated-panel__icon">
 						<Icon icon={ icon } />
 					</div>
-					<div className="woocommerce-abbreviated-panels__message">
-						<Text
-							as="h3"
-							className="woocommerce-task-payment__title"
-						>
-							{ title }
-						</Text>
-						<div className="woocommerce-task-payment__content">
-							<span>{ content }</span>
-						</div>
-						{ criticalPill() }
+					<div className="woocommerce-abbreviated-panel__message">
+						<Text as="h3">{ title }</Text>
+						<Text>{ content }</Text>
+						{ critical > 0 && addCriticalAlert() }
 					</div>
 				</Link>
 			</CardBody>
