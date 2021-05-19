a163281e530774cc3ee7ece8a0ec1797c1358f4f
Moon <moon.kyong@automattic.com>
2021-05-19 12:36:53 -0700

    Export store name

--- packages/data/src/payment-gateways/index.ts
+++ packages/data/src/payment-gateways/index.ts
@@ -13,6 +13,8 @@ import * as selectors from './selectors';
 import reducer from './reducer';
 import { STORE_KEY } from './constants';
 
+export const PAYMENT_GATEWAYS_STORE_NAME = STORE_KEY;
+
 registerStore( STORE_KEY, {
 	actions,
 	selectors,
