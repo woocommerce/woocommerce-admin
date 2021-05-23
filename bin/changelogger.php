1a14fba205402d57f92e7f9c7d96bf29108144d7
Moon <moon.kyong@automattic.com>
2021-05-23 15:48:55 -0700

    Remove use of WriteCommand and implement parse() in the Formatter

--- bin/changelogger.php
+++ bin/changelogger.php
@@ -1,12 +1,8 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/changelogger/WriteCommand.php';
 require_once __DIR__ . '/changelogger/WCAdminFormatter.php';
-require_once __DIR__ . '/changelogger/CommandLoader.php';
 
 class_alias(WCAdminFormatter::class, Automattic\Jetpack\Changelogger\Plugins\WCAdminFormatter::class);
 
-$app = new Automattic\Jetpack\Changelogger\Application();
-$app->setCommandLoader(new CommandLoader());
-exit( $app->run() );
+require_once __DIR__ .'/../vendor/bin/changelogger';
