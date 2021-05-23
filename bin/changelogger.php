c88fc13bdfa81b92ee1625d3ba5f8f7630df4f21
Moon <moon.kyong@automattic.com>
2021-05-23 13:50:52 -0700

    Refactor and clean up
    * Extends Jetpack Changelogger's write command
    
    * Only load add and merge commands
    
    * Uses changelog.txt

--- bin/changelogger.php
+++ bin/changelogger.php
@@ -1,10 +1,12 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/changelogger/WriteCommand.php';
 require_once __DIR__ . '/changelogger/WCAdminFormatter.php';
-require_once __DIR__ . '/changelogger/WCAdminWriteCommand.php';
+require_once __DIR__ . '/changelogger/CommandLoader.php';
 
 class_alias(WCAdminFormatter::class, Automattic\Jetpack\Changelogger\Plugins\WCAdminFormatter::class);
-class_alias(WCAdminWriteCommand::class, Automattic\Jetpack\Changelogger\WriteCommand::class);
 
-require_once __DIR__ . '/../vendor/bin/changelogger';
+$app = new Automattic\Jetpack\Changelogger\Application();
+$app->setCommandLoader(new CommandLoader());
+exit( $app->run() );
