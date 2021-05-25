50d0ddeb2546b830de7e820a2b54040765801a15
Moon <moon.kyong@automattic.com>
2021-05-25 07:09:00 -0700

    Add add and write commands to manage testing instructions as segments

---
+++ bin/testing-instructions/Application.php
@@ -0,0 +1,36 @@
+<?php
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * User interface for the changelogger tool.
+ */
+class Application extends SymfonyApplication {
+
+	const VERSION = '0.0.1';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		parent::__construct( 'Testing Instructions', self::VERSION );
+	}
+
+	/**
+	 * Called when the application is run.
+	 *
+	 * @param InputInterface $input InputInterface.
+	 * @param OutputInterface $output OutputInterface.
+	 *
+	 * @return int
+	 * @throws Throwable
+	 */
+	public function doRun( InputInterface $input, OutputInterface $output ) {
+		$output->getFormatter()->setStyle( 'warning', new OutputFormatterStyle( 'black', 'yellow' ) );
+		return parent::doRun( $input, $output );
+	}
+
+}
---
+++ bin/testing-instructions/WriteCommand.php
@@ -0,0 +1,92 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WriteCommand extends Command {
+
+	/**
+	 * The default command name
+	 *
+	 * @var string|null
+	 */
+	protected static $defaultName = 'write';
+
+	private $config;
+
+	public function __construct(array $config) {
+		parent::__construct();
+	    $this->config = $config;
+	}
+
+	/**
+	 * Configures the command.
+	 */
+	protected function configure() {
+		$this->setDescription( 'Updates the TESTING-INSTRUCTIONS.md from segment files' );
+		$this->addArgument( 'version', InputArgument::REQUIRED );
+	}
+
+
+	/**
+	 * Executes the command.
+	 *
+	 * @param InputInterface $input InputInterface.
+	 * @param OutputInterface $output OutputInterface.
+	 *
+	 * @return int
+	 */
+	protected function execute( InputInterface $input, OutputInterface $output ) {
+
+		$file = $this->config['index_file'];
+		$version = $input->getArgument( 'version' );
+
+		$contents = $this->mergeSegmentFiles();
+
+		$pattern = '/^## Unreleased((?:(?!^##).)+)/ms';
+		$replacement = "## Unreleased\n## {$version}\n\n" . $contents . "\n\n";
+
+		$readme = file_get_contents( $file );
+		$new_contents = preg_replace( $pattern, $replacement, $readme );
+		if ( strlen( $new_contents ) === strlen( $readme ) || null === $new_contents ) {
+			$output->writeln( "Failed to write the changelog: please check readme.txt format." );
+			return self::FATAL_EXIT;
+		}
+
+		file_put_contents( $file, $new_contents );
+
+		$this->deleteSegments();
+
+		$output->writeln( "Index file has been updated with version {$version}." );
+	}
+
+	/**
+	 * Merge segment files into a single string.
+	 *
+	 * @return string
+	 */
+	protected function mergeSegmentFiles() {
+		$segment_dir = $this->config['segment_file_dir'];
+		$files = glob( $segment_dir . '/*.md' );
+		$contents = '';
+		foreach ( $files as $file ) {
+			$content = file_get_contents($file);
+			$contents .= "\n{$content}";
+		}
+
+		return $contents;
+	}
+
+	/**
+	 * Delete segment files.
+	 */
+	protected function deleteSegments() {
+		$segment_dir = $this->config['segment_file_dir'];
+		$files = glob( $segment_dir . '/*.md' );
+		foreach ( $files as $file ) {
+			unlink($file);
+		}
+	}
+}
