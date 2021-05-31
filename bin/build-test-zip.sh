ca13c2990984aa6c091ba9276330182f5c25b9c3
Sam Seay <samueljseay@gmail.com>
2021-06-01 09:38:05 +1200

    Fix a bug where the test zip command used the wrong file suffix for JS assets
    No changelog required.

--- bin/build-test-zip.sh
+++ bin/build-test-zip.sh
@@ -2,7 +2,7 @@
 #
 # Build a test zip from current branch for uploading to test site
 #
-npm run build
+WC_ADMIN_PHASE=plugin npm run build
 composer install --no-dev
 rm woocommerce-admin.zip
 ./bin/make-zip.sh woocommerce-admin.zip
