dba5d0314103956b8f71f34452db3097267e48c6
Moon <moon.kyong@automattic.com>
2021-06-16 20:20:50 -0700

    Remove bin/changelogger.php wrapper

--- bin/changelogger.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/changelogger/WCAdminFormatter.php';
-
-class_alias(WCAdminFormatter::class, Automattic\Jetpack\Changelogger\Plugins\WCAdminFormatter::class);
-
-require_once __DIR__ .'/../vendor/bin/changelogger';
