ff2d790cea18d805f5ea913b084df7c076a46e5d
Lourens Schep <lourensschep@gmail.com>
2021-06-24 14:54:55 -0300

    Add tests for moving sections up and down

--- tests/e2e/pages/AnalyticsOverview.ts
+++ tests/e2e/pages/AnalyticsOverview.ts
@@ -1,8 +1,17 @@
+import { ElementHandle } from 'puppeteer';
 import { waitForElementByText } from '../utils/actions';
 import { Analytics } from './Analytics';
 
+type Section = {
+	title: string;
+	element: ElementHandle< Element >;
+};
+const isSection = ( item: Section | undefined ): item is Section => {
+	return !! item;
+};
+
 export class AnalyticsOverview extends Analytics {
-	async navigateTo() {
+	async navigate() {
 		await this.navigateToSection( 'overview' );
 	}
 
@@ -10,15 +19,22 @@ export class AnalyticsOverview extends Analytics {
 		const list = await this.page.$$(
 			'.woocommerce-dashboard-section .woocommerce-section-header'
 		);
-		return Promise.all(
-			list.map( async ( item ) => ( {
-				title: await item.evaluate( ( element ) => {
+		const sections = await Promise.all(
+			list.map( async ( item ) => {
+				const title = await item.evaluate( ( element ) => {
 					const header = element.querySelector( 'h2' );
 					return header?.textContent;
-				} ),
-				element: item,
-			} ) )
+				} );
+				if ( title ) {
+					return {
+						title,
+						element: item,
+					};
+				}
+				return undefined;
+			} )
 		);
+		return sections.filter( isSection );
 	}
 
 	async openSectionEllipsis( sectionTitle: string ) {
@@ -27,9 +43,30 @@ export class AnalyticsOverview extends Analytics {
 		);
 		if ( section ) {
 			const ellipsisMenu = await section.element.$(
-				'.woocommerce-ellipsis-menu button'
+				'.woocommerce-ellipsis-menu .woocommerce-ellipsis-menu__toggle'
 			);
-			ellipsisMenu?.click();
+			await ellipsisMenu?.click();
+			await this.page.waitForSelector(
+				'.woocommerce-ellipsis-menu div[role=menu]'
+			);
+		}
+	}
+
+	async closeSectionEllipsis( sectionTitle: string ) {
+		const section = ( await this.getSections() ).find(
+			( section ) => section.title === sectionTitle
+		);
+		if ( section ) {
+			const ellipsisMenu = await section.element.$(
+				'.woocommerce-ellipsis-menu .woocommerce-ellipsis-menu__toggle'
+			);
+			await ellipsisMenu?.click();
+			await page.waitFor(
+				() =>
+					! document.querySelector(
+						'.woocommerce-ellipsis-menu div[role=menu]'
+					)
+			);
 		}
 	}
 
@@ -44,5 +81,47 @@ export class AnalyticsOverview extends Analytics {
 		await this.page.click( "button[title='Add more sections']" );
 		const item = await waitForElementByText( 'span', sectionTitle );
 		await item?.click();
+	}
+
+	async moveSectionDown( sectionTitle: string ) {
+		await this.openSectionEllipsis( sectionTitle );
+		const item = await waitForElementByText( 'div', 'Move down' );
+		await item?.click();
+	}
+
+	async moveSectionUp( sectionTitle: string ) {
+		await this.openSectionEllipsis( sectionTitle );
+		const item = await waitForElementByText( 'div', 'Move up' );
+		await item?.click();
+	}
+
+	async getEllipsisMenuItems( sectionTitle: string ) {
+		await this.openSectionEllipsis( sectionTitle );
+		const list = await this.page.$$(
+			'.woocommerce-ellipsis-menu div[role=menuitem]'
+		);
+		return Promise.all(
+			list.map( async ( item ) => ( {
+				title: await item.evaluate(
+					( element ) => element?.textContent
+				),
+				element: item,
+			} ) )
+		);
+	}
+
+	async getEllipsisMenuCheckboxItems( sectionTitle: string ) {
+		await this.openSectionEllipsis( sectionTitle );
+		const list = await this.page.$$(
+			'.woocommerce-ellipsis-menu div[role=menuitemcheckbox]'
+		);
+		return Promise.all(
+			list.map( async ( item ) => ( {
+				title: await item.evaluate(
+					( element ) => element?.textContent
+				),
+				element: item,
+			} ) )
+		);
 	}
 }
