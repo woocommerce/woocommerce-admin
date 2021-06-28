c89bd489155910fad4b0f1202f6396293289c647
Jeff Stieler <jeff.m.stieler@gmail.com>
2021-06-28 09:58:50 -0500

    Fix experimental package build.

--- packages/experimental/src/vertical-css-transition/vertical-css-transition.tsx
+++ packages/experimental/src/vertical-css-transition/vertical-css-transition.tsx
@@ -1,7 +1,12 @@
 /**
  * External dependencies
  */
-import { useState, useCallback, useRef } from '@wordpress/element';
+import {
+	createElement,
+	useState,
+	useCallback,
+	useRef,
+} from '@wordpress/element';
 import { CSSTransitionProps } from 'react-transition-group/CSSTransition';
 import { CSSTransition } from 'react-transition-group';
 
