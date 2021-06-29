840dae333356e4b409dc773b0ac59d0a5d91d609
Ilyas Foo <foo.ilyas@gmail.com>
2021-06-29 13:23:09 +0800

    Adding /dist directory to release

---
+++ dist/wp-admin-scripts/navigation-opt-out.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => '1d6bf561ebd7dbaa23196d75a058175b');
---
+++ dist/wp-admin-scripts/onboarding-tax-notice.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-i18n'), 'version' => 'd4c864a4c79c1d898616e47b8dbb3af0');
