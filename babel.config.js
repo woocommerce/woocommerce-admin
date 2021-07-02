341605fb75a0d0170a251c1e2e0f29b7fb0a1c73
Jeff Stieler <jeff.m.stieler@gmail.com>
2021-07-02 10:14:22 -0500

    Fix storybook.

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,17 @@ module.exports = function ( api ) {
 			 * This allows arrow functions as class methods so that binding
 			 * methods to `this` in the constructor isn't required.
 			 */
-			'@babel/plugin-proposal-class-properties',
+			[
+				'@babel/plugin-proposal-class-properties',
+				{
+					loose: true,
+				},
+			],
+			[ '@babel/plugin-proposal-private-methods', { loose: true } ],
+			[
+				'@babel/plugin-proposal-private-property-in-object',
+				{ loose: true },
+			],
 		],
 		env: {
 			production: {
