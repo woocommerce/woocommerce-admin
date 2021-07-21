14564066490c6e8c2c9b39acadad0d9c664f2fe0
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-07-21 11:44:58 +0800

    Adding /dist directory to release

---
+++ dist/wp-admin-scripts/beta-features-tracking-modal.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wc-explat', 'wc-store-data', 'wc-tracks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '86921689434272fb99d0d6add325cde2');
---
+++ dist/wp-admin-scripts/onboarding-product-import-notice.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n'), 'version' => 'a4aefee95d1506468a41418d36650f8a');
