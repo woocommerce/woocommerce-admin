e34a4261aedf68b3cab76cca049a297b4ac4203c
Joshua Flowers <joshuatf@gmail.com>
2021-07-22 11:21:33 -0400

    Wait for install to create lookup tables

--- src/CategoryLookup.php
+++ src/CategoryLookup.php
@@ -139,9 +139,7 @@ class CategoryLookup {
 	 */
 	public function on_create( $category_id ) {
 		// If WooCommerce is being installed on a multisite, lookup tables haven't been created yet.
-		// Create the tables and regenerate so that the default categories can be created.
 		if ( 'yes' === get_transient( 'wc_installing' ) ) {
-			wp_schedule_single_event( time() + 10, 'generate_category_lookup_table' );
 			return;
 		}
 
