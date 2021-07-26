e3e0e64139b6aa22cad61353773a90ef003b3ce8
Fernando Marichal <contacto@fernandomarichal.com>
2021-07-26 16:04:15 -0300

    Adding /dist directory to release

---
+++ dist/experimental/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wc-components', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'cadcc735c30f9db5b1408406f5c806c4');
