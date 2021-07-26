b634b158bbf6c67e9c569cc2327eae9c3c397460
Fernando Marichal <contacto@fernandomarichal.com>
2021-07-26 16:11:43 -0300

    Adding /dist directory to release

---
+++ dist/data/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'wc-date', 'wc-navigation', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '6d584c3e736d173e09448c2d55237149');
