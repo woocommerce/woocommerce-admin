671ff80956d977c2d2b89b6c73ef1990fbe60b79
Moon <moon.kyong@automattic.com>
2021-08-05 11:59:24 -0700

    Adding /dist directory to release

---
+++ dist/experimental/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wc-components', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'ca40cb432cb025572fdba1128aec233b');
---
+++ dist/wp-admin-scripts/marketing-coupons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wc-components', 'wc-experimental', 'wc-tracks', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'ba29e9420f235ef75bd499887342076d');
