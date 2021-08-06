061c6fbb24e9c0e7418ed1825513b6bc1d2fe172
Maria Górska-Piszek <maria@madeincosmos.net>
2021-08-06 11:29:33 +0200

    Split the Extensions page into Marketplace and My Subscriptions.

--- includes/connect-existing-pages.php
+++ includes/connect-existing-pages.php
@@ -22,19 +22,19 @@ function wc_admin_get_core_pages_to_connect() {
 	}
 
 	return array(
-		'wc-addons'   => array(
-			'title' => __( 'Extensions', 'woocommerce-admin' ),
+		'wc-addons'        => array(
+			'title' => __( 'Marketplace', 'woocommerce-admin' ),
 			'tabs'  => array(),
 		),
-		'wc-reports'  => array(
+		'wc-reports'       => array(
 			'title' => __( 'Reports', 'woocommerce-admin' ),
 			'tabs'  => $report_tabs,
 		),
-		'wc-settings' => array(
+		'wc-settings'      => array(
 			'title' => __( 'Settings', 'woocommerce-admin' ),
 			'tabs'  => apply_filters( 'woocommerce_settings_tabs_array', array() ),
 		),
-		'wc-status'   => array(
+		'wc-status'        => array(
 			'title' => __( 'Status', 'woocommerce-admin' ),
 			'tabs'  => apply_filters(
 				'woocommerce_admin_status_tabs',
@@ -44,6 +44,10 @@ function wc_admin_get_core_pages_to_connect() {
 					'logs'   => __( 'Logs', 'woocommerce-admin' ),
 				)
 			),
+		),
+		'wc-subscriptions' => array(
+			'title' => __( 'My Subscriptions', 'woocommerce-admin' ),
+			'tabs'  => array(),
 		),
 	);
 }
@@ -56,6 +60,7 @@ function wc_admin_get_core_pages_to_connect() {
  */
 function wc_admin_filter_core_page_breadcrumbs( $breadcrumbs ) {
 	$screen_id              = PageController::get_instance()->get_current_screen_id();
+	$screen_id              = fix_helper_screen_id( $screen_id );
 	$pages_to_connect       = wc_admin_get_core_pages_to_connect();
 	$woocommerce_breadcrumb = array(
 		'admin.php?page=wc-admin',
@@ -106,6 +111,7 @@ function wc_admin_filter_core_page_breadcrumbs( $breadcrumbs ) {
 function wc_admin_connect_core_pages( $is_connected, $current_page ) {
 	if ( false === $is_connected && false === $current_page ) {
 		$screen_id        = PageController::get_instance()->get_current_screen_id();
+		$screen_id        = fix_helper_screen_id( $screen_id );
 		$pages_to_connect = wc_admin_get_core_pages_to_connect();
 
 		foreach ( $pages_to_connect as $page_id => $page_data ) {
@@ -121,6 +127,21 @@ function wc_admin_connect_core_pages( $is_connected, $current_page ) {
 }
 
 add_filter( 'woocommerce_navigation_is_connected_page', 'wc_admin_connect_core_pages', 10, 2 );
+
+/**
+ * Set a special screen ID for My Subscriptions page.
+ * We want to maintain existing page URL wc-addons&section=helper, so it can't be set the usual way.
+ *
+ * @param string $screen_id ID of the current page.
+ * @return string ID of the current page.
+ */
+function fix_helper_screen_id( $screen_id ) {
+	if ( 'woocommerce_page_wc-addons-browse-extensions-helper' === $screen_id ) {
+		$screen_id = 'woocommerce_page_wc-subscriptions-helper';
+	}
+
+	return $screen_id;
+}
 
 $posttype_list_base = 'edit.php';
 
