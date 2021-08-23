122bfa1ecb907578f227a929d9a7788526cf1c8e
Lourens Schep <lourensschep@gmail.com>
2021-08-23 15:55:56 -0300

    Make sure WooCommerce payments is not displayed when psuedo class is

--- src/Features/WCPayPromotion/Init.php
+++ src/Features/WCPayPromotion/Init.php
@@ -9,6 +9,7 @@ defined( 'ABSPATH' ) || exit;
 
 use Automattic\WooCommerce\Admin\Loader;
 use Automattic\WooCommerce\Admin\Features\PaymentGatewaySuggestions\EvaluateSuggestion;
+use Automattic\WooCommerce\Admin\PaymentPlugins;
 
 /**
  * WC Pay Promotion engine.
@@ -23,12 +24,11 @@ class Init {
 		include_once __DIR__ . '/WCPaymentGatewayPsuedoWCPay.php';
 
 		add_action( 'change_locale', array( __CLASS__, 'delete_specs_transient' ) );
+		add_filter( PaymentPlugins::FILTER_NAME, array( __CLASS__, 'possibly_filter_recommended_payment_gateways' ) );
 
 		if ( ! isset( $_GET['page'] ) || 'wc-settings' !== $_GET['page'] || ! isset( $_GET['tab'] ) || 'checkout' !== $_GET['tab'] ) { // phpcs:ignore WordPress.Security.NonceVerification
 			return;
 		}
-
-
 
 		add_filter( 'woocommerce_payment_gateways', array( __CLASS__, 'possibly_register_psuedo_wc_pay_gateway' ) );
 		add_filter( 'option_woocommerce_gateway_order', [ __CLASS__, 'set_gateway_top_of_list' ] );
@@ -53,32 +53,63 @@ class Init {
 	}
 
 	/**
-	 * Delete the specs transient.
+	 * Possibly registers a psuedo gateway.
 	 *
 	 * @param array $gateways list of gateway classes.
+	 * @return array list of gateway classes.
 	 */
 	public static function possibly_register_psuedo_wc_pay_gateway( $gateways ) {
-		if ( ! class_exists( '\WC_Payments' ) ) {
-			$wc_pay_spec = self::get_wc_pay_promotion_spec();
-
-			if ( ! $wc_pay_spec ) {
-				return;
-			}
-
-			$anon_id = isset( $_COOKIE['tk_ai'] ) ? $_COOKIE['tk_ai'] : null;
-			$abtest = new \WooCommerce\Admin\Experimental_Abtest(
-				$anon_id,
-				'woocommerce',
-				'yes' === get_option( 'woocommerce_allow_tracking' )
-			);
-
-			$variation_name = $abtest->get_variation( 'woocommerce_wc_pay_promotion_payment_methods_table_' . $wc_pay_spec->version );
-
-			if ( $variation_name === 'treatment' ) {
-				$gateways[] = 'Automattic\WooCommerce\Admin\Features\WCPayPromotion\WCPaymentGatewayPsuedoWCPay';
-			}
+		if ( self::register_psuedo_wc_pay_gateway() ) {
+			$gateways[] = 'Automattic\WooCommerce\Admin\Features\WCPayPromotion\WCPaymentGatewayPsuedoWCPay';
 		}
 		return $gateways;
+	}
+
+	/**
+	 * Possibly registers a psuedo gateway.
+	 *
+	 * @param array $gateways list of gateway classes.
+	 * @return array list of gateway classes.
+	 */
+	public static function possibly_filter_recommended_payment_gateways( $payment_methods ) {
+		error_log('possibly filter');
+		if ( self::register_psuedo_wc_pay_gateway() ) {
+			error_log('filtering');
+			return array_filter( $payment_methods, function($payment_method) {
+				return $payment_method['product'] !== 'woocommerce-payments';
+			});
+		}
+		return $payment_methods;
+	}
+
+	/**
+	 * Checks if psuedo gateway should be registered.
+	 *
+	 * @return boolean if psuedo gateway should be registered.
+	 */
+	public static function register_psuedo_wc_pay_gateway( ) {
+		if ( class_exists( '\WC_Payments' ) ) {
+			return false;
+		}
+		$wc_pay_spec = self::get_wc_pay_promotion_spec();
+
+		if ( ! $wc_pay_spec ) {
+			return false;
+		}
+
+		$anon_id = isset( $_COOKIE['tk_ai'] ) ? $_COOKIE['tk_ai'] : null;
+		$abtest = new \WooCommerce\Admin\Experimental_Abtest(
+			$anon_id,
+			'woocommerce',
+			'yes' === get_option( 'woocommerce_allow_tracking' )
+		);
+
+		$variation_name = $abtest->get_variation( 'woocommerce_wc_pay_promotion_payment_methods_table_' . $wc_pay_spec->version );
+
+		if ( $variation_name === 'treatment' ) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
