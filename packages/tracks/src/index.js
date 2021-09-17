ea83ea8d85f08378fd4a8035e58dc945c96f0c58
Lourens Schep <lourensschep@gmail.com>
2021-09-17 16:21:02 -0300

    Fix track properties and add warning for wrong track format

--- packages/tracks/src/index.js
+++ packages/tracks/src/index.js
@@ -14,8 +14,8 @@ const tracksDebug = debug( 'wc-admin:tracks' );
  * @param {string} eventName The name of the event to record, don't include the wcadmin_ prefix
  * @param {Object} eventProperties event properties to include in the event
  */
-
 export function recordEvent( eventName, eventProperties ) {
+	checkEventNameAndPropsCase( eventName, eventProperties );
 	tracksDebug( 'recordevent %s %o', 'wcadmin_' + eventName, eventProperties, {
 		_tqk: window._tkq,
 		shouldRecord:
@@ -34,6 +34,28 @@ export function recordEvent( eventName, eventProperties ) {
 	}
 
 	window.wcTracks.recordEvent( eventName, eventProperties );
+}
+
+const validRegExpString = '^[a-z_][a-z0-9_]*$';
+const validRegExp = new RegExp( validRegExpString );
+function checkEventNameAndPropsCase( eventName, eventProperties ) {
+	let valid = true;
+	if ( ! validRegExp.test( eventName ) ) {
+		console.warn(
+			`Event name ${ eventName } failed pattern ${ validRegExpString }`
+		);
+		valid = false;
+	}
+	const propertyKeys = Object.keys( eventProperties || {} );
+	for ( const key of propertyKeys ) {
+		if ( ! validRegExp.test( key ) ) {
+			console.warn(
+				`Event prop ${ key } failed pattern ${ validRegExpString } of '${ eventName }' event`
+			);
+			valid = false;
+		}
+	}
+	return valid;
 }
 
 const tracksQueue = {
