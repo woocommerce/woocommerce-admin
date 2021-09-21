31b64c1b8dcf1eeaf37d9823412423f70c5583da
Moon <moon.kyong@automattic.com>
2021-09-21 12:31:58 -0700

    Adding /dist directory to release

---
+++ dist/data/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'wc-date', 'wc-navigation', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'aa56e9003d31958cabf3a6be91f669be');
---
+++ dist/number/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '528b97ce28b1722355f5da17828a0154');
