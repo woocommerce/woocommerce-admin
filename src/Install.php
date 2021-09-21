30c9d5bc6be6e86c877d911dd37975bfec465674
Joshua T Flowers <joshuatf@gmail.com>
2021-09-21 15:06:49 -0400

    Fix unsecured report files (#7691)
    * Fix unsecured report files
    
    * Add changelog entry

--- src/Install.php
+++ src/Install.php
@@ -57,6 +57,11 @@ class Install {
 			'wc_admin_update_170_homescreen_layout',
 			'wc_admin_update_170_db_version',
 		),
+		'2.7.0'  => array(
+			'wc_admin_update_270_update_task_list_options',
+			'wc_admin_update_270_delete_report_downloads',
+			'wc_admin_update_270_db_version',
+		),
 	);
 
 	/**
