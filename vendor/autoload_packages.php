a0211143b681ef0ab4df9a15544cb5dc46bfe70e
Moon <moon.kyong@automattic.com>
2021-09-21 19:22:23 -0700

    Adding feature-config.php directory to release

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -178,7 +178,7 @@ if ( ! function_exists( __NAMESPACE__ . '\autoloader' ) ) {
 /**
  * Prepare all the classes for autoloading.
  */
-function enqueue_packages_9d77e6a6712249a9af1a6405f32ed7b7() {
+function enqueue_packages_f4828d3262faa82d9135a4e34daf0289() {
 	$class_map = require_once dirname( __FILE__ ) . '/composer/autoload_classmap_package.php';
 	foreach ( $class_map as $class_name => $class_info ) {
 		enqueue_package_class( $class_name, $class_info['version'], $class_info['path'] );
@@ -202,4 +202,4 @@ function enqueue_packages_9d77e6a6712249a9af1a6405f32ed7b7() {
 		file_loader(); // Either WordPress is not loaded or plugin is doing it wrong. Either way we'll load the files so nothing breaks.
 	}
 }
-enqueue_packages_9d77e6a6712249a9af1a6405f32ed7b7();
+enqueue_packages_f4828d3262faa82d9135a4e34daf0289();
