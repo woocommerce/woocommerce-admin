dd52cacab5184db3a671c72e51790053bf16eb0c
Moon <moon.kyong@automattic.com>
2021-09-21 21:57:26 -0700

    Adding /dist directory to release

---
+++ dist/wp-admin-scripts/print-shipping-label-banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-store-data', 'wc-tracks', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e815804c5eb786e9a5de4719065f82a8');
