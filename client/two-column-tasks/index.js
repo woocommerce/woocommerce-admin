880840f4da468ae5b78d1087d5e894ec221d7ee6
Moon <moon.kyong@automattic.com>
2021-09-24 16:27:55 -0700

    Remove unused code -- these are no longer used as we are using the new task list REST API

--- client/two-column-tasks/index.js
+++ client/two-column-tasks/index.js
@@ -4,12 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { useEffect } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
-import {
-	ONBOARDING_STORE_NAME,
-	OPTIONS_STORE_NAME,
-	PLUGINS_STORE_NAME,
-	SETTINGS_STORE_NAME,
-} from '@woocommerce/data';
+import { ONBOARDING_STORE_NAME, OPTIONS_STORE_NAME } from '@woocommerce/data';
 import { useExperiment } from '@woocommerce/explat';
 
 /**
@@ -17,73 +12,24 @@ import { useExperiment } from '@woocommerce/explat';
  */
 import '../task-list/style.scss';
 import './style.scss';
-import { getCountryCode } from '../dashboard/utils';
 import TaskList from './task-list';
 import { TaskStep } from '../task-list/task-step';
 import TaskListPlaceholder from './placeholder';
 
-const EMPTY_ARRAY = [];
 const taskDashboardSelect = ( select ) => {
-	const { getFreeExtensions, getProfileItems, getTasksStatus } = select(
-		ONBOARDING_STORE_NAME
-	);
-	const { getSettings } = select( SETTINGS_STORE_NAME );
 	const { getOption, hasFinishedResolution } = select( OPTIONS_STORE_NAME );
-	const {
-		getActivePlugins,
-		getInstalledPlugins,
-		isJetpackConnected,
-	} = select( PLUGINS_STORE_NAME );
-	const profileItems = getProfileItems();
-
 	const trackedCompletedTasks =
-		getOption( 'woocommerce_task_list_tracked_completed_tasks' ) ||
-		EMPTY_ARRAY;
-
-	const trackedCompletedActions =
-		getOption( 'woocommerce_task_list_tracked_completed_actions' ) ||
-		EMPTY_ARRAY;
-
-	const { general: generalSettings = {} } = getSettings( 'general' );
-	const countryCode = getCountryCode(
-		generalSettings.woocommerce_default_country
-	);
-	const {
-		woocommerce_store_address: storeAddress,
-		woocommerce_default_country: defaultCountry,
-		woocommerce_store_postcode: storePostCode,
-	} = generalSettings;
-	const hasCompleteAddress = Boolean(
-		storeAddress && defaultCountry && storePostCode
-	);
-
-	const activePlugins = getActivePlugins();
-	const installedPlugins = getInstalledPlugins();
-	const onboardingStatus = getTasksStatus();
-
+		getOption( 'woocommerce_task_list_tracked_completed_tasks' ) || [];
 	return {
-		activePlugins,
-		countryCode,
 		dismissedTasks: getOption( 'woocommerce_task_list_dismissed_tasks' ),
-		freeExtensions: getFreeExtensions(),
 		remindMeLaterTasks: getOption(
 			'woocommerce_task_list_remind_me_later_tasks'
 		),
-		isExtendedTaskListComplete:
-			getOption( 'woocommerce_extended_task_list_complete' ) === 'yes',
-		isExtendedTaskListHidden:
-			getOption( 'woocommerce_extended_task_list_hidden' ) === 'yes',
-		isJetpackConnected: isJetpackConnected(),
 		isSetupTaskListHidden:
 			getOption( 'woocommerce_task_list_hidden' ) === 'yes',
 		isTaskListComplete:
 			getOption( 'woocommerce_task_list_complete' ) === 'yes',
-		installedPlugins,
-		trackedCompletedActions,
-		onboardingStatus,
-		profileItems,
 		trackedCompletedTasks,
-		hasCompleteAddress,
 		isResolving:
 			! hasFinishedResolution( 'getOption', [
 				'woocommerce_task_list_complete',
@@ -92,19 +38,13 @@ const taskDashboardSelect = ( select ) => {
 				'woocommerce_task_list_hidden',
 			] ) ||
 			! hasFinishedResolution( 'getOption', [
-				'woocommerce_extended_task_list_complete',
-			] ) ||
-			! hasFinishedResolution( 'getOption', [
-				'woocommerce_extended_task_list_hidden',
-			] ) ||
-			! hasFinishedResolution( 'getOption', [
 				'woocommerce_task_list_remind_me_later_tasks',
 			] ) ||
 			! hasFinishedResolution( 'getOption', [
-				'woocommerce_task_list_tracked_completed_tasks',
+				'woocommerce_task_list_dismissed_tasks',
 			] ) ||
 			! hasFinishedResolution( 'getOption', [
-				'woocommerce_task_list_dismissed_tasks',
+				'woocommerce_task_list_tracked_completed_tasks',
 			] ),
 	};
 };
