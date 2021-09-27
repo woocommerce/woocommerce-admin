055a8e146cb6f6ce863836a222e46fabec9f87ef
Joshua Flowers <joshuatf@gmail.com>
2021-09-27 18:35:53 -0400

    Optimistically mark shipping task complete

--- client/tasks/task.tsx
+++ client/tasks/task.tsx
@@ -2,11 +2,27 @@
  * External dependencies
  */
 import { WooOnboardingTask } from '@woocommerce/onboarding';
+import { getHistory, getNewPath } from '@woocommerce/navigation';
+import { ONBOARDING_STORE_NAME } from '@woocommerce/data';
+import { recordEvent } from '@woocommerce/tracks';
+import { useCallback } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
 
 export type TaskProps = {
 	query: { task: string };
 };
 
-export const Task: React.FC< TaskProps > = ( { query } ) => (
-	<WooOnboardingTask.Slot id={ query.task } fillProps={ { query } } />
-);
+export const Task: React.FC< TaskProps > = ( { query } ) => {
+	const id = query.task;
+	const { optimisticallyCompleteTask } = useDispatch( ONBOARDING_STORE_NAME );
+
+	const onComplete = useCallback( () => {
+		optimisticallyCompleteTask( id );
+		getHistory().push( getNewPath( {}, '/', {} ) );
+		// @todo Invalidate cache store to double check?
+	}, [ id ] );
+
+	return (
+		<WooOnboardingTask.Slot id={ id } fillProps={ { onComplete, query } } />
+	);
+};
