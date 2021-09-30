b654d07ccce671e05e0edcfd9d881cb526af4c36
Joshua Flowers <joshuatf@gmail.com>
2021-09-30 13:20:39 -0400

    Move deprecated options to separate class

--- src/Features/OnboardingTasks/Init.php
+++ src/Features/OnboardingTasks/Init.php
@@ -8,6 +8,7 @@ namespace Automattic\WooCommerce\Admin\Features\OnboardingTasks;
 
 use \Automattic\WooCommerce\Admin\Loader;
 use Automattic\WooCommerce\Admin\API\Reports\Taxes\Stats\DataStore as TaxDataStore;
+use Automattic\WooCommerce\Admin\Features\OnboardingTasks\DeprecatedOptions;
 use Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks\Tax;
 
 /**
@@ -37,10 +38,7 @@ class Init {
 	public function __construct() {
 		// This hook needs to run when options are updated via REST.
 		add_action( 'admin_enqueue_scripts', array( $this, 'update_option_extended_task_list' ), 15 );
-		add_filter( 'pre_option_woocommerce_task_list_hidden', array( $this, 'get_deprecated_options' ), 10, 2 );
-		add_filter( 'pre_option_woocommerce_extended_task_list_hidden', array( $this, 'get_deprecated_options' ), 10, 2 );
-		add_action( 'pre_update_option_woocommerce_task_list_hidden', array( $this, 'update_deprecated_options' ), 10, 3 );
-		add_action( 'pre_update_option_woocommerce_extended_task_list_hidden', array( $this, 'update_deprecated_options' ), 10, 3 );
+		DeprecatedOptions::init();
 		TaskLists::init();
 
 		if ( ! is_admin() ) {
@@ -136,57 +134,6 @@ class Init {
 				return;
 			}
 			$extended_list->show();
-		}
-	}
-
-	/**
-	 * Get the values from the correct source when attempting to retrieve deprecated options.
-	 *
-	 * @param string $pre_option Pre option value.
-	 * @param string $option Option name.
-	 * @return string
-	 */
-	public function get_deprecated_options( $pre_option, $option ) {
-		if ( defined( 'WC_ADMIN_INSTALLING' ) && WC_ADMIN_INSTALLING ) {
-			return $pre_option;
-		};
-
-		$hidden = get_option( 'woocommerce_task_list_hidden_lists', array() );
-		switch ( $option ) {
-			case 'woocommerce_task_list_hidden':
-				return in_array( 'setup', $hidden, true ) ? 'yes' : 'no';
-			case 'woocommerce_extended_task_list_hidden':
-				return in_array( 'extended', $hidden, true ) ? 'yes' : 'no';
-		}
-	}
-
-	/**
-	 * Updates the new option names when deprecated options are updated.
-	 * This is a temporary fallback until we can fully remove the old task list components.
-	 *
-	 * @param string $value New value.
-	 * @param string $old_value Old value.
-	 * @param string $option Option name.
-	 * @return string
-	 */
-	public function update_deprecated_options( $value, $old_value, $option ) {
-		switch ( $option ) {
-			case 'woocommerce_task_list_hidden':
-				$task_list = TaskLists::get_list( 'setup' );
-				if ( ! $task_list ) {
-					return;
-				}
-				$update = 'yes' === $value ? $task_list->hide() : $task_list->show();
-				delete_option( 'woocommerce_task_list_hidden' );
-				return false;
-			case 'woocommerce_extended_task_list_hidden':
-				$task_list = TaskLists::get_list( 'extended' );
-				if ( ! $task_list ) {
-					return;
-				}
-				$update = 'yes' === $value ? $task_list->hide() : $task_list->show();
-				delete_option( 'woocommerce_extended_task_list_hidden' );
-				return false;
 		}
 	}
 }
