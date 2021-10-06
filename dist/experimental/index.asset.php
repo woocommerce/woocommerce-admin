70ac58bb27e27ddc8779d085ff25425bcc54e821
Matt Sherman <matt@jam123.com>
2021-10-06 17:07:44 -0400

    Adding /dist directory to release

---
+++ dist/experimental/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wc-components', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '043875e45444b389bb472e73c86fa511');
