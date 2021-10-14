aea505bc07991e5467ddfda731374f55b59279b1
Lourens Schep <lourensschep@gmail.com>
2021-10-14 17:23:31 -0300

    Update reset functionality

--- packages/admin-e2e-tests/src/fixtures/reset.ts
+++ packages/admin-e2e-tests/src/fixtures/reset.ts
@@ -1,19 +1,14 @@
 /**
- * External dependencies
- */
-import { withRestApi } from '@woocommerce/e2e-utils';
-/**
  * Internal dependencies
  */
 import { httpClient } from './http-client';
-import { resetSettingsGroupToDefault } from './settings';
 
 const resetEndpoint = '/woocommerce-reset/v1/state';
 
 export async function resetWooCommerceState() {
 	const response = await httpClient.delete( resetEndpoint );
+	expect( response.data.options ).toEqual( true );
+	expect( response.data.transients ).toEqual( true );
+	expect( response.data.notes ).toEqual( true );
 	expect( response.statusCode ).toEqual( 200 );
-	await withRestApi.resetSettingsGroupToDefault( 'general' );
-	await withRestApi.resetSettingsGroupToDefault( 'products' );
-	await withRestApi.resetSettingsGroupToDefault( 'tax' );
 }
