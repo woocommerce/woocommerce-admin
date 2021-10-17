28d80358f1c2ef49c1a0afad34b75be6dd6b9011
Fernando Marichal <contacto@fernandomarichal.com>
2021-10-17 18:57:43 -0300

    Add e2e tests

---
+++ packages/admin-e2e-tests/src/pages/ProductsSetup.ts
@@ -0,0 +1,30 @@
+/**
+ * Internal dependencies
+ */
+import { waitForElementByText, getElementByText } from '../utils/actions';
+import { BasePage } from './BasePage';
+
+export class ProductsSetup extends BasePage {
+	url = 'wp-admin/admin.php?page=wc-admin&task=products';
+
+	async isDisplayed() {
+		await waitForElementByText( 'h1', 'Add products' );
+	}
+
+	async isStartWithATemplateDisplayed( templatesCount: number ) {
+		await waitForElementByText( 'h1', 'Start with a template' );
+		const length = await this.page.$$eval(
+			'.components-radio-control__input',
+			( items ) => items.length
+		);
+		expect( length === templatesCount ).toBeTruthy();
+	}
+
+	async clickStartWithTemplate() {
+		const startWithATemplate = await getElementByText(
+			'span',
+			'Start with a template'
+		);
+		await startWithATemplate?.click();
+	}
+}
