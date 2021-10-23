e5162f891e8590cc5c1fbd69475d51ce6b2421c2
Matt Sherman <matt@jam123.com>
2021-10-22 21:41:21 -0400

    Adding /dist directory to release

---
+++ dist/data/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'wc-date', 'wc-navigation', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-data-controls', 'wp-deprecated', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '3dbf416e43e296bd27152079dca2aa58');
