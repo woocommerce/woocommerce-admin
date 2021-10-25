26b2c2405eb7cef6e4edb3050e64b471188fcd84
Moon <moon.kyong@automattic.com>
2021-10-25 14:52:56 -0700

    Remove unallowedTasks and move it to concat

--- client/two-column-tasks/extended-task.tsx
+++ client/two-column-tasks/extended-task.tsx
@@ -112,15 +112,14 @@ const ExtendedTask: React.FC< TasksProps > = ( { query } ) => {
 		return list.id === 'setup';
 	} );
 
-	const unallowedTasks =
+	extendedTaskList.tasks = extendedTaskList.tasks.concat(
 		setupTaskList?.tasks.filter( ( unallowedTask ) => {
 			return (
 				! allowedTasks.includes( unallowedTask.id ) &&
 				unallowedTask.id !== 'store_details'
 			);
-		} ) || [];
-
-	extendedTaskList.tasks = extendedTaskList.tasks.concat( unallowedTasks );
+		} ) || []
+	);
 
 	if ( extendedTaskList.tasks.length === 0 ) {
 		return null;
