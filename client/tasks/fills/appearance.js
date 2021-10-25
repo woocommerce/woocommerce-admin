91b9b08aa9095077b71d684686b42b948282bfca
Joshua Flowers <joshuatf@gmail.com>
2021-10-25 18:09:32 -0400

    Remove task status data store

--- client/tasks/fills/appearance.js
+++ client/tasks/fills/appearance.js
@@ -50,7 +50,7 @@ class Appearance extends Component {
 	}
 
 	componentDidMount() {
-		const { themeMods } = this.props.tasksStatus;
+		const { themeMods } = this.props.task.extra;
 
 		if ( themeMods && themeMods.custom_logo ) {
 			/* eslint-disable react/no-did-mount-set-state */
@@ -106,7 +106,7 @@ class Appearance extends Component {
 	}
 
 	importProducts() {
-		const { clearTaskStatusCache, createNotice } = this.props;
+		const { createNotice } = this.props;
 		this.setState( { isPending: true } );
 
 		recordEvent( 'tasklist_appearance_import_demo', {} );
@@ -132,7 +132,6 @@ class Appearance extends Component {
 							'woocommerce-admin'
 						)
 					);
-					clearTaskStatusCache();
 				}
 
 				this.setState( { isPending: false } );
@@ -421,26 +420,18 @@ class Appearance extends Component {
 const AppearanceWrapper = compose(
 	withSelect( ( select ) => {
 		const { getOption } = select( OPTIONS_STORE_NAME );
-		const { getTasksStatus } = select( ONBOARDING_STORE_NAME );
-		const tasksStatus = getTasksStatus();
 
 		return {
 			demoStoreNotice: getOption( 'woocommerce_demo_store_notice' ),
-			stylesheet: getOption( 'stylesheet' ),
-			tasksStatus,
 		};
 	} ),
 	withDispatch( ( dispatch ) => {
 		const { createNotice } = dispatch( 'core/notices' );
 		const { updateOptions } = dispatch( OPTIONS_STORE_NAME );
-		const { actionTask, invalidateResolutionForStoreSelector } = dispatch(
-			ONBOARDING_STORE_NAME
-		);
+		const { actionTask } = dispatch( ONBOARDING_STORE_NAME );
 
 		return {
 			actionTask,
-			clearTaskStatusCache: () =>
-				invalidateResolutionForStoreSelector( 'getTasksStatus' ),
 			createNotice,
 			updateOptions,
 		};
--- src/Features/OnboardingTasks/Init.php
+++ src/Features/OnboardingTasks/Init.php
@@ -41,16 +41,6 @@ class Init {
 	public function __construct() {
 		DeprecatedOptions::init();
 		TaskLists::init();
-
-		if ( ! is_admin() ) {
-			return;
-		}
-
-		// Old settings injection.
-		// Run after Onboarding.
-		add_filter( 'woocommerce_components_settings', array( __CLASS__, 'component_settings' ), 30 );
-		// New settings injection.
-		add_filter( 'woocommerce_admin_shared_settings', array( $this, 'component_settings' ), 30 );
 	}
 
 	/**
@@ -67,36 +57,6 @@ class Init {
 				? $wc_payments_gateway->is_connected()
 				: false;
 		}
-
-		return $settings;
-	}
-
-	/**
-	 * Add task items to component settings.
-	 *
-	 * @param array $settings Component settings.
-	 * @return array
-	 */
-	public function component_settings( $settings ) {
-		// Bail early if not on a wc-admin powered page, or task list shouldn't be shown.
-		if (
-			! \Automattic\WooCommerce\Admin\Loader::is_admin_page() ||
-			! count( TaskLists::get_visible() )
-		) {
-			return $settings;
-		}
-
-		// If onboarding isn't enabled this will throw warnings.
-		if ( ! isset( $settings['onboarding'] ) ) {
-			$settings['onboarding'] = array();
-		}
-
-		$settings['onboarding'] = array_merge(
-			$settings['onboarding'],
-			array(
-				'tasksStatus' => self::get_settings(),
-			)
-		);
 
 		return $settings;
 	}
