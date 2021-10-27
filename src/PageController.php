5b4ee50f6ebb2fa41acd97ba91b5be40bed0d85e
hellwig-data443 <80766926+hellwig-data443@users.noreply.github.com>
2021-10-27 09:59:15 -0400

    Update PageController.php

--- src/PageController.php
+++ src/PageController.php
@@ -356,8 +356,10 @@ class PageController {
 
 		// Disable embed on the block editor.
 		$current_screen = did_action( 'current_screen' ) ? get_current_screen() : false;
-		if ( method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) {
-			$is_connected_page = false;
+		if ( $current_screen !== false ) {
+			if ( method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) {
+				$is_connected_page = false;
+			}
 		}
 
 		/**
