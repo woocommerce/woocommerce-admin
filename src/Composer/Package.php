f467fb6a6425f2dbe0e747476a43a0d1a468aaa4
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-11-03 09:49:58 +0800

    Prepare 2.9.0-beta.1 release

--- src/Composer/Package.php
+++ src/Composer/Package.php
@@ -26,7 +26,7 @@ class Package {
 	 *
 	 * @var string
 	 */
-	const VERSION = '2.9.0-dev';
+	const VERSION = '2.9.0-beta.1';
 
 	/**
 	 * Package active.
--- woocommerce-admin.php
+++ woocommerce-admin.php
@@ -7,7 +7,7 @@
  * Author URI: https://woocommerce.com/
  * Text Domain: woocommerce-admin
  * Domain Path: /languages
- * Version: 2.9.0-dev
+ * Version: 2.9.0-beta.1
  * Requires at least: 5.6
  * Requires PHP: 7.0
  *
