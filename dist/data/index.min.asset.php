a5378e6ea4ec8d21d8779d2c4ac96a8c0efd9258
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-11-17 14:04:33 +0800

    Adding /dist directory to release

---
+++ dist/data/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'wc-date', 'wc-navigation', 'wp-api-fetch', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-data-controls', 'wp-deprecated', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '2487a256c51ba96ee02ad726e767897e');
