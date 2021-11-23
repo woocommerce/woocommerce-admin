8c98f4566a10f9e334502a8ee5ffdb9b1524bbc9
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-11-23 17:05:15 +0800

    Bump version

--- woocommerce-admin.php
+++ woocommerce-admin.php
@@ -7,7 +7,7 @@
  * Author URI: https://woocommerce.com/
  * Text Domain: woocommerce-admin
  * Domain Path: /languages
- * Version: 2.9.0-rc.2
+ * Version: 2.9.0-rc.3
  * Requires at least: 5.6
  * Requires PHP: 7.0
  *
