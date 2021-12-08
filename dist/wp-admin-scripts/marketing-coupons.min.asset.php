cab02750c54a4f06dcedcf4ce8294b004661c535
Joel Thiessen <dygerati@gmail.com>
2021-12-07 16:13:53 -0800

    Adding /dist directory to release

---
+++ dist/wp-admin-scripts/marketing-coupons.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wc-components', 'wc-experimental', 'wc-tracks', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '514a284993411bb8a0767e1278821551');
