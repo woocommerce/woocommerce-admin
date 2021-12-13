8c753aaa538d3c8817b640621ffd1bca41acfd2f
Paul Sealock <psealock@gmail.com>
2021-12-14 12:26:24 +1300

    dashboard

--- client/dashboard/customizable.js
+++ client/dashboard/customizable.js
@@ -32,6 +32,24 @@ import {
 } from '../lib/currency-context';
 
 const DASHBOARD_FILTERS_FILTER = 'woocommerce_admin_dashboard_filters';
+
+/**
+ * An object defining a set of report filters.
+ *
+ * @typedef {Object} filter
+ * @property {string} label Label describing the set of filters.
+ * @property {string} param Url query param this set of filters operates on.
+ * @property {Array.<string>} staticParams Array of `param` that remain constant when other params are manipulated.
+ * @property {Function} showFilters A function with url query as an argument returning a Boolean depending on whether or not the filters should be shown.
+ * @property {Array} filters An array of filter objects.
+ */
+
+/**
+ * Add Report filters to the dashboard. None are added by default.
+ *
+ * @filter woocommerce_admin_dashboard_filters
+ * @param {Array.<filter>} filters Report filters.
+ */
 const filters = applyFilters( DASHBOARD_FILTERS_FILTER, [] );
 
 const mergeSectionsWithDefaults = ( prefSections ) => {
