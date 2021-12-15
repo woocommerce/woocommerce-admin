519fa94a6937ed3e3395052d0aa8b9ffdff4b492
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-12-15 22:33:43 +0800

    Adding /dist directory to release

---
+++ dist/onboarding/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-components', 'wc-experimental', 'wc-tracks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '287d486ff1f439de9a73ff3c2b50abdd');
