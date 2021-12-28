92a236f9a70619dc613e0c2932e615f50bef4b31
Fernando Marichal <contacto@fernandomarichal.com>
2021-12-28 12:46:16 -0300

    Adding /dist directory to release

---
+++ dist/wp-admin-scripts/navigation-opt-out.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd8f15ac109dff5d9582db7dd882a2bcc');
---
+++ dist/wp-admin-scripts/onboarding-product-notice.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-i18n'), 'version' => 'ac2f6d9edca9f56ca631e14e50627e72');
