452eef37682aa200100c05fbfd64337ec2c795a0
Ilyas Foo <foo.ilyas@gmail.com>
2022-01-13 10:28:31 +0800

    Adding /dist directory to release

---
+++ dist/experimental/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wc-components', 'wp-components', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'b5bd1bc1e818f2bfaed40580638248c2');
---
+++ dist/explat/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '0fc92f293d6a3816f8a384737ca01d0a');
