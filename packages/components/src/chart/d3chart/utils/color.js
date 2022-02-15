ddb2849cc4b00efac1e3dba5bbe07fc328b65639
Bero <berislav.grgicak@gmail.com>
2022-02-15 10:00:48 +0100

    Add filter example

--- packages/components/src/chart/d3chart/utils/color.js
+++ packages/components/src/chart/d3chart/utils/color.js
@@ -15,6 +15,26 @@ export const getColor = ( orderedKeys, colorScheme ) => ( key ) => {
 			? selectionLimit
 			: orderedKeys.length;
 	const idx = findIndex( orderedKeys, ( d ) => d.key === key );
+
+	/**
+	 * Color to be used for a chart item.
+	 *
+	 * @filter woocommerce_admin_chart_item_color
+	 * @example
+	 * addFilter(
+	 * 	'woocommerce_admin_chart_item_color',
+	 * 	'example',
+	 * ( idx ) => {
+	 * 	const colorScales = [
+	 *	  "#0A2F51",
+	 *	  "#0E4D64",
+	 *	  "#137177",
+	 *	  "#188977",
+	 *	];
+	 * 	return colorScales[ idx ] || false;
+	 * });
+	 *
+	 */
 	const color = applyFilters(
 		'woocommerce_admin_chart_item_color',
 		idx,
