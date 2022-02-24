2e9a03da46b16ebabb2720dcce7d5dddb669d501
Moon <moon.kyong@automattic.com>
2022-02-24 13:12:43 -0800

    tmp: Add comment and clean up

--- src-internal/Admin/RegisterActions.php
+++ src-internal/Admin/RegisterActions.php
@@ -8,31 +8,37 @@ use Automattic\WooCommerce\Admin\Events;
 use Automattic\WooCommerce\Vendor\League\Container\Container;
 
 
+// Temp test code;
+$container = new Container();
+$container->add(
+	CategoryLookup::class,
+	function() {
+		return CategoryLookup::instance();
+	}
+);
+$container->add(
+	Events::class,
+	function() {
+		return Events::instance();
+	}
+);
+
+/**
+ * Register actions with a custom callback that utilizes DI container
+ * to resolve the callback and call it.
+ */
 class RegisterActions {
 	private $actions;
 	private $container;
-	public function __construct( $actions ) {
+	public function __construct( $actions, Container $container ) {
 		$this->actions = $actions;
-
-		// Temp test code;
-		$container = new Container();
-		$container->add(
-			CategoryLookup::class,
-			function() {
-				return CategoryLookup::instance();
-			}
-		);
-		$container->add(
-			Events::class,
-			function() {
-				return Events::instance();
-			}
-		);
 		$this->container = $container;
 	}
 
 	public function register() {
+		// Loop through each action.
 		foreach ( $this->actions as $action_name => $callbacks ) {
+			// and register action's callbacks.
 			foreach ( $callbacks as $callback ) {
 				add_action(
 					$action_name,
@@ -40,7 +46,8 @@ class RegisterActions {
 						$class_instance = $this->container->get( $callback[0] );
 						call_user_func_array( [ $class_instance, $callback[1] ], func_get_args() );
 					},
-					$callback[2] ?? 10
+					$callback[2] ?? 10,
+					$callback[3] ?? 1
 				);
 			}
 		}
