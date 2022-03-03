c885ce3efc8fbc4d64fa72b02b271a0150d5c9e3
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-03-03 11:59:58 +0800

    Use integers for menu page priority
     In wordpress >= 6.0, the seventh parameter passed to add_menu_page() should be an integer representing menu position.

--- src-internal/Admin/WcPayWelcomePage.php
+++ src-internal/Admin/WcPayWelcomePage.php
@@ -72,7 +72,7 @@ class WcPayWelcomePage {
 				'admin.php?page=wc-admin&path=/wc-pay-welcome-page',
 				null,
 				$menu_icon,
-				55.7,
+				56,
 			);
 
 			call_user_func_array( 'add_menu_page', $menu_with_nav_data );
