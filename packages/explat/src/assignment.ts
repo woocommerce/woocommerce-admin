553b51fa112eb52f7a7ca7854e756ef99043b9dc
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-03-08 16:37:55 +0800

    Export auth exPlat utils

--- packages/explat/src/assignment.ts
+++ packages/explat/src/assignment.ts
@@ -3,13 +3,18 @@
  */
 import { stringify } from 'qs';
 import { applyFilters } from '@wordpress/hooks';
+import apiFetch from '@wordpress/api-fetch';
 
 const EXPLAT_VERSION = '0.1.0';
 
-export const fetchExperimentAssignment = async ( {
+const _fetchExperimentAssignment = async ( {
+	url,
+	path,
 	experimentName,
 	anonId,
 }: {
+	url?: string;
+	path?: string;
 	experimentName: string;
 	anonId: string | null;
 } ): Promise< unknown > => {
@@ -17,6 +22,10 @@ export const fetchExperimentAssignment = async ( {
 		throw new Error(
 			`Tracking is disabled, can't fetch experimentAssignment`
 		);
+	}
+
+	if ( ! url && ! path ) {
+		throw new Error( 'Url or path is required' );
 	}
 
 	/**
@@ -33,21 +42,47 @@ export const fetchExperimentAssignment = async ( {
 	 * });
 	 *
 	 */
-	const params = applyFilters( 'woocommerce_explat_request_args', {
-		experiment_name: experimentName,
-		anon_id: anonId ?? undefined,
-		woo_country_code:
-			window.wcSettings?.preloadSettings?.general
-				?.woocommerce_default_country ||
-			window.wcSettings?.admin?.preloadSettings?.general
-				?.woocommerce_default_country,
-	} );
-
-	const response = await window.fetch(
-		`https://public-api.wordpress.com/wpcom/v2/experiments/${ EXPLAT_VERSION }/assignments/woocommerce?${ stringify(
-			params
-		) }`
+	const queryString = stringify(
+		applyFilters( 'woocommerce_explat_request_args', {
+			experiment_name: experimentName,
+			anon_id: anonId ?? undefined,
+			woo_country_code:
+				window.wcSettings?.preloadSettings?.general
+					?.woocommerce_default_country ||
+				window.wcSettings?.admin?.preloadSettings?.general
+					?.woocommerce_default_country,
+		} )
 	);
 
-	return await response.json();
+	const response = await apiFetch( {
+		url: url && `${ url }?${ queryString }`,
+		path: path && `${ path }?${ queryString }`,
+	} );
+	return response;
 };
+
+export const fetchExperimentAssignment = async ( {
+	experimentName,
+	anonId,
+}: {
+	experimentName: string;
+	anonId: string | null;
+} ): Promise< unknown > =>
+	_fetchExperimentAssignment( {
+		url: `https://public-api.wordpress.com/wpcom/v2/experiments/${ EXPLAT_VERSION }/assignments/woocommerce?`,
+		experimentName,
+		anonId,
+	} );
+
+export const fetchExperimentAssignmentWithAuth = async ( {
+	experimentName,
+	anonId,
+}: {
+	experimentName: string;
+	anonId: string | null;
+} ): Promise< unknown > =>
+	_fetchExperimentAssignment( {
+		path: '/wc-admin/experiments/assignment',
+		experimentName,
+		anonId,
+	} );
