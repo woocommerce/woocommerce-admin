7ef618eb887b4879009f3d8e293b5b16fee43d9a
Lourens Schep <lourensschep@gmail.com>
2022-03-18 15:46:42 -0300

    Delete purchase E2E test file

--- tests/e2e/specs/tasks/purchase.test.tsx
+++
@@ -1,6 +0,0 @@
-const {
-	testAdminPurchaseSetupTask,
-} = require( '@woocommerce/admin-e2e-tests' );
-
-// skip for now as the second part of the test is failing on Github.
-// testAdminPurchaseSetupTask();
